c20d8ec7c0103fdaa700c06973616438e457f078
Lars Olesen <lars@intraface.dk>
2007-05-17 19:06:03 +0000

    Ran the file through CodeSniffer and updated accordingly.

--- Intraface/XMLRPC/Newsletter/Server.php
+++ Intraface/XMLRPC/Newsletter/Server.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Newsletter XMLRPC Server
+ *
  * @package Newsletter
  * @author  Lars Olesen <lars@legestue.net>
  * @since   0.1.0
@@ -13,193 +15,200 @@ require_once 'Intraface/Intranet.php';
 require_once 'Intraface/Setting.php';
 require_once 'Intraface/modules/contact/Contact.php';
 
-class Intraface_XMLRPC_Newsletter_Server {
+class Intraface_XMLRPC_Newsletter_Server
+{
 
-	private $kernel;
-	private $list;
-	private $subscriber;
-	private $credentials;
+    private $kernel;
+    private $list;
+    private $subscriber;
+    private $credentials;
 
-	function factoryList($list_id) {
-		$this->kernel->useModule('newsletter');
+    function factoryList($list_id)
+    {
+        $this->kernel->useModule('newsletter');
 
-		$this->list = new NewsletterList($this->kernel, $list_id);
+        $this->list = new NewsletterList($this->kernel, $list_id);
 
-		if (!$this->list->doesListExist()) {
-			throw new XML_RPC2_FaultException('the newsletter list does not exist', -2);
-		}
+        if (!$this->list->doesListExist()) {
+            throw new XML_RPC2_FaultException('the newsletter list does not exist', -2);
+        }
 
-		$this->subscriber = new NewsletterSubscriber($this->list);
-	}
+        $this->subscriber = new NewsletterSubscriber($this->list);
+    }
 
-	/**
-	 * Subscribe to newsletter list
-	 *
-	 * @param struct $credentials
-	 * @param integer $list_id
-	 * @param string $email
-	 * @param string $ip
-	 * @return boolean
-	 */
+    /**
+     * Subscribe to newsletter list
+     *
+     * @param struct  $credentials Must include private_key and session_id
+     * @param integer $list_id     List to subscribe to
+     * @param string  $email       Email to subscribe
+     * @param string  $ip          Which email subscribes
+     *
+     * @return boolean
+     */
+    function subscribe($credentials, $list_id, $email, $ip)
+    {
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-	function subscribe($credentials, $list_id, $email, $ip) {
-		if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+        $this->factoryList($list_id);
 
-		$this->factoryList($list_id);
+        if (!$this->subscriber->subscribe(array('email' => $email, 'ip' => $ip))) {
+            throw new XML_RPC2_FaultException('an error occurred when trying to subscribe: ' . implode(',', $this->subscriber->error->message), -4);
+        }
 
-		if (!$this->subscriber->subscribe(array('email' => $email, 'ip' => $ip))) {
-			throw new XML_RPC2_FaultException('an error occurred when trying to subscribe: ' . implode(',', $this->subscriber->error->message), -4);
-		}
+        return true;
 
-		return true;
+    }
 
-	}
+    /**
+     * Unsubscribe from newsletter list
+     *
+     * @param struct  $credentials Must include private_key and session_id
+     * @param integer $list_id     List would you unsubscribe from
+     * @param string  $email       Email to unsubscribe
+     *
+     * @return boolean
+     */
+    function unsubscribe($credentials, $list_id, $email)
+    {
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-	/**
-	 * Unsubscribe from newsletter list
-	 *
-	 * @param struct $credentials
-	 * @param integer $list_id
-	 * @param string $email
-	 * @return boolean
-	 */
+        $this->factoryList($list_id);
 
-	function unsubscribe($credentials, $list_id, $email) {
-		if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+        if (!$this->subscriber->unsubscribe($email)) {
+            throw new XML_RPC2_FaultException('Du kunne ikke framelde dig ' .$arg[1], -4);
+        }
 
-		$this->factoryList($list_id);
+        return 1;
+    }
 
-		if (!$this->subscriber->unsubscribe($email)) {
-			throw new XML_RPC2_FaultException('Du kunne ikke framelde dig ' .$arg[1], -4);
-		}
+    /**
+     * Opt in for a newsletter
+     *
+     * @param struct  $credentials Must include private_key and session_id
+     * @param integer $list_id     Which list should be opted in to
+     * @param string  $optin_code  The code to optin with
+     * @param string  $ip          Which ip is optin from
+     *
+     * @return boolean
+     */
+    function optin($credentials, $list_id, $optin_code, $ip)
+    {
 
-		return 1;
-	}
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-	/**
-	 * Opt in for a newsletter
-	 *
-	 * @param struct $credenials
-	 * @param integer $list_id
-	 * @param string $optin_code
-	 * @param string $ip
-	 * @return boolean
-	 */
-	function optin($credentials, $list_id, $optin_code, $ip) {
+        $this->factoryList($list_id);
 
-		if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+        if (!$this->subscriber->optIn($optin_code, $ip)) {
+            throw new XML_RPC2_FaultException('Du kunne ikke bekræfte din tilmelding', -4);
+        }
 
-		$this->factoryList($list_id);
+        return true;
 
-		if (!$this->subscriber->optIn($optin_code, $ip)) {
-			throw new XML_RPC2_FaultException('Du kunne ikke bekræfte din tilmelding', -4);
-		}
+    }
 
-		return 1;
+    /**
+     * Gets all newsletter lists on an intranet
+     *
+     * @param struct $credentials Must include private_key and session_id
+     *
+     * @return array
+     */
+    function getNewsletterList($credentials)
+    {
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-	}
+        if (!$this->kernel->intranet->hasModuleAccess('newsletter')) {
+            return array();
+        }
+        $this->kernel->module('newsletter');
 
-	/**
-	 * Gets all newsletter lists on an intranet
-	 *
-	 * @param struct $credentials
-	 * @return array
-	 */
-	function getNewsletterList($credentials) {
-		if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+        $list = new NewsletterList($this->kernel);
 
-		if (!$this->kernel->intranet->hasModuleAccess('newsletter')) {
-			return array();
-		}
-		$this->kernel->module('newsletter');
+        return $list->getList();
+    }
 
-		$list = new NewsletterList($this->kernel);
+    /**
+     * Gets all the users subscriptions
+     *
+     * @param struct  $credentials Must include private_key and session_id
+     * @param integer $contact_id  The contact id
+     *
+     * @return array
+     */
+    function getSubscriptions($credentials, $contact_id)
+    {
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-		return $list->getList();
-	}
+        $this->kernel->useModule('contact', true);
 
-	/**
-	 * Gets all the users subscriptions
-	 *
-	 * @param struct $credentials
-	 * @param integer $contact_id
-	 * @return array
-	 */
-	function getSubscriptions($credentials, $contact_id) {
-		if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+        $contact = new Contact($this->kernel, $contact_id);
+        return $contact->getNewsletterSubscriptions();
+    }
 
-		$this->kernel->useModule('contact', true);
+    /**
+     * Gets all the lists that needs the contacts attention for optin
+     *
+     * @param struct  $credentials Must include private_key and session_id
+     * @param integer $contact_id  The contact id
+     *
+     * @return array
+     */
+    function needOptin($credentials, $contact_id)
+    {
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-		$contact = new Contact($this->kernel, $contact_id);
-		return $contact->getNewsletterSubscriptions();
-	}
+        $this->kernel->useModule('contact', true);
 
-	/**
-	 * Gets all the lists that needs the contacts attention for optin
-	 *
-	 * @param struct $credentials
-	 * @param integer $contact_id
-	 * @return array
-	 */
-	function needOptin($credentials, $contact_id) {
-		if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+        $contact = new Contact($this->kernel, $contact_id);
+        return $contact->needNewsletterOptin();
+    }
 
-		$this->kernel->useModule('contact', true);
+    /**
+     * Checking credentials
+     *
+     * @param struct $credentials Must include private_key and session_id
+     *
+     * @return array
+     */
+    function checkCredentials($credentials)
+    {
+        if (count($credentials) != 2) { // -4
+            throw new XML_RPC2_FaultException('wrong argument count in $credentials - got ' . count($credentials) . ' arguments - need 2', -4);
+        }
+        if (empty($credentials['private_key'])) { // -5
+            throw new XML_RPC2_FaultException('supply a private_key', -5);
+        }
+        if (empty($credentials['session_id'])) { // -5
+            throw new XML_RPC2_FaultException('supply a session_id', -5);
+        }
 
-		$contact = new Contact($this->kernel, $contact_id);
-		return $contact->needNewsletterOptin();
-	}
+        $weblogin = new Weblogin();
+        if (!$intranet_id = $weblogin->auth('private', $credentials['private_key'], $credentials['session_id'])) {
+            throw new XML_RPC2_FaultException('access to intranet denied', -2);
+        }
 
-	/**
-	 * Checking credentials
-	 *
-	 * @param struct $credentials
-	 * @return array
-	 */
-	function checkCredentials($credentials) {
-		/*
-		if (is_object($this->kernel) AND is_object($this->kernel->intranet)) {
-			return true;
-		}
-		*/
+        $this->kernel = new Kernel();
+        $this->kernel->intranet = new Intranet($intranet_id);
+        $this->kernel->setting = new Setting($this->kernel->intranet->get('id'));
 
-		if (count($credentials) != 2) { // -4
-			throw new XML_RPC2_FaultException('wrong argument count in $credentials - got ' . count($credentials) . ' arguments - need 2', -4);
-		}
-		if (empty($credentials['private_key'])) { // -5
-			throw new XML_RPC2_FaultException('supply a private_key', -5);
-		}
-		if (empty($credentials['session_id'])) { // -5
-			throw new XML_RPC2_FaultException('supply a session_id', -5);
-		}
+        if (!is_object($this->kernel->intranet)) { // -2
+            throw new XML_RPC2_FaultException('could not create intranet', -2);
+        }
 
-		$weblogin = new Weblogin('some session');
-		if (!$intranet_id = $weblogin->auth('private', $credentials['private_key'], $credentials['session_id'])) {
-			throw new XML_RPC2_FaultException('access to intranet denied', -2);
-		}
-
-		$this->kernel = new Kernel();
-		$this->kernel->intranet = new Intranet($intranet_id);
-		$this->kernel->setting = new Setting($this->kernel->intranet->get('id'));
-
-		if (!is_object($this->kernel->intranet)) { // -2
-			throw new XML_RPC2_FaultException('could not create intranet', -2);
-		}
-
-		return true;
-	}
-
+        return true;
+    }
 }
 ?>
