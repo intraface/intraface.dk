2606abf0adf246a157434591f7785345e727adfc
Lars Olesen <lars@intraface.dk>
2007-05-18 03:48:22 +0000

    Added createDBQuery

--- intraface.dk/modules/intranetmaintenance/index.php
+++ intraface.dk/modules/intranetmaintenance/index.php
@@ -8,15 +8,16 @@ $module = $kernel->module("intranetmaintenance");
 $translation = $kernel->getTranslation('intranetmaintenance');
 
 $intranetmaintenance = new IntranetMaintenance($kernel);
+$intranetmaintenance->createDBQuery();
 
 if(isset($_GET["search"])) {
 
-	if(isset($_GET["text"]) && $_GET["text"] != "") {
-		$intranetmaintenance->dbquery->setFilter("text", $_GET["text"]);
-	}
+    if(isset($_GET["text"]) && $_GET["text"] != "") {
+        $intranetmaintenance->dbquery->setFilter("text", $_GET["text"]);
+    }
 }
 elseif(isset($_GET['character'])) {
-	$intranetmaintenance->dbquery->useCharacter();
+    $intranetmaintenance->dbquery->useCharacter();
 }
 
 $intranetmaintenance->dbquery->defineCharacter('character', 'name');
@@ -33,18 +34,18 @@ $page->start($translation->get('intranets'));
 <h1><?php echo $translation->get('intranets'); ?></h1>
 
 <ul class="options">
-	<li><a href="intranet_edit.php"><?php echo $translation->get('create', 'common'); ?></a></li>
-	<li><a href="users.php"><?php echo $translation->get('users'); ?></a></li>
+    <li><a href="intranet_edit.php"><?php echo $translation->get('create', 'common'); ?></a></li>
+    <li><a href="users.php"><?php echo $translation->get('users'); ?></a></li>
 </ul>
 
 <form method="get" action="index.php">
-	<fieldset>
-		<legend><?php echo safeToHtml($translation->get('search')); ?></legend>
-		<label><?php echo safeToHtml($translation->get('search text')); ?>:
-			<input type="text" name="text" value="<?php echo $intranetmaintenance->dbquery->getFilter("text"); ?>" />
-		</label>
-		<span><input type="submit" name="search" value="<?php echo safeToHtml($translation->get('search')); ?>" /></span>
-	</fieldset>
+    <fieldset>
+        <legend><?php echo safeToHtml($translation->get('search')); ?></legend>
+        <label><?php echo safeToHtml($translation->get('search text')); ?>:
+            <input type="text" name="text" value="<?php echo $intranetmaintenance->dbquery->getFilter("text"); ?>" />
+        </label>
+        <span><input type="submit" name="search" value="<?php echo safeToHtml($translation->get('search')); ?>" /></span>
+    </fieldset>
 </form>
 
 
@@ -53,24 +54,24 @@ $page->start($translation->get('intranets'));
 
 <table>
 <thead>
-	<tr>
-		<th><?php echo $translation->get('name'); ?></th>
-		<th>&nbsp;</th>
-	</tr>
+    <tr>
+        <th><?php echo $translation->get('name'); ?></th>
+        <th>&nbsp;</th>
+    </tr>
 </thead>
 <tbody>
-	<?php
-	for($i = 0; $i < count($intranets); $i++) {
-		?>
-		<tr>
-			<td><a href="intranet.php?id=<?php echo intval($intranets[$i]["id"]); ?>"><?php echo safeToHtml($intranets[$i]["name"]); ?></a></td>
-			<td class="buttons">
-				<a href="intranet_edit.php?id=<?php print($intranets[$i]["id"]); ?>"><?php echo $translation->get('edit', 'common'); ?></a>
-			</td>
-		</tr>
-		<?php
-	}
-	?>
+    <?php
+    for($i = 0; $i < count($intranets); $i++) {
+        ?>
+        <tr>
+            <td><a href="intranet.php?id=<?php echo intval($intranets[$i]["id"]); ?>"><?php echo safeToHtml($intranets[$i]["name"]); ?></a></td>
+            <td class="buttons">
+                <a href="intranet_edit.php?id=<?php print($intranets[$i]["id"]); ?>"><?php echo $translation->get('edit', 'common'); ?></a>
+            </td>
+        </tr>
+        <?php
+    }
+    ?>
 </tbody>
 </table>
 
