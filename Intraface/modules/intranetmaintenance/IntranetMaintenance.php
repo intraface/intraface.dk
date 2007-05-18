a35d0b6b040b93f3af60ed5ebc26d206a57e6d7f
Lars Olesen <lars@intraface.dk>
2007-05-18 02:09:07 +0000

    updated according to coding standards

--- Intraface/modules/intranetmaintenance/IntranetMaintenance.php
+++ Intraface/modules/intranetmaintenance/IntranetMaintenance.php
@@ -11,15 +11,14 @@
  * @author	Sune Jensen <sj@sunet.dk>
  * @author	Lars Olesen <lars@legestue.net>
  *
- * @since	1.1
- * @version	1.2
+ * @version	@package-version@
  *
  */
-
 require_once 'Intraface/Intranet.php';
 require_once 'Intraface/DBQuery.php';
 
-class IntranetMaintenance extends Intranet {
+class IntranetMaintenance extends Intranet
+{
 
     var $db; // databaseobject mdb2
     var $id; // intranet id
@@ -29,7 +28,8 @@ class IntranetMaintenance extends Intranet {
      * Constructor
      */
 
-    function __construct($kernel, $intranet_id = 0) {
+    function __construct($kernel, $intranet_id = 0)
+    {
         if (!is_object($kernel) OR strtolower(get_class($kernel)) != 'kernel') {
             trigger_error('intranetmaintenance needs a kernel', E_USER_ERROR);
         }
@@ -46,11 +46,13 @@ class IntranetMaintenance extends Intranet {
         }
     }
 
-    function createDBQuery() {
+    function createDBQuery()
+    {
         $this->dbquery = new DBQuery($this->kernel, 'intranet');
     }
 
-    function flushAccess() {
+    function flushAccess()
+    {
         if ($this->id == 0) {
             trigger_error('cannot flush access because no id i set', E_USER_ERROR);
         }
@@ -59,7 +61,8 @@ class IntranetMaintenance extends Intranet {
         return 1;
     }
 
-    function setModuleAccess($module_id) {
+    function setModuleAccess($module_id)
+    {
         if ($this->id == 0) {
             trigger_error('cannot set access because no id i set', E_USER_ERROR);
         }
@@ -68,33 +71,32 @@ class IntranetMaintenance extends Intranet {
             $result = $this->db->query("SELECT id FROM module WHERE name='".$module_id."'");
             if ($row = $result->fetchRow(MDB2_FETCHMODE_ASSOC)) {
                 $module_id = $row['id'];
-            }
-            else {
+            } else {
                 trigger_error("intranet maintenance says unknown module", E_USER_ERROR);
             }
 
         }
 
         $res = $this->db->query("SELECT id FROM module WHERE id = ".intval($module_id));
-        if($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+        if ($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
             $this->db->query("INSERT INTO permission SET
                 intranet_id = ".intval($this->id).",
                 user_id = 0,
                 module_id = ".intval($row['id']));
-        }
-        else {
+        } else {
             trigger_error("intranet maintenance says unknown module_id", E_USER_ERROR);
         }
     }
 
-    function validate($input) {
+    function validate($input)
+    {
         $validator = new Validator($this->error);
 
         $validator->isString($input["name"], "Navn skal være en streng", "", "");
-        if($validator->isNumeric($input["maintained_by_user_id"], "Vedligholder er ugyldig", "greater_than_zero")) {
+        if ($validator->isNumeric($input["maintained_by_user_id"], "Vedligholder er ugyldig", "greater_than_zero")) {
             $temp_user = new User($input["maintained_by_user_id"]);
 
-            if(!$temp_user->hasIntranetAccess($this->kernel->intranet->get('id'))) {
+            if (!$temp_user->hasIntranetAccess($this->kernel->intranet->get('id'))) {
                 $this->error->set("Ugyldig bruger som vedligeholder");
             }
         }
@@ -109,8 +111,9 @@ class IntranetMaintenance extends Intranet {
      * This method will only update a few parameters in the intranet.
      */
 
-    function save($input) {
-        if(!is_array($input)) {
+    function save($input)
+    {
+        if (!is_array($input)) {
             trigger_error('input is not an array', E_USER_ERROR);
         }
 
@@ -121,18 +124,18 @@ class IntranetMaintenance extends Intranet {
         $sql = "name = \"".$this->db->escape($input["name"])."\",
             maintained_by_user_id = ".intval($input["maintained_by_user_id"])."";
 
-        if(isset($input["identifier"])) {
+        if (isset($input["identifier"])) {
             $sql .= ", identifier = \"".$this->db->escape($input['identifier'])."\"";
         }
-        if($this->id == 0 || isset($input["generate_private_key"])) {
+        if ($this->id == 0 || isset($input["generate_private_key"])) {
             $sql .= ", private_key = \"".$this->db->escape($this->kernel->randomKey(50))."\"";
         }
 
-        if($this->id == 0 || isset($input["generate_public_key"])) {
+        if ($this->id == 0 || isset($input["generate_public_key"])) {
             $sql .= ", public_key = \"".$this->kernel->randomKey(15)."\"";
         }
 
-        if($this->id == 0) {
+        if ($this->id == 0) {
             $this->db->query("INSERT INTO intranet SET ".$sql.", date_changed = NOW()");
             $this->id = $this->db->lastInsertID('intranet', 'id');
 
@@ -143,21 +146,21 @@ class IntranetMaintenance extends Intranet {
             print('ff'.$this->id.'ff');
 
             $this->load();
-        }
-        else {
+        } else {
             $this->db->query("UPDATE intranet SET ".$sql.", date_changed = NOW() WHERE id = ".intval($this->id));
             $this->load();
         }
         return true;
     }
 
-    function getList() {
+    function getList()
+    {
 
-        if($this->dbquery->checkFilter('text')) {
+        if ($this->dbquery->checkFilter('text')) {
             $this->dbquery->setCondition('name LIKE "%'.safeToHtml($this->dbquery->getFilter('text')).'%"');
         }
 
-        if($this->dbquery->checkFilter('user_id')) {
+        if ($this->dbquery->checkFilter('user_id')) {
             $this->dbquery->setJoin('LEFT', 'permission', 'permission.intranet_id = intranet.id', '');
             $this->dbquery->setCondition('permission.user_id = '.$this->dbquery->getFilter('user_id'));
         }
@@ -167,7 +170,7 @@ class IntranetMaintenance extends Intranet {
         $i = 0;
         $intranet = array();
         $db = $this->dbquery->getRecordset('DISTINCT(intranet.id), intranet.name');
-        while($db->nextRecord()) {
+        while ($db->nextRecord()) {
             $intranet[$i]['id'] = $db->f('id');
             $intranet[$i]['name'] = $db->f('name');
             $i++;
@@ -187,7 +190,8 @@ class IntranetMaintenance extends Intranet {
         return $intranet;
     }
 
-    function setContact($contact_id) {
+    function setContact($contact_id)
+    {
         $this->db->query("UPDATE intranet SET contact_id = ".intval($contact_id)." WHERE id = " . intval($this->id));
         return true;
     }
