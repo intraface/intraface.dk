82db2aa4e2ffb6932cf4f31f68e9a3942247112c
Lars Olesen <lars@intraface.dk>
2007-05-19 23:00:45 +0000

    Started to test the run() method. It seems to work allright. We just need a couple of precise tests. I think in time the whole thing has to be rewritten to consist of several classes each with only one responsibiilty (much easier to test) but used by BasketEvaluation.

--- Intraface/modules/webshop/tests/BasketEvaluationTest.php
+++ Intraface/modules/webshop/tests/BasketEvaluationTest.php
@@ -2,24 +2,34 @@
 require_once 'PHPUnit/Framework.php';
 
 require_once 'Intraface/Standard.php';
+require_once 'Intraface/Kernel.php';
 require_once 'Intraface/modules/webshop/BasketEvaluation.php';
+require_once 'Intraface/modules/webshop/Basket.php';
 
-class FakeEvaluationKernel {
-    public $intranet;
-}
 class FakeEvaluationIntranet {
     function get() {
         return 1;
     }
+    function hasModuleAccess() {
+        return true;
+    }
+}
+
+class FakeEvaluationUser {
+    function hasModuleAccess() { return true; }
+    function get() { return 1; }
+
 }
 class FakeEvaluationWebshop {
     public $kernel;
 }
-class FakeEvaluationBasket {
-    public $webshop;
-}
+
 
 define('DB_DSN', 'mysql://root:@localhost/pear');
+define('PATH_INCLUDE_MODULE', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/modules/');
+define('PATH_INCLUDE_SHARED', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/shared/');
+define('PATH_INCLUDE_CONFIG', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/config/');
+
 
 class BasketEvaluationTest extends PHPUnit_Framework_TestCase
 {
@@ -29,16 +39,23 @@ class BasketEvaluationTest extends PHPUnit_Framework_TestCase
         $this->emptyEvaluationTable();
     }
 
+    function tearDown()
+    {
+        $this->emptyEvaluationTable();
+    }
+
     function emptyEvaluationTable()
     {
         $db = MDB2::factory(DB_DSN);
         $db->query('TRUNCATE webshop_basket_evaluation');
+        $db->query('TRUNCATE basket');
     }
 
     function createKernel()
     {
-        $kernel = new FakeEvaluationKernel;
+        $kernel = new Kernel;
         $kernel->intranet = new FakeEvaluationIntranet;
+        $kernel->user = new FakeEvaluationUser;
         return $kernel;
     }
 
@@ -51,8 +68,15 @@ class BasketEvaluationTest extends PHPUnit_Framework_TestCase
     function createBasket()
     {
         $kernel = $this->createKernel();
-        $webshop = new FakeEvaluationWebshop($kernel);
-        $basket = new FakeEvaluationBasket($webshop);
+        $webshop = new FakeEvaluationWebshop();
+        $webshop->kernel = $kernel;
+        return new Basket($webshop, 'somesessionid');
+
+    }
+
+    function saveEvaluation()
+    {
+
     }
 
     function testCreateBasketEvaluation()
@@ -79,6 +103,63 @@ class BasketEvaluationTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue($evaluation->save($valid_data));
         $this->assertEquals(count($evaluation->getList()), 1);
+    }
+
+    function testDelete() {
+        $evaluation = $this->createBasketEvaluation();
+
+        $valid_data = array(
+            'running_index' => 1,
+            'evaluate_target_key' => 0,
+            'evaluate_method_key' => 2,
+            'evaluate_value' => 1000,
+            'go_to_index_after' => 10000,
+            'action_action_key' => 1,
+            'action_value' => 10,
+            'action_quantity' => 10,
+            'action_unit_key' => 0
+        );
+
+        $this->assertTrue($evaluation->save($valid_data));
+        $this->assertTrue($evaluation->delete());
+        $this->assertEquals(count($evaluation->getList()), 0);
+    }
+
+    function testRun()
+    {
+        $evaluation = $this->createBasketEvaluation();
+
+        $basket = $this->createBasket();
+
+        // setting up products
+        $basket->webshop->kernel->module('product');
+        $product = new Product($basket->webshop->kernel);
+        $product_id = $product->save(array('name' => 'Test', 'price' => 2000));
+
+        $product = new Product($basket->webshop->kernel);
+        $filter_product_id = $product->save(array('name' => 'Filterproduct', 'price' => -1));
+
+        $valid_data = array(
+            'running_index' => 1,
+            'evaluate_target_key' => 0,
+            'evaluate_method_key' => 2,
+            'evaluate_value' => 1000,
+            'go_to_index_after' => 10000,
+            'action_action_key' => 1,
+            'action_value' => $filter_product_id,
+            'action_quantity' => 10,
+            'action_unit_key' => 0
+        );
+
+        $this->assertTrue($evaluation->save($valid_data));
+
+        $quantity = 2;
+        $basket->add($product_id, $quantity);
+
+        $this->assertTrue($evaluation->run($basket));
+
+        print_r($basket->getItems());
+
     }
 }
 ?>
