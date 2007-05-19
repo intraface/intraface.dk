9535058f65a194b85b90ed81e5173133239cac80
Sune T. Jensen <sj@sunet.dk>
2007-05-19 00:36:22 +0000

    added webshop_basket_filter

--- intraface.dk/install/database-update.sql
+++ intraface.dk/install/database-update.sql
@@ -17,3 +17,19 @@ PRIMARY KEY ( `id` )
 ALTER TABLE `contact_reminder_single` ADD `created_by_user_id` INT NOT NULL AFTER `contact_id` ;
 
 ALTER TABLE `debtor` ADD `internal_note` TEXT NOT NULL AFTER `message` ;
+
+
+CREATE TABLE `webshop_basket_filter` (
+`id` INT NOT NULL AUTO_INCREMENT ,
+`intranet_id` INT NOT NULL ,
+`filter_index` INT NOT NULL ,
+`evaluate_key` INT NOT NULL ,
+`evaluate_method` INT NOT NULL ,
+`evaluate_value` VARCHAR( 255 ) NOT NULL ,
+`exit_after` INT NOT NULL ,
+`action_key` INT NOT NULL ,
+`action_value` VARCHAR( 255 ) NOT NULL ,
+`action_amount` INT NOT NULL ,
+`action_unit` INT NOT NULL ,
+PRIMARY KEY ( `id` )
+) TYPE = MYISAM ;
