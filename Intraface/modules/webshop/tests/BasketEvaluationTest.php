bd539cb75da8154e076e57d2341d7e0a3703eedc
Lars Olesen <lars@intraface.dk>
2007-05-19 18:37:06 +0000

    Added tests for webshop

--- Intraface/modules/webshop/tests/BasketEvaluationTest.php
+++ Intraface/modules/webshop/tests/BasketEvaluationTest.php
@@ -4,40 +4,81 @@ require_once 'PHPUnit/Framework.php';
 require_once 'Intraface/Standard.php';
 require_once 'Intraface/modules/webshop/BasketEvaluation.php';
 
-class FakeKernel {
+class FakeEvaluationKernel {
     public $intranet;
 }
-class FakeIntranet {
+class FakeEvaluationIntranet {
     function get() {
         return 1;
     }
 }
-class FakeWebshop {
+class FakeEvaluationWebshop {
     public $kernel;
 }
-class FakeBasket {
+class FakeEvaluationBasket {
     public $webshop;
 }
 
+define('DB_DSN', 'mysql://root:@localhost/pear');
 
 class BasketEvaluationTest extends PHPUnit_Framework_TestCase
 {
+
+    function setUp()
+    {
+        $this->emptyEvaluationTable();
+    }
+
+    function emptyEvaluationTable()
+    {
+        $db = MDB2::factory(DB_DSN);
+        $db->query('TRUNCATE webshop_basket_evaluation');
+    }
+
+    function createKernel()
+    {
+        $kernel = new FakeEvaluationKernel;
+        $kernel->intranet = new FakeEvaluationIntranet;
+        return $kernel;
+    }
+
     function createBasketEvaluation()
     {
-        $kernel = new FakeKernel;
-        $kernel->intranet = new FakeIntranet;
+        $kernel = $this->createKernel();
+        return new BasketEvaluation($kernel);
+    }
 
-        $webshop = new FakeWebshop($kernel);
-
-        $basket = new FakeBasket($webshop);
-
-        return new BasketEvaluation($basket);
+    function createBasket()
+    {
+        $kernel = $this->createKernel();
+        $webshop = new FakeEvaluationWebshop($kernel);
+        $basket = new FakeEvaluationBasket($webshop);
     }
 
     function testCreateBasketEvaluation()
     {
         $evaluation = $this->createBasketEvaluation();
         $this->assertTrue(is_object($evaluation));
+    }
+
+    function testSaveWithValidValuesSucceeds()
+    {
+        $evaluation = $this->createBasketEvaluation();
+
+        $valid_data = array(
+            'running_index' => 1,
+            'evaluate_target_key' => 0,
+            'evaluate_method_key' => 2,
+            'evaluate_value' => 1000,
+            'go_to_index_after' => 10000,
+            'action_action_key' => 1,
+            'action_value' => 10,
+            'action_quantity' => 10,
+            'action_unit_key' => 0
+        );
+
+        $this->assertTrue($evaluation->save($valid_data));
+        $this->assertEquals(count($evaluation->getList()), 1);
     }
 }
 ?>
---
+++ Intraface/modules/webshop/tests/BasketTest.php
@@ -0,0 +1,79 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+
+require_once 'Intraface/Standard.php';
+require_once 'Intraface/Kernel.php';
+require_once 'Intraface/modules/webshop/Basket.php';
+
+class FakeKernel {
+    public $intranet;
+    function useModule()
+    {
+        return true;
+    }
+    function useShared()
+    {
+        return true;
+    }
+}
+class FakeIntranet {
+    function get() {
+        return 1;
+    }
+    function hasModuleAccess()
+    {
+        return true;
+    }
+}
+class FakeWebshop {
+    public $kernel;
+}
+
+
+define('DB_DSN', 'mysql://root:@localhost/pear');
+define('PATH_INCLUDE_MODULE', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/modules/');
+define('PATH_INCLUDE_SHARED', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/shared/');
+define('PATH_INCLUDE_CONFIG', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/config/');
+
+class BasketTest extends PHPUnit_Framework_TestCase
+{
+
+    function setUp()
+    {
+        $this->emptyBasketTable();
+    }
+
+    function emptyBasketTable()
+    {
+        $db = MDB2::factory(DB_DSN);
+        $db->query('TRUNCATE basket');
+    }
+
+    function createBasket()
+    {
+        $kernel = new Kernel;
+        $kernel->intranet = new FakeIntranet;
+        $webshop = new FakeWebshop();
+        $webshop->kernel = $kernel;
+        $basket = new Basket($webshop, 'somesessionid');
+        return $basket;
+    }
+
+    function testCreateBasket()
+    {
+        $basket = $this->createBasket();
+        $this->assertTrue(is_object($basket));
+    }
+
+    function testAddToBasket()
+    {
+        $basket = $this->createBasket();
+
+        $product_id = 1;
+        $quantity = 1;
+
+        $this->assertTrue($basket->add($product_id, $quantity));
+    }
+
+}
+?>
