9d8c39adb74510545a1a6ce9cf4e016c6bbb4237
Lars Olesen <lars@intraface.dk>
2007-05-20 15:27:25 +0000

    Added frontpage to servers.

---
+++ intraface.dk/xmlrpc/index.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><a href="cms/server2.php">CMS</a></li>
+    <li><a href="contact/server.php">Contact</a></li>
+    <li><a href="debtor/server.php">Debtor</a></li>
+    <li><a href="newsletter/server.php">Newsletter</a></li>
+    <li><a href="shop/server3.php">Shop</a></li>
+</ul>
