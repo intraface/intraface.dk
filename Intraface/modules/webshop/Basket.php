915e38111cee13d3ed3822abfb8fdeb3071dfb73
Lars Olesen <lars@intraface.dk>
2007-05-20 21:08:45 +0000

    corrected link to db_sql to be case sensitive

--- Intraface/modules/webshop/Basket.php
+++ Intraface/modules/webshop/Basket.php
@@ -9,7 +9,7 @@
  */
 
 require_once 'MDB2.php';
-require_once 'Intraface/3Party/Database/DB_sql.php';
+require_once 'Intraface/3Party/Database/Db_sql.php';
 require_once 'Intraface/functions/functions.php';
 require_once 'Intraface/modules/product/Product.php';
 
