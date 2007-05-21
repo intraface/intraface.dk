dadb9cfe312562d040b9857be770ae1bf517da74
Lars Olesen <lars@intraface.dk>
2007-05-21 14:24:36 +0000

    Updated path in hack. Make sure hack goes away.

--- Intraface/modules/accounting/Year.php
+++ Intraface/modules/accounting/Year.php
@@ -447,7 +447,7 @@ class Year extends Standard {
                     // include($module_accounting->includeFile('standardaccounts.php')); // 'intraface_modules/accounting/standardaccounts.php');
                     // HACK
 
-                    require(PATH_ROOT . 'intraface/modules/accounting/standardaccounts.php');
+                    require(PATH_ROOT . 'pear/php/Intraface/modules/accounting/standardaccounts.php');
 
                     if (empty($standardaccounts)) {
                         return 0;
