01a7123383d6818f35b85e1594d14b883baf3a49
Lars Olesen <lars@intraface.dk>
2007-06-19 05:46:00 +0000

    Made sure that common knows it has to be in same directory.

--- Intraface/include_first.php
+++ Intraface/include_first.php
@@ -1,5 +1,5 @@
 <?php
-require 'common.php';
+require dirname(__FILE__) . '/common.php';
 
 ob_start(); // ob_gzhandler()
 session_start();
@@ -9,14 +9,14 @@ require_once 'Intraface/Auth.php';
 $auth = new Auth(session_id());
 
 if (!$user_id = $auth->isLoggedIn()) {
-	$auth->toLogin();
+    $auth->toLogin();
 }
 
 $kernel = new Kernel;
 $kernel->user = new User($user_id);
 
 if (!$intranet_id = $kernel->user->getActiveIntranetId('id')) {
-	trigger_error('no active intranet_id', E_USER_ERROR);
+    trigger_error('no active intranet_id', E_USER_ERROR);
 }
 
 $kernel->intranet = new Intranet($intranet_id);
@@ -29,20 +29,20 @@ $language = $kernel->setting->get('user', 'language');
 
 // set the parameters to connect to your db
 $dbinfo = array(
-	'hostspec' => DB_HOST,
-	'database' => DB_NAME,
-	'phptype'  => 'mysql',
-	'username' => DB_USER,
-	'password' => DB_PASS
+    'hostspec' => DB_HOST,
+    'database' => DB_NAME,
+    'phptype'  => 'mysql',
+    'username' => DB_USER,
+    'password' => DB_PASS
 );
 
 if (!defined('LANGUAGE_TABLE_PREFIX')) {
-	define('LANGUAGE_TABLE_PREFIX', 'core_translation_');
+    define('LANGUAGE_TABLE_PREFIX', 'core_translation_');
 }
 
 $params = array(
-	'langs_avail_table' => LANGUAGE_TABLE_PREFIX.'langs',
-	'strings_default_table' => LANGUAGE_TABLE_PREFIX.'i18n'
+    'langs_avail_table' => LANGUAGE_TABLE_PREFIX.'langs',
+    'strings_default_table' => LANGUAGE_TABLE_PREFIX.'i18n'
 );
 
 require_once 'Translation2.php';
@@ -51,14 +51,14 @@ $translation = Translation2::factory('MDB2', $dbinfo, $params);
 //always check for errors. In this examples, error checking is omitted
 //to make the example concise.
 if (PEAR::isError($translation)) {
-	trigger_error('Could not start Translation ' . $translation->getMessage(), E_USER_ERROR);
+    trigger_error('Could not start Translation ' . $translation->getMessage(), E_USER_ERROR);
 }
 
 // set primary language
 $set_language = $translation->setLang($language);
 
 if (PEAR::isError($set_language)) {
-	trigger_error($set_language->getMessage(), E_USER_ERROR);
+    trigger_error($set_language->getMessage(), E_USER_ERROR);
 }
 
 // set the group of strings you want to fetch from
