51086fe9154da1d285d23af6fa1cf3b480004f3e
Lars Olesen <lars@intraface.dk>
2007-06-24 18:50:33 +0000

    Added getKeywords method on server.

--- Intraface/XMLRPC/Contact/Server.php
+++ Intraface/XMLRPC/Contact/Server.php
@@ -18,218 +18,223 @@ require_once 'MDB2.php';
 
 class Intraface_XMLRPC_Contact {
 
-	private $credentials;
-	//private $kernel;
+    private $credentials;
+    //private $kernel;
 
-	//function __construct() {
-	function __construct($kernel = '') {
-		//$this->kernel = $kernel;
-	}
+    //function __construct() {
+    function __construct($kernel = '') {
+        //$this->kernel = $kernel;
+    }
 
-	/**
-	 * Gets a contact
-	 *
-	 * @param  struct  $credentials
-	 * @param  integer $id
-	 * @return array
-	 */
-	function getContact($credentials, $id) {
+    /**
+     * Gets a contact
+     *
+     * @param  struct  $credentials
+     * @param  integer $id
+     * @return array
+     */
+    function getContact($credentials, $id) {
 
-		$this->checkCredentials($credentials);
+        $this->checkCredentials($credentials);
 
-		$contact = new Contact($this->kernel, $id);
-		if (!$contact->get('id') > 0) { // -4
-			throw new XML_RPC2_FaultException('contact does not exist', -4);
-		}
-		$contact_info = array_merge($contact->get(), $contact->address->get());
-		$contact_info['id'] = $contact->get('id');
-		if (!$contact_info) {
-			return array();
-		}
+        $contact = new Contact($this->kernel, $id);
+        if (!$contact->get('id') > 0) { // -4
+            throw new XML_RPC2_FaultException('contact does not exist', -4);
+        }
+        $contact_info = array_merge($contact->get(), $contact->address->get());
+        $contact_info['id'] = $contact->get('id');
+        if (!$contact_info) {
+            return array();
+        }
 
-		return $contact_info;
-	}
+        return $contact_info;
+    }
 
-	/**
-	 * Authenticates a contact
-	 *
-	 * @param  struct  $credentials
-	 * @param  string  $contact_key
-	 * @return array
-	 */
-	function authenticateContact($credentials, $contact_key) {
-		$this->checkCredentials($credentials);
+    /**
+     * Authenticates a contact
+     *
+     * @param  struct  $credentials
+     * @param  string  $contact_key
+     * @return array
+     */
+    function authenticateContact($credentials, $contact_key) {
+        $this->checkCredentials($credentials);
 
-		$contact = Contact::factory($this->kernel, 'code', $contact_key);
-		if (!is_object($contact) OR !$contact->get('id') > 0) {
-			return false;
-		}
+        $contact = Contact::factory($this->kernel, 'code', $contact_key);
+        if (!is_object($contact) OR !$contact->get('id') > 0) {
+            return false;
+        }
 
-		$contact_info = array_merge($contact->get(), $contact->address->get());
-		$contact_info['id'] = $contact->get('id');
+        $contact_info = array_merge($contact->get(), $contact->address->get());
+        $contact_info['id'] = $contact->get('id');
 
-		if (!$contact_info) {
-			return array();
-		}
+        if (!$contact_info) {
+            return array();
+        }
 
-		return $contact_info;
-	}
+        return $contact_info;
+    }
 
-	/**
-	 * Saves a contact
-	 *
-	 * @param  struct $credentials
-	 * @param  array  $input (remember to include id key)
-	 * @return boolean
-	 */
-	function saveContact($credentials, $input) {
-		$this->checkCredentials($credentials);
+    /**
+     * Saves a contact
+     *
+     * @param  struct $credentials
+     * @param  array  $input (remember to include id key)
+     * @return boolean
+     */
+    function saveContact($credentials, $input) {
+        $this->checkCredentials($credentials);
 
-		if (!is_array($input)) { // -5
-			throw new XML_RPC2_FaultException('input is no an array', -5);
-		}
+        if (!is_array($input)) { // -5
+            throw new XML_RPC2_FaultException('input is no an array', -5);
+        }
 
-		$values = $input;
+        $values = $input;
 
-		$contact = new Contact($this->kernel, $values['id']);
+        $contact = new Contact($this->kernel, $values['id']);
 
-		if (!$contact->save($values)) {
-			$contact->error->view(); // -6
-			throw new XML_RPC2_FaultException('could not update contact', -6);
-		}
+        if (!$contact->save($values)) {
+            $contact->error->view(); // -6
+            throw new XML_RPC2_FaultException('could not update contact', -6);
+        }
 
-		return true;
-	}
-	/*
-	function sendLoginEmail($arg) {
-		if (count($arg) != 2) {
-			return new IXR_Error(-4, 'Der er ikke det rigtige antal argumenter til sendLoginEmail()');
-		}
+        return true;
+    }
+    /*
+    function sendLoginEmail($arg) {
+        if (count($arg) != 2) {
+            return new IXR_Error(-4, 'Der er ikke det rigtige antal argumenter til sendLoginEmail()');
+        }
 
-		if (is_object($return = $this->checkCredentials($arg[0]))) {
-			return $return;
-		}
+        if (is_object($return = $this->checkCredentials($arg[0]))) {
+            return $return;
+        }
 
-		if (!is_numeric($arg[1])) {
-			return new IXR_Error(-5, 'Det andet parameter er ikke et numerisk');
-		}
+        if (!is_numeric($arg[1])) {
+            return new IXR_Error(-5, 'Det andet parameter er ikke et numerisk');
+        }
 
-		$id = $arg[1];
+        $id = $arg[1];
 
-		$contact = new Contact($this->kernel, $id);
+        $contact = new Contact($this->kernel, $id);
 
-		if (!is_object($contact) OR !$contact->get('id') > 0) {
-			return new IXR_Error(-5, 'Kontakten fandtes ikke ' .$arg[1]);
-		}
+        if (!is_object($contact) OR !$contact->get('id') > 0) {
+            return new IXR_Error(-5, 'Kontakten fandtes ikke ' .$arg[1]);
+        }
 
-		if (!$contact->sendLoginEmail()) {
-			return new IXR_Error(-6, 'Du kunne ikke sende email ' .$arg[1]);
-		}
+        if (!$contact->sendLoginEmail()) {
+            return new IXR_Error(-6, 'Du kunne ikke sende email ' .$arg[1]);
+        }
 
-		return 1;
-	}
-
-
-	function getKeywords($arg) {
-		$credentials = $arg;
-		if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
-		$contact = new Contact($this->kernel);
-		$contact->getKeywords();
-
-		return $contact->keywords->getAllKeywords();
-	}
+        return 1;
+    }
 
 
-	function getConnectedKeywords($arg) {
-		if (count($arg) != 2) {
-			return new IXR_Error(-4, 'Der er ikke det rigtige antal argumenter til get()');
-		}
+    function getKeywords($arg) {
+        $credentials = $arg;
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
+        $contact = new Contact($this->kernel);
+        $contact->getKeywords();
 
-		if (is_object($return = $this->checkCredentials($arg[0]))) {
-			return $return;
-		}
+        return $contact->keywords->getAllKeywords();
+    }
+    */
 
-		$contact = new Contact($this->kernel, $arg[1]);
-		if (!$contact->get('id') > 0) {
-			return 0;
-		}
-		$contact->getKeywords();
-		$keywords = array();
-		$keywords = $contact->keywords->getConnectedKeywords();
-		return $keywords;
-	}
-	*/
+    /**
+     * Gets intranet permissions to use with a menu
+     *
+     * @param struct  $credentials Credentials provided by intraface
+     * @param integer $contact_id  Contact id
+     *
+     * @return array Keywords
+     */
+     function getConnectedKeywords($credentials, $contact_id) {
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-	/**
-	 * Gets intranet permissions to use with a menu
-	 *
-	 * Might be deprecated in the future so use with caution
-	 *
-	 * @param  struct $credentials
-	 * @return array
-	 */
-	function getIntranetPermissions($credentials) {
-		if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+        $contact = new Contact($this->kernel, $contact_id);
+        if (!$contact->get('id') > 0) {
+            return 0;
+        }
+        $contact->getKeywords();
+        $keywords = array();
+        $keywords = $contact->keywords->getConnectedKeywords();
+        return $keywords;
+    }
 
-		$permissions = array();
 
-		if ($this->kernel->intranet->hasModuleAccess('newsletter')) {
-			$permissions[] = 'newsletter';
-		}
+    /**
+     * Gets intranet permissions to use with a menu
+     *
+     * Might be deprecated in the future so use with caution
+     *
+     * @param  struct $credentials
+     *
+     * @return array
+     */
+    public function getIntranetPermissions($credentials) {
+        if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-		if ($this->kernel->intranet->hasModuleAccess('todo')) {
-			$permissions[] = 'todo';
-		}
+        $permissions = array();
 
-		if ($this->kernel->intranet->hasModuleAccess('debtor')) {
-			$permissions[] = 'debtor';
-		}
+        if ($this->kernel->intranet->hasModuleAccess('newsletter')) {
+            $permissions[] = 'newsletter';
+        }
 
-		return $permissions;
-	}
+        if ($this->kernel->intranet->hasModuleAccess('todo')) {
+            $permissions[] = 'todo';
+        }
 
-	/**
-	 * Checking credentials
-	 *
-	 * @param struct $credentials
-	 * @return array
-	 */
-	function checkCredentials($credentials) {
-		/*
-		if (is_object($this->kernel) AND is_object($this->kernel->intranet)) {
-			return true;
-		}
-		*/
+        if ($this->kernel->intranet->hasModuleAccess('debtor')) {
+            $permissions[] = 'debtor';
+        }
 
-		if (count($credentials) != 2) { // -4
-			throw new XML_RPC2_FaultException('wrong argument count in $credentials - got ' . count($credentials) . ' arguments - need 2', -4);
-		}
-		if (empty($credentials['private_key'])) { // -5
-			throw new XML_RPC2_FaultException('supply a private_key', -5);
-		}
-		if (empty($credentials['session_id'])) { // -5
-			throw new XML_RPC2_FaultException('supply a session_id', -5);
-		}
+        return $permissions;
+    }
 
-		$weblogin = new Weblogin('some session');
-		if (!$intranet_id = $weblogin->auth('private', $credentials['private_key'], $credentials['session_id'])) {
-			throw new XML_RPC2_FaultException('contact says access to intranet - please supply a valid private key', -2);
-		}
+    /**
+     * Checking credentials
+     *
+     * @param struct $credentials
+     * @return array
+     */
+    function checkCredentials($credentials) {
+        /*
+        if (is_object($this->kernel) AND is_object($this->kernel->intranet)) {
+            return true;
+        }
+        */
 
-		$this->kernel = new Kernel();
-		$this->kernel->intranet = new Intranet($intranet_id);
-		$this->kernel->setting = new Setting($this->kernel->intranet->get('id'));
+        if (count($credentials) != 2) { // -4
+            throw new XML_RPC2_FaultException('wrong argument count in $credentials - got ' . count($credentials) . ' arguments - need 2', -4);
+        }
+        if (empty($credentials['private_key'])) { // -5
+            throw new XML_RPC2_FaultException('supply a private_key', -5);
+        }
+        if (empty($credentials['session_id'])) { // -5
+            throw new XML_RPC2_FaultException('supply a session_id', -5);
+        }
 
-		if (!is_object($this->kernel->intranet)) { // -2
-			throw new XML_RPC2_FaultException('could not create intranet', -2);
-		}
+        $weblogin = new Weblogin('some session');
+        if (!$intranet_id = $weblogin->auth('private', $credentials['private_key'], $credentials['session_id'])) {
+            throw new XML_RPC2_FaultException('contact says access to intranet - please supply a valid private key', -2);
+        }
 
-		return true;
-	}
+        $this->kernel = new Kernel();
+        $this->kernel->intranet = new Intranet($intranet_id);
+        $this->kernel->setting = new Setting($this->kernel->intranet->get('id'));
+
+        if (!is_object($this->kernel->intranet)) { // -2
+            throw new XML_RPC2_FaultException('could not create intranet', -2);
+        }
+
+        return true;
+    }
 
 }
 ?>
