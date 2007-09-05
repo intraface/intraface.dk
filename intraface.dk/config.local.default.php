c8a5d158a659c5e5bba41dcb0cce2bac77c86530
Sune T. Jensen <sj@sunet.dk>
2007-09-05 20:00:47 +0000

    Activated errorHandler again and changed a little in the way the decorator logs the errors

--- intraface.dk/config.local.default.php
+++ intraface.dk/config.local.default.php
@@ -29,11 +29,10 @@ define('CONNECTION_INTERNET', true); // if the system has access to dns and more
 define('SERVER_STATUS', 'TEST'); // if the system is in PRODUCTION or TEST mode
 
 // error log
-define('ERROR_REPORT_EMAIL', '');
+define('ERROR_HANDLE_LEVEL', E_ALL); //  which levels should error_handler take care of: E_ALL 
+define('ERROR_LEVEL_CONTINUE_SCRIPT', 0); // Which level should the script continue executing. Development: 0, Production: E_USER_NOTICE ^ E_NOTICE
+define('ERROR_REPORT_EMAIL', ''); // if you want to recieve an e-mail on every error.
 define('ERROR_LOG', PATH_ROOT.'intraface/log/error.log'); // exact directory and filename
 define('ERROR_LOG_UNIQUE', PATH_ROOT.'intraface/log/unique-error.log'); // exact directory and filename
-define('ERROR_DISPLAY_USER', true);
-define('ERROR_DISPLAY', true);
-define('ERROR_HANDLE_LEVEL', E_ALL);
-define('ERROR_LEVEL_CONTINUE_SCRIPT', 10);
+define('ERROR_USE_BLUESCREEN', true); // use the errorhandler bluescreen. Development: true, Production: false
 ?>
