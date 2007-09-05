70b0b3099cc8fe3984d4630cf52f00eb9eaf2be9
Sune T. Jensen <sj@sunet.dk>
2007-09-05 17:55:58 +0000

    New error reader

---
+++ tools.intraface.dk/error/index.php
@@ -0,0 +1,27 @@
+<?php
+require('../include_first.php');
+
+
+
+if (!empty($_GET['action']) AND $_GET['action'] == 'deletelog') {
+	
+}
+
+
+
+$items[] = array(
+            'title' => 'Delete log when finished',
+            'description' => 'When you have corrected errors, you have to delete the log.',
+            'pubDate' => date('r'),
+            'link' => htmlspecialchars($_SERVER['PHP_SELF']).'?action=deletelog',
+            'author' => 'Intraface.dk'
+        );
+
+echo '<html><body>';
+
+foreach($rss_items AS $item) {
+	echo '<p><strong>'.$item['title'].'</strong> '.$item['description'].'<br />'.$item['pubDate'].': <em><a href="'.$item['link'].'">'.$item['link'].'</a></em>';	
+}
+
+echo '</body></html>';
+?>
