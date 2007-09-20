d74dd5f921b89e72a7928c4532123d3035efcb38
Lars Olesen <lars@intraface.dk>
2007-09-20 14:36:22 +0000

    Made a test for Webshop and splitted placeOrder up in smaller methods.

--- Intraface/modules/webshop/Webshop.php
+++ Intraface/modules/webshop/Webshop.php
@@ -18,19 +18,24 @@
  * @see Contact
  */
 
-class Webshop {
+class Webshop
+{
     var $kernel;
     var $basket;
     var $order;
     var $contact;
     var $error;
+    private $session_id;
 
     /**
      * Construktor
-     * @param $kernel (object) Kernel
-     * @param $session_id (varchar)
+     *
+     * @param object $kernel     Kernel object
+     * @param string $session_id Unikt session id
+     *
+     * @return void
      */
-    function __construct($kernel, $session_id)
+    public function __construct($kernel, $session_id)
     {
         if (!is_object($kernel) OR strtolower(get_class($kernel)) != 'kernel') {
             trigger_error('Webshopmodulet har brug for Kernel', E_USER_ERROR);
@@ -42,86 +47,119 @@ class Webshop {
         $this->kernel->useModule('product');
         $this->kernel->useModule('contact');
 
-        $this->basket = new Basket($this, $session_id);
+        $this->session_id = $session_id;
+        $this->basket = $this->createBasket();
 
         $this->error = new Error;
     }
 
     /**
-     * placeOrder()
-     * Funktion til at placere ordren
+     * Convenience method to create the basket
      *
-     * @see Contact
-     * @param $input (array) $array med kundedata
-     * @return boelean
-   */
-    function placeOrder($input)
+     * @return object
+     */
+    private function createBasket()
     {
+        return new Basket($this, $this->session_id);
+    }
 
+    /**
+     * Gets the basket
+     *
+     * @return object
+     */
+    public function getBasket()
+    {
+        return $this->basket;
+    }
+
+    /**
+     * Places the order and utilizes basket
+     *
+     * @param array $input Array with customer data
+     *
+     * @return integer Order id
+     */
+    public function placeOrder($input)
+    {
         // kunne være vakst om denne tjekkede om kontaktpersonen allerede findes
-
         if (!empty($input['contact_id'])) {
             $this->contact = new Contact($this->kernel, $input['contact_id']);
             $value['contact_id'] = $this->contact->get('id');
             $value['contact_address_id'] = $this->contact->address->get('id');
             $value['contact_person_id'] = 0;
-        }
-        else {
+        } else {
             $this->contact = new Contact($this->kernel);
 
-            # sørger for at tjekke om det er et firma
+            // sørger for at tjekke om det er et firma
             if (!empty($input['contactperson'])) {
                 $input['type'] = 1; // firma
             }
-            
+
             // sets preffered invoice to email. Should be a setting in webshop.
             $input['preferred_invoice'] = 2;
 
-            # opdaterer kontakten
+            // opdaterer kontakten
             if (!$contact_id = $this->contact->save($input)) {
                 $this->error->message = array_merge($this->error->message, $this->contact->error->message);
-                return 0;
+                return false;
             }
+            $contact_person_id = 0;
 
             if ($input['type'] == 1) { // firma
                 $contactperson = new ContactPerson($this->contact);
                 if (!$contact_person_id = $contactperson->save(array('name'=>$input['contactperson']))) {
                     $this->error->message = array_merge($this->error->message, $contactperson->error->message);
-                    return 0;
+                    return false;
                 }
             }
             $value['contact_id'] = $contact_id;
             $value['contact_address_id'] = $this->contact->address->get('id');
             $value['contact_person_id'] = $contact_person_id;
-
         }
-
 
         $value['this_date'] = date('d-m-Y');
         $value['due_date'] = date('d-m-Y');
         $value['description'] = $input['description'];
-        $value['internal_note'] = $input['internal_note'];      
+        $value['internal_note'] = $input['internal_note'];
         $value['message'] = $input['message'];
-        
+
         if(isset($input['customer_coupon']) && $input['customer_coupon'] != '') {
             if($value['message'] != '') $value['message'] .= "\n\n";
             $value['message'] .= "Kundekupon:". $input['customer_coupon'];
         }
-        
+
         if(isset($input['customer_comment']) && $input['customer_comment'] != '') {
             if($value['message'] != '') $value['message'] .= "\n\n";
             $value['message'] .= "Kommentar:\n". $input['customer_comment'];
         }
-        
-        
 
         $this->order = new Debtor($this->kernel, 'order');
         if (!$order_id = $this->order->update($value, 'webshop')) {
             $this->error->message = array_merge($this->error->message, $this->order->error->message);
-            return 0;
+            return false;
         }
 
         $products = $this->basket->getItems();
+
+        $this->addOrderLines($products);
+
+        $this->basket->reset();
+
+        $this->sendEmail($order_id);
+
+        return $order_id;
+    }
+
+    /**
+     * Adds the order lines
+     *
+     * @param array $products
+     *
+     * @return boolean
+     */
+    private function addOrderLines($products = array())
+    {
         foreach ($products AS $product) {
             $this->order->loadItem();
             $value['product_id'] = $product['id'];
@@ -129,29 +167,50 @@ class Webshop {
             $value['description'] = $product['text'];
             $this->order->item->save($value);
         }
-        $this->basket->reset();
 
-        $email = new Email($this->kernel);
-        if (!$email->save(array(
-            'contact_id' => $this->contact->get('id'),
-            'subject' => 'Bekræftelse på bestilling #' . $order_id,
-            'body' => $this->kernel->setting->get('intranet', 'webshop.confirmation_text') . "\n" . $this->contact->getLoginUrl() . "\n\nVenlig hilsen\n" . $this->kernel->intranet->address->get('name'),
-            'from_email' => $this->kernel->intranet->address->get('email'),
-            'from_name' => $this->kernel->intranet->address->get('name'),
-            'type_id' => 12, // webshop
-            'belong_to' => $order_id
-        ))) {
-            $email->error->view();
-        }
-        if(!$email->send()) {
-            $email->error->view();
-        }
-
-        return $order_id;
+        return true;
     }
-    
 
-    function addOnlinePayment($order_id, $transaction_number, $transaction_status, $amount)
+    /**
+     * Sends the email
+     *
+     * @param integer $order_id
+     *
+     * @return boolean
+     */
+    private function sendEmail($order_id)
+    {
+        $this->kernel->useShared('email');
+        $email = new Email($this->kernel);
+
+        if (!$email->save(array('contact_id' => $this->contact->get('id'),
+                                'subject' => 'Bekræftelse på bestilling #' . $order_id,
+                                'body' => $this->kernel->setting->get('intranet', 'webshop.confirmation_text') . "\n" . $this->contact->getLoginUrl() . "\n\nVenlig hilsen\n" . $this->kernel->intranet->address->get('name'),
+                                'from_email' => $this->kernel->intranet->address->get('email'),
+                                'from_name' => $this->kernel->intranet->address->get('name'),
+                                'type_id' => 12, // webshop
+                                'belong_to' => $order_id))) {
+            echo $email->error->view();
+        }
+
+        if(!$email->send()) {
+            echo $email->error->view();
+        }
+
+        return true;
+    }
+
+    /**
+     * Places the order and utilizes basket
+     *
+     * @param integer $order_id
+     * @param integer $transaction_number
+     * @param integer $transaction_status
+     * @param float   $transaction_amount
+     *
+     * @return boolean
+     */
+    public function addOnlinePayment($order_id, $transaction_number, $transaction_status, $amount)
     {
         if ($order_id == 0) {
             return 0;
@@ -169,14 +228,11 @@ class Webshop {
         $this->kernel->useModule('onlinepayment');
         $onlinepayment = new OnlinePayment($this->kernel);
 
-        $values = array(
-            'belong_to' => 'order',
-            'belong_to_id' => (int)$order_id,
-            'transaction_number' => $transaction_number,
-             'transaction_status' => $transaction_status,
-            'amount' => $amount
-
-        );
+        $values = array('belong_to'          => 'order',
+                        'belong_to_id'       => (int)$order_id,
+                        'transaction_number' => $transaction_number,
+                        'transaction_status' => $transaction_status,
+                        'amount'             => $amount);
 
         if ($payment_id = $onlinepayment->save($values)) {
             return $payment_id;
@@ -185,19 +241,25 @@ class Webshop {
             return 0;
         }
     }
-    
+
     /**
      * Returns receipt text
-     * 
+     *
      * @return array with receipt
      */
-    
-    function getReceiptText()
+    public function getReceiptText()
     {
         return array('receipt_text' => $this->kernel->setting->get('intranet','webshop.webshop_receipt'));
     }
 
-    function setSent($order_id)
+    /**
+     * Sets the order as sent
+     *
+     * @param integer $order_id
+     *
+     * @return boolean
+     */
+    public function setSent($order_id)
     {
         $order_id = (int)$order_id;
         if ($order_id == 0) {
@@ -205,7 +267,6 @@ class Webshop {
         }
         $debtor = Debtor::factory($order_id);
         return $debtor->setSent();
-
     }
 }
 ?>
--- tests/unit/webshop/WebshopTest.php
+++ tests/unit/webshop/WebshopTest.php
@@ -1,24 +1,78 @@
 <?php
+require_once dirname(__FILE__) . '/../config.test.php';
+
 require_once 'PHPUnit/Framework.php';
 
 require_once 'Intraface/Standard.php';
+require_once 'Intraface/tools/Date.php';
 require_once 'Intraface/Kernel.php';
+require_once 'Intraface/modules/webshop/Webshop.php';
 require_once 'Intraface/modules/webshop/Basket.php';
 require_once 'Intraface/modules/product/ProductDetail.php';
 
-define('DB_DSN', 'mysql://root:@localhost/pear');
-define('PATH_INCLUDE_MODULE', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/modules/');
-define('PATH_INCLUDE_SHARED', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/shared/');
-define('PATH_INCLUDE_CONFIG', 'c:/Users/Lars Olesen/workspace/intraface/Intraface/config/');
+error_reporting(E_ALL);
+
+class FakeWebshopIntranet
+{
+    public $address;
+    function __construct() {
+        $this->address = new FakeWebshopAddress;
+    }
+    function hasModuleAccess() { return true; }
+    function get() { return '1'; }
+}
+
+class FakeWebshopWeblogin {
+    function get() { return 1; }
+}
+
+class FakeWebshopAddress
+{
+    function get($key = '') { if ($key == 'email') return 'lars@legestue.net'; else return 1; }
+}
+
+class FakeWebshopSetting {
+    function get() { return 1; }
+}
 
 class WebshopTest extends PHPUnit_Framework_TestCase
 {
-    function testPlaceOrderResetsBasket()
+    private $webshop;
+
+    function setUp()
     {
-        // TODO needs to be updated
-        $this->markTestIncomplete('Needs a test for this');
+        $kernel = new Kernel;
+        $kernel->intranet = new FakeWebshopIntranet;
+        $kernel->weblogin = new FakeWebshopWeblogin;
+        $kernel->setting = new FakeWebshopSetting;
+        $this->webshop = new Webshop($kernel, 'thissession');
     }
 
+    ////////////////////////////////////////////////
+
+    function testConstruction()
+    {
+        $this->assertTrue(is_object($this->webshop));
+        $this->assertTrue(is_object($this->webshop->basket));
+    }
+
+    function testPlaceOrderReturnsTrue()
+    {
+        $data = array('name' => 'Customer', 'email' => 'lars@legestue.net', 'type' => 0, 'description' => 'test', 'internal_note' => '', 'message' => '');
+        $order_id = $this->webshop->placeOrder($data);
+        $this->assertTrue($order_id > 0);
+    }
+
+    function testPlaceOrderResetsBasket()
+    {
+        $data = array('name' => 'Customer', 'email' => 'lars@legestue.net', 'type' => 0, 'description' => 'test', 'internal_note' => '', 'message' => '');
+        $order_id = $this->webshop->placeOrder($data);
+        $this->assertTrue($order_id > 0);
+
+        $basket = $this->webshop->getBasket();
+        $this->assertTrue(count($basket->getItems()) == 0);
+
+    }
 
 }
 ?>
