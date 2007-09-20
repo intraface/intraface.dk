da2350b3255848d3797d13f615807bd30ebbe6d0
Sune T. Jensen <sj@sunet.dk>
2007-09-20 15:59:25 +0000

    added modulepackage tabels

--- intraface.dk/install/database-update.sql
+++ intraface.dk/install/database-update.sql
@@ -39,8 +39,8 @@ ALTER TABLE `basket` ADD `basketevaluation_product` INT NOT NULL AFTER `date_cha
 
 ALTER TABLE `basket` ADD `text` TEXT NOT NULL ;
 
-ALTER TABLE `accounting_account` CHANGE `primosaldo_debet` `primosaldo_debet` DOUBLE( 11, 2 ) NOT NULL DEFAULT '0.00'
-ALTER TABLE `accounting_account` CHANGE `primosaldo_credit` `primosaldo_credit` DOUBLE( 11, 2 ) NOT NULL DEFAULT '0.00'
+ALTER TABLE `accounting_account` CHANGE `primosaldo_debet` `primosaldo_debet` DOUBLE( 11, 2 ) NOT NULL DEFAULT '0.00';
+ALTER TABLE `accounting_account` CHANGE `primosaldo_credit` `primosaldo_credit` DOUBLE( 11, 2 ) NOT NULL DEFAULT '0.00';
 
 CREATE TABLE `basket_details` (
   `id` int(11) NOT NULL auto_increment,
@@ -106,3 +106,16 @@ ALTER TABLE `module_package` ADD `active` INT NOT NULL AFTER `product_id` ;
 ALTER TABLE `module_package_module` ADD `active` INT NOT NULL AFTER `limiter` ;
 
 ALTER TABLE `intranet_module_package` ADD `active` INT NOT NULL AFTER `status_key` ;
+
+ALTER TABLE `module_package` ADD `sorting_index` INT NOT NULL AFTER `name` ;
+
+ALTER TABLE `module_package_group` ADD `sorting_index` INT NOT NULL AFTER `name` ;
+
+CREATE TABLE `module_package_plan` (
+`id` INT NOT NULL AUTO_INCREMENT ,
+`variation` VARCHAR( 255 ) NOT NULL ,
+`sorting_index` INT NOT NULL ,
+PRIMARY KEY ( `id` )
+) TYPE = MYISAM ;
+
+ALTER TABLE `module_package_group` CHANGE `name` `group` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL 
