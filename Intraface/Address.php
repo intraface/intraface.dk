0e5bf48c03dd519bd74bb6843ba4efb328d3e106
Lars Olesen <lars@intraface.dk>
2007-09-25 13:27:02 +0000

    Added a dependency of functions because Address uses safeToDb()

--- Intraface/Address.php
+++ Intraface/Address.php
@@ -14,6 +14,7 @@
 
 require_once 'Intraface/Standard.php';
 require_once 'Intraface/3Party/Database/Db_sql.php';
+require_once 'Intraface/functions/functions.php';
 
 class Address extends Standard {
 
