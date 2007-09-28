bb1f65c7ed02c4db09bd0c849ca6e5caf3f29092
Lars Olesen <lars@intraface.dk>
2007-09-28 07:42:27 +0000

    Added packageinformation for use with phpdoc

--- Intraface/modules/quotation/MainQuotation.php
+++ Intraface/modules/quotation/MainQuotation.php
@@ -1,36 +1,33 @@
 <?php
 /**
- * @package Order
- * @author	Lars Olesen <lars@legestue.net>
- * @since	1.0
- * @version	1.0 
- *
+ * @package Intraface_Quotation
  */
+
 class MainQuotation Extends Main {
 
-	function MainQuotation() {
-		$this->module_name = 'quotation'; // Navnet der vil stå i menuen
-		$this->menu_label = 'quotation'; // Navnet der vil stå i menuen
-		$this->show_menu = 0; // Skal modulet vises i menuen.
-		$this->active = 1; // Er modulet aktivt.
-		$this->menu_index = 62;
-		$this->frontpage_index = 51;
-		
-		$this->addPreloadFile('Quotation.php');
+    function MainQuotation() {
+        $this->module_name = 'quotation'; // Navnet der vil stå i menuen
+        $this->menu_label = 'quotation'; // Navnet der vil stå i menuen
+        $this->show_menu = 0; // Skal modulet vises i menuen.
+        $this->active = 1; // Er modulet aktivt.
+        $this->menu_index = 62;
+        $this->frontpage_index = 51;
 
-		// $this->addFrontpageFile('include_front.php');
+        $this->addPreloadFile('Quotation.php');
 
-		// Tilføj undermenu punkter.
-		// $this->addSubMenuItem("Årsafslutning", "end.php");									
+        // $this->addFrontpageFile('include_front.php');
 
-		// Tilføj subaccess punkter
-		// opretkunde: et kort navn der er sigende
-		// Opret ny kunde: En beskrivelse af subaccess.
-		//$this->addSubAccessItem("opretkunde", "Opret ny kunde");
+        // Tilføj undermenu punkter.
+        // $this->addSubMenuItem("Årsafslutning", "end.php");
 
-		// hvilke units kan man vælge imellem?
-		//$this->addSetting("unit", array(1=>"kr.", 2=>"stk."));
+        // Tilføj subaccess punkter
+        // opretkunde: et kort navn der er sigende
+        // Opret ny kunde: En beskrivelse af subaccess.
+        //$this->addSubAccessItem("opretkunde", "Opret ny kunde");
 
-	}
+        // hvilke units kan man vælge imellem?
+        //$this->addSetting("unit", array(1=>"kr.", 2=>"stk."));
+
+    }
 }
 ?>
--- Intraface/modules/quotation/Quotation.php
+++ Intraface/modules/quotation/Quotation.php
@@ -1,10 +1,12 @@
 <?php
-
+/**
+ * @package Intraface_Quotation
+ */
 class Quotation extends Debtor {
 
-	function Quotation(& $kernel, $id = 0) {
-		Debtor::Debtor($kernel, 'quotation', $id);
-	}
+    function Quotation(& $kernel, $id = 0) {
+        Debtor::Debtor($kernel, 'quotation', $id);
+    }
 
 }
 
