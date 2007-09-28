37a0f2ecbef957cbebc1487a8533f4797a5089d0
Lars Olesen <lars@intraface.dk>
2007-09-28 07:43:04 +0000

    Added packageinformation for use with phpdoc

--- Intraface/modules/order/MainOrder.php
+++ Intraface/modules/order/MainOrder.php
@@ -1,36 +1,36 @@
 <?php
 /**
- * @package Order
+ * @package Intraface_Order
  * @author	Lars Olesen <lars@legestue.net>
  * @since	1.0
- * @version	1.0 
+ * @version	1.0
  *
  */
 class MainOrder Extends Main {
 
-	function MainOrder() {
-		$this->module_name = 'order'; // Navnet der vil stå i menuen
-		$this->menu_label = 'order'; // Navnet der vil stå i menuen
-		$this->show_menu = 0; // Skal modulet vises i menuen.
-		$this->active = 1; // Er modulet aktivt.
-		$this->menu_index = 64;
-		$this->frontpage_index = 52;
-		
-		$this->addFrontpageFile('include_front.php');
+    function MainOrder() {
+        $this->module_name = 'order'; // Navnet der vil stå i menuen
+        $this->menu_label = 'order'; // Navnet der vil stå i menuen
+        $this->show_menu = 0; // Skal modulet vises i menuen.
+        $this->active = 1; // Er modulet aktivt.
+        $this->menu_index = 64;
+        $this->frontpage_index = 52;
 
-		$this->addPreloadFile('Order.php');
+        $this->addFrontpageFile('include_front.php');
 
-		// Tilføj undermenu punkter.
-		// $this->addSubMenuItem("Årsafslutning", "end.php");									
+        $this->addPreloadFile('Order.php');
 
-		// Tilføj subaccess punkter
-		// opretkunde: et kort navn der er sigende
-		// Opret ny kunde: En beskrivelse af subaccess.
-		//$this->addSubAccessItem("opretkunde", "Opret ny kunde");
+        // Tilføj undermenu punkter.
+        // $this->addSubMenuItem("Årsafslutning", "end.php");
 
-		// hvilke units kan man vælge imellem?
-		//$this->addSetting("unit", array(1=>"kr.", 2=>"stk."));
+        // Tilføj subaccess punkter
+        // opretkunde: et kort navn der er sigende
+        // Opret ny kunde: En beskrivelse af subaccess.
+        //$this->addSubAccessItem("opretkunde", "Opret ny kunde");
 
-	}
+        // hvilke units kan man vælge imellem?
+        //$this->addSetting("unit", array(1=>"kr.", 2=>"stk."));
+
+    }
 }
 ?>
--- Intraface/modules/order/Order.php
+++ Intraface/modules/order/Order.php
@@ -1,10 +1,13 @@
 <?php
+/**
+ * @package Intraface_Order
+ */
 
 class Order extends Debtor {
 
-	function Order(& $kernel, $id = 0) {
-		Debtor::Debtor($kernel, 'order', $id);
-	}
+    function Order(& $kernel, $id = 0) {
+        Debtor::Debtor($kernel, 'order', $id);
+    }
 
 }
 
