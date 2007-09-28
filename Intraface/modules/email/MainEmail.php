e511fe2f31fc52018fc07753540568d55cd54438
Lars Olesen <lars@intraface.dk>
2007-09-28 07:44:56 +0000

    Added packageinformation for use with phpdoc

--- Intraface/modules/email/MainEmail.php
+++ Intraface/modules/email/MainEmail.php
@@ -1,25 +1,21 @@
 <?php
 /**
- * Kontakt
- *
- * @package Contact
- * @author	Lars Olesen
- * @since	1.0
- * @version	1.0
+ * @package Intraface_Email
  */
- 
+
+
 class MainEmail Extends Main {
 
-	function MainEmail() {
-		$this->module_name = 'email';
-		$this->menu_label = 'email'; // Navnet der vil stå i menuen
-		$this->show_menu = 1; // Skal modulet vises i menuen.
-		$this->active = 1; // Er modulet aktivt.
-		$this->menu_index = 210;
-		$this->frontpage_index = 200;
-		
-		$this->addRequiredShared('email');
-	}
+    function MainEmail() {
+        $this->module_name = 'email';
+        $this->menu_label = 'email'; // Navnet der vil stå i menuen
+        $this->show_menu = 1; // Skal modulet vises i menuen.
+        $this->active = 1; // Er modulet aktivt.
+        $this->menu_index = 210;
+        $this->frontpage_index = 200;
+
+        $this->addRequiredShared('email');
+    }
 
 }
 
