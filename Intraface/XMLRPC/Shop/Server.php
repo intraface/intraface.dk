0d1c2934a70e5073229faa5dfa3ddeae11b0919f
Sune T. Jensen <sj@sunet.dk>
2007-10-09 21:20:11 +0000

    Bug in comparing order_id

--- Intraface/XMLRPC/Shop/Server.php
+++ Intraface/XMLRPC/Shop/Server.php
@@ -345,7 +345,7 @@ class Intraface_XMLRPC_Shop_Server
         }
 
         $values = $this->utf8Decode($values);
-        if ($order_id = $this->webshop->placeOrder($values)) {
+        if (!$order_id = $this->webshop->placeOrder($values)) {
             throw new XML_RPC2_FaultException('order could not be placed. It returned the following error: ' . strtolower(implode(', ', $this->webshop->error->message)), -4);
         }
 
