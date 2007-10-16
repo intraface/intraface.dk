407f3d90c6fd809ad58b8408952fe4e4881fe6f7
Lars Olesen <lars@intraface.dk>
2007-10-16 15:29:23 +0000

    Added a and b to error 4 to which one is valid.

--- intraface.dk/main/file/index.php
+++ intraface.dk/main/file/index.php
@@ -15,7 +15,7 @@ require(PATH_INCLUDE_CONFIG.'setting_file_type.php');
 
 // file should stop if no querystring
 if (empty($_SERVER["QUERY_STRING"])) {
-	exit;
+    exit;
 }
 
 $query_parts = explode('/', $_SERVER["QUERY_STRING"]);
@@ -25,10 +25,10 @@ $query_parts = safeToDb($query_parts);
 
 $weblogin = new Weblogin();
 if (!$intranet_id = $weblogin->auth('public', $query_parts[1])) {
-	die('FEJL I LÆSNING AF BILLEDE (0)');
+    die('FEJL I LÆSNING AF BILLEDE (0)');
 }
 if($intranet_id == false) {
-	trigger_error("FEJL I LÆSNING AF BILLEDE (1)", E_USER_ERROR);
+    trigger_error("FEJL I LÆSNING AF BILLEDE (1)", E_USER_ERROR);
 }
 
 $kernel = new Kernel;
@@ -37,37 +37,37 @@ $filehandler_shared = $kernel->useShared('filehandler');
 
 $filehandler = FileHandler::factory($kernel, $query_parts[2]);
 if(!is_object($filehandler)) {
-	trigger_error("FEJL I LÆSNING AF BILLEDE (2)", E_USER_ERROR);
+    trigger_error("FEJL I LÆSNING AF BILLEDE (2)", E_USER_ERROR);
 }
 switch($filehandler->get('accessibility')) {
-	case 'personal':
-		// Not implemented - continue to next
-	case 'intranet':
-		// You have to be logged in to access this file
-		session_start();
-		require('Intraface/Auth.php');
-		$auth = new Auth(session_id());
+    case 'personal':
+        // Not implemented - continue to next
+    case 'intranet':
+        // You have to be logged in to access this file
+        session_start();
+        require('Intraface/Auth.php');
+        $auth = new Auth(session_id());
 
-		if (!$user_id = $auth->isLoggedIn()) {
-			die("FEJL I LÆSNING AF BILLEDE (4)");
-		}
+        if (!$user_id = $auth->isLoggedIn()) {
+            die("FEJL I LÆSNING AF BILLEDE (4a)");
+        }
 
-		$user = new User($user_id);
-		$intranet = new Intranet($user->getActiveIntranetId());
+        $user = new User($user_id);
+        $intranet = new Intranet($user->getActiveIntranetId());
 
-		if($intranet->get('id') != $intranet_id) {
-			die("FEJL I LÆSNING AF BILLEDE (4)");
-		}
+        if($intranet->get('id') != $intranet_id) {
+            die("FEJL I LÆSNING AF BILLEDE (4b)");
+        }
 
-		break;
-	case 'public':
-		// public alle må se den
-		// vi gør intet
-		break;
-	default:
-		// Dette er en ugyldig type
-		trigger_error("FEJL I LÆSNING AF BILLEDE (5)", E_USER_ERROR);
-		break;
+        break;
+    case 'public':
+        // public alle må se den
+        // vi gør intet
+        break;
+    default:
+        // Dette er en ugyldig type
+        trigger_error("FEJL I LÆSNING AF BILLEDE (5)", E_USER_ERROR);
+        break;
 }
 
 
@@ -81,9 +81,9 @@ $filehandler_shared->includeFile('InstanceHandler.php');
 $instancehandler = new InstanceHandler($filehandler);
 
 if($instancehandler->_checkType($query_parts[3]) !== false) {
-	$filehandler->createInstance($query_parts[3]);
+    $filehandler->createInstance($query_parts[3]);
 
-	$file_path = $filehandler->instance->get('file_path');
+    $file_path = $filehandler->instance->get('file_path');
 }
 
 $last_modified = filemtime($file_path);
