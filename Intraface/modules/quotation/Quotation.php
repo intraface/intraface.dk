11ab397ae3e04cd41d2cc71cc6cf2409254193c6
Lars Olesen <lars@intraface.dk>
2007-10-18 20:52:05 +0000

    Updated coding standards to php5

--- Intraface/modules/quotation/Quotation.php
+++ Intraface/modules/quotation/Quotation.php
@@ -2,10 +2,12 @@
 /**
  * @package Intraface_Quotation
  */
-class Quotation extends Debtor {
+class Quotation extends Debtor
+{
 
-    function Quotation(& $kernel, $id = 0) {
-        Debtor::Debtor($kernel, 'quotation', $id);
+    function Quotation($kernel, $id = 0)
+    {
+        parent::__construct($kernel, 'quotation', $id);
     }
 
 }
