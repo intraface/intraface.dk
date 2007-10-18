c54e7669dc1eb961ad8a9a3a4976d93442b00b08
Sune T. Jensen <sj@sunet.dk>
2007-10-18 14:31:24 +0000

    Added tests

--- tests/unit/common/AddressTest.php
+++ tests/unit/common/AddressTest.php
@@ -1,28 +1,81 @@
 <?php
 require_once dirname(__FILE__) . '/../config.test.php';
-
 require_once 'PHPUnit/Framework.php';
-
+require_once 'Intraface/Error.php';
+require_once 'Intraface/Validator.php';
 require_once 'Intraface/Address.php';
 
 class AddressTest extends PHPUnit_Framework_TestCase
 {
-    function createAddress($id = 0)
+
+    private $kernel;
+
+    function setUp()
     {
-        return new Address($id);
+        $db = MDB2::factory(DB_DSN);
+        $db->query('TRUNCATE address');
+    }
+    
+    function getValidAddress() {
+        return array('name' => 'test', 'address' => 'road 1', 'postcode' => '0123', 'city' => 'Mycity', 'country' => '', 'email' => 'sj@sunet.dk', 'cvr' => '', 'website' => '', 'phone' => '', 'ean' => '');
     }
 
-    function testConstruction()
-    {
-        $address = $this->createAddress();
-        $this->assertTrue(is_object($address));
+    function testValidateWithValidAddress() {
+        
+        $address = Address::factory('intranet', 1);
+        $this->assertTrue($address->validate($this->getValidAddress()));
     }
-
-    function testFactory()
-    {
-        $address = Address::factory('contact', 1);
-        $this->assertTrue(is_object($address));
+    
+    function testValidateWithInvalidAddress() {
+        $address = Address::factory('intranet', 1);
+        $this->assertFalse($address->validate(array()));
     }
-
+    
+    function testSaveOnEmptyDB() {
+        $address = Address::factory('intranet', 1);
+        $this->assertTrue($address->save($this->getValidAddress()));
+        $this->assertEquals(1, $address->get('address_id')); // on empty database this must be 1
+    }
+    
+    function testSaveWithSaveTwoTimeWithSameData() {    
+        $address = Address::factory('intranet', 1);
+        $address->save($this->getValidAddress());
+        
+        
+        // we repeat the save, this should automatically determine that nothing should be done. Can we determine whether this is true?
+        $this->assertTrue($address->save($this->getValidAddress()));
+        $this->assertEquals(1, $address->get('address_id')); // on empty database this must be 1
+    }
+    
+    function testSaveWithChangeInName() {    
+        $address = Address::factory('intranet', 1);
+        
+        $address_array = $this->getValidAddress();
+        
+        $address = Address::factory('intranet', 1);
+        $address->save($address_array);
+        
+        $address_array['name'] = 'test 2';
+        $this->assertTrue($address->save($address_array));
+        $this->assertEquals(2, $address->get('address_id')); // on empty database this must be 1
+    }
+    
+    function testUpdate() {
+        /*
+        Da metoden er fjernet fra klassen er testen også fjernet.
+        $address = Address::factory('intranet', 1);
+        $address->save($this->getValidAddress());
+        $this->assertTrue($address->update($this->getValidAddress()));
+        $this->assertEquals(1, $address->get('address_id')); // on empty database this must be 1
+        */
+    }
+    
+    function testLoad() {
+        $address = Address::factory('intranet', 1);
+        $address->save($this->getValidAddress());
+        
+        $address = new Address(1);
+        $this->assertEquals(1, $address->get('address_id'));
+    }
 }
 ?>
