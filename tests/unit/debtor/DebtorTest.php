727e6c87524ff77d5a626afd4e3e1a5cd812b0b4
Sune T. Jensen <sj@sunet.dk>
2007-10-18 11:06:40 +0000

    Added more tests

--- tests/unit/debtor/DebtorTest.php
+++ tests/unit/debtor/DebtorTest.php
@@ -6,11 +6,12 @@ require_once 'Intraface/Kernel.php';
 require_once 'Intraface/Setting.php';
 require_once 'Intraface/DBQuery.php';
 require_once 'Intraface/modules/debtor/Debtor.php';
+require_once 'Intraface/tools/Date.php';
 
 
 class FakeDebtorAddress {
     function get($key = '') {
-        $info = array('name' => 'Lars Olesen', 'address' => 'Græsvangen 8, Syvsten', 'postcode' => 9300, 'city' => 'Aarhus N', 'cvr' => '', 'ean' => '', 'phone' => '75820811', 'email' => 'lars@legestue.net');
+        $info = array('name' => 'Lars Olesen', 'address' => 'Græsvangen 8, Syvsten', 'postcode' => 9300, 'city' => 'Aarhus N', 'cvr' => '', 'ean' => '', 'phone' => '75820811', 'email' => 'lars@legestue.net', 'address_id' => 1);
         if (empty($key)) return $info;
         else return $info[$key];
     }
@@ -33,26 +34,70 @@ class FakeDebtorIntranet
     function __construct() {
         $this->address = new FakeDebtorAddress;
     }
-    function get() {
-        return array('name' => 'Intranetname', 'contact_person' => '');
+    function get($key = '') {
+        $info = array('name' => 'Intranetname', 'contact_person' => '','id' => 1);
+        if (empty($key)) return $info;
+        else return $info[$key];
     }
+    
+    function hasModuleAccess() {
+        return true;
+    }
+}
+
+class FakeDebtorSetting {
+    
+    function get($type, $setting) {
+        
+        $info = array('intranet' => array('onlinepayment.provider_key' => 1));
+        
+        return $info[$type][$setting];
+    }
+    
 }
 
 class DebtorTest extends PHPUnit_Framework_TestCase
 {
+    private $kernel;
+    
     function createDebtor()
     {
         $kernel = new Kernel;
         $kernel->user = new FakeDebtorUser;
         $kernel->intranet = new FakeDebtorIntranet;
+        $kernel->setting = new FakeDebtorSetting;
         $kernel->useModule('debtor');
+        $this->kernel = $kernel;
+        
         return new Debtor($kernel, 'order');
+    }
+    
+    function createContact() {
+        $this->kernel->useModule('contact');
+        $contact = new Contact($this->kernel);
+        
+        return $contact->save(array('name' => 'Test', 'email' => 'lars@legestue.net', 'phone' => '98468269'));
     }
 
     function testConstruct()
     {
         $debtor = $this->createDebtor();
         $this->assertTrue(is_object($debtor));
+    }
+    
+    
+    
+    function testUpdate() {
+        $debtor = $this->createDebtor();
+        
+        
+        $this->assertTrue($debtor->update(
+            array(
+                'contact_id' => $this->createContact(), 
+                'description' =>'test',
+                'this_date' => date('d-m-Y'),
+                'due_date' => date('d-m-Y'))
+            ) > 0);
     }
 }
 
