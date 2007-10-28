618c2dcbb79f3bd3d095f4966764d520c6914dc8
Sune T. Jensen <sj@sunet.dk>
2007-10-28 21:01:13 +0000

    database updates for instance types

--- install/database-update.sql
+++ install/database-update.sql
@@ -1,64 +1,14 @@
-ALTER TABLE `email` ADD `contact_person_id` INT NOT NULL AFTER `contact_id` ;
-ALTER TABLE `email` ADD `bcc_to_user` INT NOT NULL AFTER `user_id` ;
+CREATE TABLE `file_handler_instance_type` (
+`id` INT NOT NULL AUTO_INCREMENT ,
+`intranet_id` INT NOT NULL ,
+`name` VARCHAR( 255 ) NOT NULL ,
+`max_height` INT NOT NULL ,
+`max_width` INT NOT NULL ,
+PRIMARY KEY ( `id` )
+) TYPE = MYISAM ;
 
-ALTER TABLE `basket` ADD `product_detail_id` INT NOT NULL AFTER `product_id` ;
+ALTER TABLE `file_handler_instance` CHANGE `type` `type_key` INT( 11 ) NOT NULL DEFAULT '0';
 
-CREATE TABLE `intranet_module_package` (
-  `id` int(11) NOT NULL auto_increment,
-  `intranet_id` int(11) NOT NULL,
-  `module_package_id` int(11) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `order_debtor_id` int(11) NOT NULL,
-  `status_key` int(11) NOT NULL,
-  `active` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM ;
+ALTER TABLE `file_handler_instance_type` ADD `type_key` INT NOT NULL AFTER `name` ;
 
-CREATE TABLE `module_package` (
-  `id` int(11) NOT NULL auto_increment,
-  `module_package_group_id` int(11) NOT NULL,
-  `module_package_plan_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `active` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM ;
-
-CREATE TABLE `module_package_action` (
-  `id` int(11) NOT NULL auto_increment,
-  `intranet_id` int(11) NOT NULL,
-  `order_debtor_id` int(11) NOT NULL,
-  `date_created` datetime NOT NULL,
-  `action` text NOT NULL,
-  `active` int(11) NOT NULL default '1',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM ;
-
-CREATE TABLE `module_package_group` (
-  `id` int(11) NOT NULL auto_increment,
-  `group_name` varchar(255) NOT NULL,
-  `sorting_index` int(11) NOT NULL,
-  `active` int(11) NOT NULL default '1',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM ;
-
-CREATE TABLE `module_package_module` (
-  `id` int(11) NOT NULL auto_increment,
-  `module_package_id` int(11) NOT NULL,
-  `module` varchar(255) NOT NULL,
-  `limiter` varchar(255) NOT NULL,
-  `active` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM ;
-
-CREATE TABLE `module_package_plan` (
-  `id` int(11) NOT NULL auto_increment,
-  `plan` varchar(255) NOT NULL,
-  `plan_index` int(11) NOT NULL,
-  `active` int(11) NOT NULL default '1',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM ;
-
-ALTER TABLE `webshop_basket_evaluation` ADD `evaluate_value_case_sensitive` INT NOT NULL AFTER `evaluate_value` ;
-
-ALTER TABLE `basket_details` ADD `customer_ean` VARCHAR( 255 ) NOT NULL AFTER `customer_coupon` ;
+ALTER TABLE `file_handler_instance_type` ADD `active` INT DEFAULT '1' NOT NULL ;
