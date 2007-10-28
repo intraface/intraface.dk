77bad52131795503faec01c262dfe879691587ed
Sune T. Jensen <sj@sunet.dk>
2007-10-28 20:57:39 +0000

    added settings for custom instances

---
+++ intraface.dk/shared/filehandler/edit_instance_type.php
@@ -0,0 +1,90 @@
+<?php
+require('../../include_first.php');
+$shared_filehandler = $kernel->useShared('filehandler');
+$translation = $kernel->getTranslation('filehandler');
+$shared_filehandler->includeFile('InstanceManager.php');
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    if(!empty($_POST['submit'])) {
+        $instance_manager = new InstanceManager($kernel, (int)$_POST['type_key']);
+        
+        if($instance_manager->save($_POST)) {
+            header('location: settings.php');
+            exit;
+        }
+        $value = $_POST;
+    }
+    else {
+        trigger_error("submit was not set!", E_USER_ERROR);
+        exit;
+    }  
+}
+elseif($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_GET['type_key'])) {
+        $instance_manager = new InstanceManager($kernel, (int)$_GET['type_key']);
+        $value = $instance_manager->get();
+    }
+    else {
+        $instance_manager = new InstanceManager($kernel);
+        $value = $instance_manager->get();
+    } 
+}
+
+$page = new Page($kernel);
+$page->start($translation->get('edit instance type'));
+
+?>
+
+<h1><?php echo safeToHtml($translation->get('edit instance type')); ?></h1>
+
+<?php $instance_manager->error->view(); ?>
+
+<form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
+
+    <input type="hidden" name="type_key" value="<?php echo intval($instance_manager->get('type_key')); ?>" />
+    
+    <fieldset>
+        <legend><?php echo safeToHtml($translation->get('instance')); ?></legend>
+        
+        <div class="formrow">
+            <label for="name"><?php echo safeToHtml($translation->get('name')); ?>:</label>
+            <?php if($instance_manager->get('type_key') > 0 && $instance_manager->get('origin') != 'custom'): ?>
+                <?php if(isset($value['name'])) echo '<div>'.safeToForm($value['name']).'</div>'; ?>
+            <?php else: ?>
+                <input type="input" name="name" id="name" value="<?php if(isset($value['name'])) echo safeToForm($value['name']); ?>" /> <span><?php echo safeToHtml($translation->get('allowed characters')); ?>: a-z 0-9 _ -</span>
+            <?php endif; ?>
+        </div>
+        
+        <div class="formrow">
+            <label for="max_width"><?php echo safeToHtml($translation->get('maximum width')); ?>:</label>
+            <input type="input" name="max_width" id="max_width" value="<?php if(isset($value['max_width'])) echo safeToForm($value['max_width']); ?>" />
+        </div>
+        
+        <div class="formrow">
+            <label for="max_height"><?php echo safeToHtml($translation->get('maximum height')); ?>:</label>
+            <input type="input" name="max_height" id="max_height" value="<?php if(isset($value['max_height'])) echo safeToForm($value['max_height']); ?>" />
+        </div>
+        
+        <div class="formrow">
+            <label for="resize_type"><?php echo safeToHtml($translation->get('resize type')); ?>:</label>
+            <select name="resize_type" id="resize_type">
+                <?php foreach($instance_manager->getResizeTypes() AS $resize_type): ?>
+                    <option value="<?php echo safeToHtml($resize_type); ?>" <?php if(isset($value['resize_type']) && $value['resize_type'] == $resize_type) echo safeToHtml('selected="selected"'); ?> ><?php echo safeToHtml($translation->get($resize_type)); ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        
+    </fieldset>
+    
+    <p>
+        <input type="submit" class="submit" name="submit" value="<?php echo safeToForm($translation->get('save', 'common')); ?>" /> 
+        <?php echo safeToHtml($translation->get('or', 'common')); ?> 
+        <a href="settings.php"><?php echo safeToHtml($translation->get('cancel', 'common')); ?></a>
+    </p>
+</form>
+
+<?php
+$page->end();
+?>
---
+++ intraface.dk/shared/filehandler/settings.php
@@ -0,0 +1,73 @@
+<?php
+require('../../include_first.php');
+
+$translation = $kernel->getTranslation('filehandler');
+$shared_filehandler = $kernel->useShared('filehandler');
+$shared_filehandler->includeFile('InstanceManager.php');
+
+
+if(!empty($_GET['delete_instance_type_key'])) {
+    $instance_manager = new InstanceManager($kernel, (int)$_GET['delete_instance_type_key']);
+    $instance_manager->delete();
+}
+
+$filehandler = new Filehandler($kernel);
+$instance_manager = new InstanceManager($kernel);
+
+
+
+$page = new Page($kernel);
+$page->start($translation->get('filehandler settings'));
+
+?>
+<h1><?php echo safeToHtml($translation->get('filehandler settings')); ?></h1>
+
+<?php $instance_manager->error->view(); ?>
+
+<?php
+// $filehandler->createInstance();
+// $instances = $filehandler->instance->getTypes();
+
+$instances = $instance_manager->getList();
+if(count($instances) > 0): ?>
+    <table class="stripe">
+        <caption><?php echo safeToHtml($translation->get('instance types')); ?></caption>
+        <thead>
+            <tr>
+                <th><?php echo safeToHtml($translation->get('name')); ?></th>
+                <th><?php echo safeToHtml($translation->get('maximum width')); ?></th>
+                <th><?php echo safeToHtml($translation->get('maximum height')); ?></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($instances AS $instance): ?>
+                <tr>
+                    <td><?php echo safeToHtml($instance['name']); ?></td>
+                    <td><?php echo safeToHtml($instance['max_width']); ?></td>
+                    <td><?php echo safeToHtml($instance['max_height']); ?></td>
+                    <td>
+                      <?php
+                      echo '<a class="edit" href="edit_instance_type.php?type_key='.intval($instance['type_key']).'">'.safeToHtml($translation->get('edit', 'common')).'</a> ';
+                      
+                      if($instance['origin'] == 'overwritten') {
+                          echo '<a class="delete" href="settings.php?delete_instance_type_key='.intval($instance['type_key']).'">'.safeToHtml($translation->get('reset to standard')).'</a>';
+                      }  
+                      elseif($instance['origin'] == 'custom') {
+                          echo '<a class="delete" href="settings.php?delete_instance_type_key='.intval($instance['type_key']).'">'.safeToHtml($translation->get('delete', 'common')).'</a>';
+                      }  
+                      ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    
+    <ul class="options">
+        <li><a href="edit_instance_type.php"><?php echo safeToHtml($translation->get('add new instance type')); ?></a><li>
+    </ul>
+<?php endif; ?>
+
+<?php
+$page->end();
+?>
