3699ee0599365973bc6e85682f21bb8af7fbce42
Sune T. Jensen <sj@sunet.dk>
2007-10-29 08:06:26 +0000

    added field for file_handler_instance_type

--- install/database-update.sql
+++ install/database-update.sql
@@ -12,3 +12,5 @@ ALTER TABLE `file_handler_instance` CHANGE `type` `type_key` INT( 11 ) NOT NULL 
 ALTER TABLE `file_handler_instance_type` ADD `type_key` INT NOT NULL AFTER `name` ;
 
 ALTER TABLE `file_handler_instance_type` ADD `active` INT DEFAULT '1' NOT NULL ;
+
+ALTER TABLE `file_handler_instance_type` ADD `resize_type_key` INT NOT NULL AFTER `max_width` ;
