8f7b3ffcabd880ece20341d06ac7b5690370c2d0
Sune T. Jensen <sj@sunet.dk>
2007-10-29 21:21:20 +0000

    implemented fileviewer

--- intraface.dk/main/file/index.php
+++ intraface.dk/main/file/index.php
@@ -10,91 +10,51 @@
 
 require('../../common.php');
 
-// den her er klodset - den henter $_file_type;
-require(PATH_INCLUDE_CONFIG.'setting_file_type.php');
-
-
 // file should stop if no querystring
 if (empty($_SERVER["QUERY_STRING"])) {
+    trigger_error('no querystring is given!', E_USER_WARNING);
     exit;
 }
-
 $query_parts = explode('/', $_SERVER["QUERY_STRING"]);
-
-// safeToDb er forkert at bruge her
-$query_parts = safeToDb($query_parts);
 
 $weblogin = new Weblogin();
 if (!$intranet_id = $weblogin->auth('public', $query_parts[1])) {
-    die('FEJL I LÆSNING AF BILLEDE (0)');
+    trigger_error('Error logging in to intranet with public key '.$query_parts[1], E_USER_WARNING);
+    exit;
 }
-if($intranet_id == false) {
-    trigger_error("FEJL I LÆSNING AF BILLEDE (1)", E_USER_ERROR);
-}
-
 $kernel = new Kernel;
 $kernel->intranet = new Intranet($intranet_id);
 $filehandler_shared = $kernel->useShared('filehandler');
+$filehandler_shared->includeFile('FileViewer.php');
 
 $filehandler = FileHandler::factory($kernel, $query_parts[2]);
-if(!is_object($filehandler)) {
-    trigger_error("FEJL I LÆSNING AF BILLEDE (2)", E_USER_ERROR);
-}
-switch($filehandler->get('accessibility')) {
-    case 'personal':
-        // Not implemented - continue to next
-    case 'intranet':
-        // You have to be logged in to access this file
-        session_start();
-        require('Intraface/Auth.php');
-        $auth = new Auth(session_id());
-
-        if (!$user_id = $auth->isLoggedIn()) {
-            die("FEJL I LÆSNING AF BILLEDE (4a)");
-        }
-
-        $user = new User($user_id);
-        $intranet = new Intranet($user->getActiveIntranetId());
-
-        if($intranet->get('id') != $intranet_id) {
-            die("FEJL I LÆSNING AF BILLEDE (4b)");
-        }
-
-        break;
-    case 'public':
-        // public alle må se den
-        // vi gør intet
-        break;
-    default:
-        // Dette er en ugyldig type
-        trigger_error("FEJL I LÆSNING AF BILLEDE (5)", E_USER_ERROR);
-        break;
+if(!is_object($filehandler) || $filehandler->get('id') == 0) {
+    trigger_error('Invalid image: '.$_SERVER['QUERY_STRING'], E_USER_WARNING);
+    exit;
 }
 
+settype($query_parts[3], 'string');
+$fileviewer = new FileViewer($filehandler, $query_parts[3]);
 
-
-$file_id = $filehandler->get('id');
-$file_name = $filehandler->get('file_name');
-$mime_type = $_file_type[$filehandler->get('file_type_key')]['mime_type'];
-$file_path = $filehandler->get('file_path');
-
-$filehandler_shared->includeFile('InstanceHandler.php');
-$instancehandler = new InstanceHandler($filehandler);
-
-if($instancehandler->checkType($query_parts[3]) !== false) {
-    $filehandler->createInstance($query_parts[3]);
-
-    $file_path = $filehandler->instance->get('file_path');
+if($fileviewer->needLogin()) {
+    session_start();
+    require('Intraface/Auth.php');
+    $auth = new Auth(session_id());
+    // the user is logged in but...
+    if (!$user_id = $auth->isLoggedIn()) {
+        trigger_error('You need to be logged in to view the file', E_USER_WARNING);
+        exit;
+    }
+     
+    // ...we need to check that it is the right intranet
+    $user = new User($user_id);
+    $intranet = new Intranet($user->getActiveIntranetId());
+    if($intranet->get('id') != $intranet_id) {
+        trigger_error('You where not logged into the correct intranet to view the file', E_USER_WARNING);
+        exit;
+    }
 }
 
-$last_modified = filemtime($file_path);
-
-header('Content-Type: '.$mime_type);
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $last_modified).' GMT');
-header('Cache-Control:');
-header('Content-Disposition: inline; filename='.$file_name);
-header('Pragma:');
-
-readfile($file_path);
+$fileviewer->out();
 exit;
 ?>
