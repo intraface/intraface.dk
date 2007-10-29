f47e46b7fd4f9298cbcbc95f79dec041c25794ce
Sune T. Jensen <sj@sunet.dk>
2007-10-29 21:15:01 +0000

    implemented fileviewer

--- Intraface/shared/filehandler/FileViewer.php
+++ Intraface/shared/filehandler/FileViewer.php
@@ -18,7 +18,80 @@ require_once 'Intraface/Intranet.php';
 require_once 'Intraface/Kernel.php';
 require_once 'Intraface/shared/filehandler/FileHandler.php';
 
-class FileViewer
+
+class FileViewer {
+    
+    /**
+     * @var string file_name
+     */
+    private $file_name;
+    
+    /**
+     * @var string mime_type
+     */
+    private $mime_type;
+    
+    /**
+     * @var string file_path
+     */
+    private $file_path;
+    
+    /**
+     * @var object filehandler
+     */
+    private $filehandler;
+    
+    
+    public function __construct($filehandler, $instance) {
+        
+        if(!is_object($filehandler)) {
+            trigger_error('the first parameter needs to be filehandler in FileViewet->__construct', E_USER_ERROR);
+            exit;
+        }
+        
+        $this->filehandler = $filehandler;
+        $this->file_path = $filehandler->get('file_path');
+        $this->file_name = $filehandler->get('file_name');
+        $_file_type = array();
+        // $file_type is filled by the include_file
+        require(PATH_INCLUDE_CONFIG.'setting_file_type.php');
+        $this->mime_type = $_file_type[$filehandler->get('file_type_key')]['mime_type'];
+        
+        $this->filehandler->createInstance();
+        
+        if(!empty($instance) && $filehandler->instance->checkType($instance) !== false) {
+            $this->filehandler->createInstance($instance);
+            $this->file_path = $filehandler->instance->get('file_path');
+        }
+    }
+    
+    public function needLogin() {
+        return $this->filehandler->get('accessibility') != 'public';
+    }
+    
+    
+    public function out() {
+        
+        if(!file_exists($this->file_path)) {
+            return 'invalid file';
+        }
+        
+        $last_modified = filemtime($this->file_path);
+        
+        header('Content-Type: '.$this->mime_type);
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $last_modified).' GMT');
+        header('Cache-Control:');
+        header('Content-Disposition: inline; filename='.$this->file_name);
+        header('Pragma:');
+        return readfile($this->file_path);
+    }
+    
+}
+
+
+
+
+class _old_FileViewer
 {
 
     /**
