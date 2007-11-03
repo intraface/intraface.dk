cf7d12c7e73041edac94f13ed4ebd144dd2f589b
Sune T. Jensen <sj@sunet.dk>
2007-11-03 18:22:09 +0000

    Added comment

--- tests/unit/common/ErrorTest.php
+++ tests/unit/common/ErrorTest.php
@@ -1,15 +1,18 @@
 <?php
 require_once dirname(__FILE__) . '/../config.test.php';
-
 require_once 'PHPUnit/Framework.php';
 require_once 'Intraface/Error.php';
+
+/**
+ * Remember this is only test for if the extend works. The test of the actual functionality is in Intraface_3Party
+ */
 
 class ErrorTest extends PHPUnit_Framework_TestCase
 {
     function testConstruction()
     {
         $error = new Error;
-        $this->assertTrue(true);
+        $this->assertObject($error);
     }
 
 }
