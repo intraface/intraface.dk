d091c1b3d0d031669c999abac631f63a7384b756
Lars Olesen <lars@intraface.dk>
2007-11-10 16:44:37 +0000

    Deleted config.local for shop.

--- intraface.dk/demo/shop/config.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-// XMLRPC
-define('INTRAFACE_PRIVATE_KEY', 'privatekeyshouldbereplaced');
-define('INTRAFACE_DEBUG', false);
-define('PATH_XMLRPC', 'http://intraface.dk.asterix/xmlrpc/shop/server3.php');
-define('WEBSHOP_GRAPHICS_PATH', 'http://intraface.dk.asterix/demo/shop/graphics/');
-define('WEBSHOP_ONLINEPAYMENT_RUNNING', true);
-define('WEBSHOP_ONLINEPAYMENT_PRODUCTION', true);
-?>
