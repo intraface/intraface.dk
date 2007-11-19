bd4580e4874f8c430d67cf4ed9d57026872e3643
Lars Olesen <lars@intraface.dk>
2007-11-19 16:23:42 +0000

    Reparied stuff to do with keywords.

--- intraface.dk/modules/product/related_product.php
+++ intraface.dk/modules/product/related_product.php
@@ -2,39 +2,38 @@
 require('../../include_first.php');
 
 if (empty($_REQUEST['id']) OR !is_numeric($_REQUEST['id'])) {
-	trigger_error('Der skal være sat et id på add_related_product.php', E_USER_ERROR);
+    trigger_error('Der skal være sat et id på add_related_product.php', E_USER_ERROR);
 }
 
 $module = $kernel->module("product");
 
 // hente liste med produkter - bør hentes med getList!
 
-
 if (!empty($_POST['product'])) {
-		foreach ($_POST['product'] AS $key=>$value) {
+        foreach ($_POST['product'] AS $key=>$value) {
 
 
-		$product = new Product($kernel, $_POST['id']);
-		if (!empty($_POST['relate'][$key]) AND $product->setRelatedProduct($_POST['product'][$key], $_POST['relate'][$key])) {
-		}
-	}
-	/*
-	if (isAjax()) {
-		echo '1';
-		exit;
-	}
+        $product = new Product($kernel, $_POST['id']);
+        if (!empty($_POST['relate'][$key]) AND $product->setRelatedProduct($_POST['product'][$key], $_POST['relate'][$key])) {
+        }
+    }
+    /*
+    if (isAjax()) {
+        echo '1';
+        exit;
+    }
 
-	else {
-	*/
-	if (!empty($_POST['close'])) {
-		header ('Location: product.php?id='.intval($_POST['id']));
-		exit;
-	}
+    else {
+    */
+    if (!empty($_POST['close'])) {
+        header ('Location: product.php?id='.intval($_POST['id']));
+        exit;
+    }
 
 
-		header('Location: related_product.php?id='.(int)$_POST['id']);
-		exit;
-	//}
+        header('Location: related_product.php?id='.(int)$_POST['id']);
+        exit;
+    //}
 
 }
 
@@ -44,20 +43,20 @@ $product->createDBQuery();
 
 $related_products = $product->getRelatedProducts();
 
-$keywords = $product->getKeywords();
+$keywords = $product->getKeywordAppender();
 
 if(isset($_GET["search"]) || isset($_GET["keyword_id"])) {
 
-	if(isset($_GET["search"])) {
-		$product->dbquery->setFilter("search", $_GET["search"]);
-	}
+    if(isset($_GET["search"])) {
+        $product->dbquery->setFilter("search", $_GET["search"]);
+    }
 
-	if(isset($_GET["keyword_id"])) {
-		$product->dbquery->setKeyword($_GET["keyword_id"]);
-	}
+    if(isset($_GET["keyword_id"])) {
+        $product->dbquery->setKeyword($_GET["keyword_id"]);
+    }
 }
 else {
-	$product->dbquery->useCharacter();
+    $product->dbquery->useCharacter();
 }
 
 $product->dbquery->defineCharacter("character", "detail.name");
@@ -76,76 +75,76 @@ $page->start("Tilføj relaterede varer");
 <p>... til <?php echo $product->get('name'); ?></p>
 
 <ul class="options">
-	<li><a href="product.php?id=<?php echo $_GET['id']; ?>&amp;from=related&amp;use_stored=true#related">Luk</a></li>
+    <li><a href="product.php?id=<?php echo $_GET['id']; ?>&amp;from=related&amp;use_stored=true#related">Luk</a></li>
 </ul>
 
 <form action="<?php echo basename(__FILE__); ?>" method="get">
-	<fieldset>
-		<legend>Søgning</legend>
-		<!--
-		<label>Filter
-		<select name="filter" id="filter">
-			<option>Ingen</option>
-			<option value="notpublished" <?php if (!empty($_GET['filter']) AND $_GET['filter'] == 'notpublished') echo ' selected="selected"'; ?>>Ikke udgivet</option>
-			<option value="webshop"<?php if (!empty($_GET['filter']) AND $_GET['filter'] == 'webshop') echo ' selected="selected"'; ?>>Webshop</option>
-			<option value="stock"<?php if (!empty($_GET['filter']) AND $_GET['filter'] == 'stock') echo ' selected="selected"'; ?>>Lager</option>
-		</select>
-		</label>
-		-->
-		<label>Søg efter
-		<input type="text" value="<?php print($product->dbquery->getFilter("search")); ?>" name="search" id="search" />
-	</label>
-	<label>
-		Vis med nøgleord
-		<select name="keyword_id" id="keyword_id">
-			<option value="">Ingen</option>
-			<?php foreach ($keywords->getUsedKeywords() AS $k) { ?>
-			<option value="<?php echo $k['id']; ?>" <?php if($k['id'] == $product->dbquery->getKeyword(0)) { echo ' selected="selected"'; }; ?>><?php echo $k['keyword']; ?></option>
-			<?php } ?>
-		</select>
-	</label>
-	<span>
-		<input type="submit" value="Afsted!" class="search" />
+    <fieldset>
+        <legend>Søgning</legend>
+        <!--
+        <label>Filter
+        <select name="filter" id="filter">
+            <option>Ingen</option>
+            <option value="notpublished" <?php if (!empty($_GET['filter']) AND $_GET['filter'] == 'notpublished') echo ' selected="selected"'; ?>>Ikke udgivet</option>
+            <option value="webshop"<?php if (!empty($_GET['filter']) AND $_GET['filter'] == 'webshop') echo ' selected="selected"'; ?>>Webshop</option>
+            <option value="stock"<?php if (!empty($_GET['filter']) AND $_GET['filter'] == 'stock') echo ' selected="selected"'; ?>>Lager</option>
+        </select>
+        </label>
+        -->
+        <label>Søg efter
+        <input type="text" value="<?php print($product->dbquery->getFilter("search")); ?>" name="search" id="search" />
+    </label>
+    <label>
+        Vis med nøgleord
+        <select name="keyword_id" id="keyword_id">
+            <option value="">Ingen</option>
+            <?php foreach ($keywords->getUsedKeywords() AS $k) { ?>
+            <option value="<?php echo $k['id']; ?>" <?php if($k['id'] == $product->dbquery->getKeyword(0)) { echo ' selected="selected"'; }; ?>><?php echo $k['keyword']; ?></option>
+            <?php } ?>
+        </select>
+    </label>
+    <span>
+        <input type="submit" value="Afsted!" class="search" />
     <input type="hidden" value="<?php echo $product->get('id'); ?>" name="id" />
-		</span>
-	</fieldset>
-	<br style="clear: both;" />
+        </span>
+    </fieldset>
+    <br style="clear: both;" />
 </form>
 
 <?php
 echo $product->dbquery->display('character');
 ?>
 <form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
-	<input type="hidden" name="id" value="<?php echo $product->get('id'); ?>" id="product_id" />
-	<table summary="Produkter" class="stripe">
-		<caption>Produkter</caption>
-		<thead>
-			<tr>
-				<th>Vælg</th>
-				<th>Varenummer</th>
-				<th>Navn</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($list AS $p) { ?>
-			<tr>
-				<td>
-					<input type="hidden" name="product[<?php echo $p['id']; ?>]" value="<?php echo $p['id']; ?>" />
-					<input class="input-relate" id="<?php echo $p['id']; ?>" type="checkbox" name="relate[<?php echo $p['id']; ?>]" value="relate" <?php if (array_key_exists($p['id'], $related_products)) echo ' checked="checked"'; ?> />
-				</td>
-				<td><?php echo htmlentities($p['number']); ?></td>
-				<td><?php echo htmlentities($p['name']); ?></td>
-			</tr>
-			<?php } // end foreach ?>
-		</tbody>
-	</table>
-  	<p>
-  		<input type="submit" value="Gem" />
-  		<input type="submit" value="Gem og luk" name="close" />
-  	</p>
+    <input type="hidden" name="id" value="<?php echo $product->get('id'); ?>" id="product_id" />
+    <table summary="Produkter" class="stripe">
+        <caption>Produkter</caption>
+        <thead>
+            <tr>
+                <th>Vælg</th>
+                <th>Varenummer</th>
+                <th>Navn</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($list AS $p) { ?>
+            <tr>
+                <td>
+                    <input type="hidden" name="product[<?php echo $p['id']; ?>]" value="<?php echo $p['id']; ?>" />
+                    <input class="input-relate" id="<?php echo $p['id']; ?>" type="checkbox" name="relate[<?php echo $p['id']; ?>]" value="relate" <?php if (array_key_exists($p['id'], $related_products)) echo ' checked="checked"'; ?> />
+                </td>
+                <td><?php echo htmlentities($p['number']); ?></td>
+                <td><?php echo htmlentities($p['name']); ?></td>
+            </tr>
+            <?php } // end foreach ?>
+        </tbody>
+    </table>
+      <p>
+          <input type="submit" value="Gem" />
+          <input type="submit" value="Gem og luk" name="close" />
+      </p>
 
   <?php echo $product->dbquery->display('paging'); ?>
-	</form>
+    </form>
 <?php
 $page->end();
 ?>
