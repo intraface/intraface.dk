30fc507d10ab9f255afb58da4ffc5873b30ae045
Lars Olesen <lars@intraface.dk>
2007-11-24 07:51:25 +0000

    New keyword implementation implemented.

--- intraface.dk/modules/contact/import.php
+++ intraface.dk/modules/contact/import.php
@@ -8,26 +8,28 @@ $translation = $kernel->getTranslation('contact');
 
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
+
     $data = $_SESSION['shared_fileimport_data'];
-    
+
     if(!is_array($data) || count($data) == 0) {
         trigger_error('This is not a valid dataset!', E_USER_ERROR);
         exit;
     }
-    
+
     $errors = array();
     $e = 0;
     $success = 0;
-    
+
     foreach($data AS $line => $contact) {
-        
+
         $contact_object = new Contact($kernel);
-        
+
         if($contact_object->save($contact)) {
-            
-            $contact_object->getKeywords();
-            $contact_object->keywords->addKeywordsByString($_POST['keyword']);
+
+            $appender = $contact_object->getKeywordAppender();
+            $string_appender = new Intraface_Keyword_StringAppender($contact_object->getKeyword(), $appender);
+
+            $string_appender->addKeywordsByString($_POST['keyword']);
             $success++;
         }
         else {
@@ -35,24 +37,24 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
             $errors[$e]['name'] = $contact['name'];
             $errors[$e]['email'] = $contact['email'];
             $errors[$e]['error'] = $contact_object->error;
-        }        
+        }
     }
-    
-    unset($_SESSION['shared_fileimport_data']);    
+
+    unset($_SESSION['shared_fileimport_data']);
 }
 else {
-    
+
     $redirect = Redirect::returns($kernel);
     if($redirect->getId('id') == 0) {
         trigger_error('we did not recive a redirect', E_USER_ERROR);
         exit;
     }
-    
+
     if($redirect->getParameter('session_variable_name') != 'shared_fileimport_data') {
         trigger_error('the session variable name must have been changed as it is not the same anymore: "'.$redirect->getParameter('session_variable_name').'"', E_USER_ERROR);
         exit;
     }
-    
+
     $data = $_SESSION['shared_fileimport_data'];
 }
 
@@ -61,49 +63,49 @@ $page->start($translation->get('import contacts'));
 ?>
 <h1><?php echo safeToHtml($translation->get('import contacts')); ?></h1>
 
-<?php /* echo $contact->error->view(); */ ?> 
+<?php /* echo $contact->error->view(); */ ?>
 
 <?php if(isset($success) && isset($errors)): ?>
 
     <fieldset>
         <legend><?php echo safeToHtml($translation->get('imported contacts')); ?></legend>
-            
-        <div><?php echo safeToHtml(sprintf($translation->get('%d records was imported successfully'), $success)); ?></div>          
+
+        <div><?php echo safeToHtml(sprintf($translation->get('%d records was imported successfully'), $success)); ?></div>
     </fieldset>
-    
+
     <h3><?php echo safeToHtml($translation->get('errors')); ?></h3>
-    
+
     <?php if(count($errors) == 0): ?>
-        <div><?php echo safeToHtml($translation->get('lucky you - no errors in import')); ?></div>          
+        <div><?php echo safeToHtml($translation->get('lucky you - no errors in import')); ?></div>
     <?php else: ?>
-        
-        <?php foreach($errors AS $error): ?>            
+
+        <?php foreach($errors AS $error): ?>
             <div><?php echo safeToHtml(sprintf($translation->get('error in line %d. unable to import %s <%s>'), $error['line'], $error['name'], $error['email'])); ?></div>
-            <?php echo $error['error']->view($translation); ?>       
+            <?php echo $error['error']->view($translation); ?>
         <?php endforeach; ?>
-    <?php endif; ?>    
+    <?php endif; ?>
 
 <?php else: ?>
 
     <form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
-      
-    
+
+
     <fieldset>
         <legend><?php echo safeToHtml($translation->get('data')); ?></legend>
-            
+
         <div><?php echo safeToHtml(sprintf($translation->get('there are %d records to import'), count($data))); ?></div>
-               
+
     </fieldset>
-    
+
     <fieldset>
         <legend><?php echo safeToHtml($translation->get('keywords')); ?></legend>
-        
+
         <div class="formrow">
             <label for="keyword"><?php echo safeToHtml($translation->get('keywords')); ?></label>
             <input type="text" name="keyword" id="keyword" value="" /> <?php echo safeToHtml($translation->get('separated by comma')); ?>
         </div>
     </fieldset>
-    
+
     <?php /* if($kernel->user->hasModuleAccess('newsletter')): ?>
         <?php
         $module_newsletter = $kernel->useModule('newsletter');
@@ -112,7 +114,7 @@ $page->start($translation->get('import contacts'));
         ?>
         <fieldset>
             <legend><?php echo safeToHtml($translation->get('newsletter')); ?></legend>
-            
+
             <?php foreach($list AS $newsletter): ?>
                 <div id="formrow">
                     <label for="newsletter_<?php echo intval($newsletter['id']); ?>"><?php echo safeToHtml($newsletter['title']); ?></label>
@@ -121,11 +123,11 @@ $page->start($translation->get('import contacts'));
             <?php endforeach; ?>
         </fieldset>
     <?php endif; */ ?>
-    
+
     <input type="submit" class="save" name="submit" value="<?php echo safeToHtml($translation->get('import')); ?>" />
-    <?php echo safeToHtml($translation->get('or', 'common')); ?> 
+    <?php echo safeToHtml($translation->get('or', 'common')); ?>
     <a href="<?php echo 'index.php'; ?>"><?php echo safeToHtml($translation->get('regret', 'common')); ?></a>
-    
+
     </form>
 <?php endif; ?>
 <?php
