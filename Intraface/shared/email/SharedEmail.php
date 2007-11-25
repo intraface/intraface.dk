a13392a4023c26c7869c7df630d63c36d8d21cf7
Lars Olesen <lars@intraface.dk>
2007-11-25 14:58:09 +0000

    changed constructor to php5

--- Intraface/shared/email/SharedEmail.php
+++ Intraface/shared/email/SharedEmail.php
@@ -4,20 +4,16 @@
  * @package <SystemMessage>
  * @author	<Sune>
  * @since	1.0
- * @version	1.0 
+ * @version	1.0
  *
  */
- 
+class SharedEmail Extends Shared
+{
+    function __construct()
+    {
+        $this->shared_name = 'email'; // Navn på på mappen med modullet
+        $this->active = 1; // Er shared aktivt
 
- 
-class SharedEmail Extends Shared {
-
-	function SharedEmail() {
-		$this->shared_name = 'email'; // Navn på på mappen med modullet
-		$this->active = 1; // Er shared aktivt
-		
-		$this->addPreloadFile('Email.php');
-	}
+        $this->addPreloadFile('Email.php');
+    }
 }
-
-?>
