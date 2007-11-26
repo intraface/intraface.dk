a7f050f83beb54169a256c54146b13f05b14bedd
Lars Olesen <lars@intraface.dk>
2007-11-26 15:19:42 +0000

    Changed the way the include paths are implemented.

--- Intraface/common.php
+++ Intraface/common.php
@@ -11,10 +11,10 @@
 //configuration - this should never be edited - only edit intraface.dk/config.local.php
 
 // paths
-if(!defined('PATH_INCLUDE_IHTML')) define('PATH_INCLUDE_IHTML', PATH_ROOT.'Intraface/ihtml' . DIRECTORY_SEPARATOR);
-if(!defined('PATH_INCLUDE_MODULE')) define('PATH_INCLUDE_MODULE', PATH_ROOT.'Intraface/modules' . DIRECTORY_SEPARATOR);
-if(!defined('PATH_INCLUDE_SHARED')) define('PATH_INCLUDE_SHARED', PATH_ROOT.'Intraface/shared' . DIRECTORY_SEPARATOR);
-if(!defined('PATH_INCLUDE_CONFIG')) define('PATH_INCLUDE_CONFIG', PATH_ROOT.'/Intraface/config'.DIRECTORY_SEPARATOR);
+if(!defined('PATH_INCLUDE_IHTML')) define('PATH_INCLUDE_IHTML', dirname(__FILE__).'/ihtml' . DIRECTORY_SEPARATOR);
+if(!defined('PATH_INCLUDE_MODULE')) define('PATH_INCLUDE_MODULE', dirname(__FILE__).'Intraface/modules' . DIRECTORY_SEPARATOR);
+if(!defined('PATH_INCLUDE_SHARED')) define('PATH_INCLUDE_SHARED', dirname(__FILE__).'Intraface/shared' . DIRECTORY_SEPARATOR);
+if(!defined('PATH_INCLUDE_CONFIG')) define('PATH_INCLUDE_CONFIG', dirname(__FILE__).'/Intraface/config'.DIRECTORY_SEPARATOR);
 if(!defined('PATH_UPLOAD_TEMPORARY')) define('PATH_UPLOAD_TEMPORARY', 'tempdir/'); // Mappen i Upload_path, under intranetid, hvor temp-filer placeres.
 define('PATH_CAPTCHA', PATH_ROOT . 'captcha/'); // remember trailing slash - used for the demo formular
 if(!defined('PATH_CACHE')) {
