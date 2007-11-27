85b623282b1058813326d5bd06bee82f2163b99a
Lars Olesen <lars@intraface.dk>
2007-11-27 07:45:53 +0000

    Added realpath to the whitelist stuff.

--- tests/unit/AllTests.php
+++ tests/unit/AllTests.php
@@ -2,29 +2,25 @@
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
+PHPUnit_Util_Filter::addDirectoryToWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/'), '.php');
+PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/certificates/'), '.php');
+PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/3Party/'), '.php');
+PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/config/'), '.php');
+PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/ihtml/'), '.php');
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/accounting/include_frontpage.php'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/administration/include_frontpage.php'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/contact/include_frontpage.php'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/invoice/include_front.php'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/onlinepayment/include_front.php'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/order/include_front.php'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/procurement/include_frontpage.php'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/product/include_frontpage.php'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../Intraface/modules/todo/include_frontpage.php'));
+
 class AllTests
 {
     public static function suite()
     {
-        /*
-        PHPUnit_Util_Filter::addDirectoryToFilter('c:/wamp/php/', '.php');
-
-        PHPUnit_Util_Filter::addDirectoryToWhitelist(dirname(__FILE__) . '/../../Intraface/', '.php');
-        PHPUnit_Util_Filter::removeDirectoryFromWhitelist(dirname(__FILE__) . '/../../Intraface/certificates/', '.php');
-        PHPUnit_Util_Filter::removeDirectoryFromWhitelist(dirname(__FILE__) . '/../../Intraface/3Party/', '.php');
-        PHPUnit_Util_Filter::removeDirectoryFromWhitelist(dirname(__FILE__) . '/../../Intraface/config/', '.php');
-        PHPUnit_Util_Filter::removeDirectoryFromWhitelist(dirname(__FILE__) . '/../../Intraface/ihtml/', '.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/accounting/include_frontpage.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/administration/include_frontpage.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/contact/include_frontpage.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/invoice/include_front.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/onlinepayment/include_front.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/order/include_front.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/procurement/include_frontpage.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/product/include_frontpage.php');
-        PHPUnit_Util_Filter::removeFileFromWhitelist(dirname(__FILE__) . '/../../Intraface/modules/todo/include_frontpage.php');
-        */
-
         $suite = new PHPUnit_Framework_TestSuite('Intraface');
 
         $tests = array('Product',
