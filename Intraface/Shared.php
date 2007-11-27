011aebaf470af7bf9a3f2d387c7374f0379a0265
Lars Olesen <lars@intraface.dk>
2007-11-27 08:16:31 +0000

    Improved coding standards and added some todos.

--- Intraface/Shared.php
+++ Intraface/Shared.php
@@ -5,32 +5,28 @@
  * @author Sune Jensen <sj@sunet.dk>
  * @example SharedExample.php
  */
+class Shared
+{
+    protected $active;
+    protected $preload_file = array();
+    protected $shared_name;
+    protected $setting;
+    protected $controlpanel_files;
+    protected $frontpage_files; // til brug på forsiden
+    protected $translation;
 
-class Shared {
-
-    var $active;
-    var $preload_file = array();
-    var $shared_name;
-    var $setting;
-    var $controlpanel_files;
-    var $frontpage_files; // til brug på forsiden
-    var $translation;
-
-
-    function __construct() {
+    public function __construct() {
         // init
         $this->shared_name = '';
         $this->active = 0;
     }
 
     /**
-     * Denne funktion kï¿½res af kernel, nï¿½r man loader shared
-     *
+     * Is run by kernel when loading a shared module
      */
-
-    function load() {
+    public function load()
+    {
         // Inkluder preload filerne
-
         for($i = 0, $max = count($this->preload_file); $i<$max; $i++) {
             $this->includeFile($this->preload_file[$i]);
         }
@@ -40,36 +36,42 @@ class Shared {
      * Denne funktion bruges af SharedNavn.php til at fortï¿½lle, hvor includefilen til
    * det enkelte modul ligger.
      */
-    function addFrontpageFile($filename) {
+    function addFrontpageFile($filename)
+    {
         $this->frontpage_files[] = $filename;
     }
 
-    function getFrontpageFiles() {
+    function getFrontpageFiles()
+    {
         return $this->frontpage_files;
     }
 
-    function addControlpanelFile($title, $url) {
+    function addControlpanelFile($title, $url)
+    {
         $this->controlpanel_files[] = array(
             'title' => $title,
             'url' => $url
         );
     }
 
-    function getControlpanelFiles() {
+    function getControlpanelFiles()
+    {
         return $this->controlpanel_files;
     }
 
-    function addPreloadFile($file) {
+    function addPreloadFile($file)
+    {
         $this->preload_file[] = $file;
     }
 
     /**
-   * Bruges til at inkludere fil
-   *
-   * ï¿½ndret med at tjekke om filen eksisterer.
-   */
-    function includeFile($file) {
-        $file = PATH_INCLUDE_SHARED.$this->shared_name."/".$file;
+     * Bruges til at inkludere fil
+     *
+     * @todo why is this not using getPath?
+     */
+    function includeFile($file)
+    {
+        $file = PATH_INCLUDE_SHARED . $this->shared_name . '/' . $file;
         if (!file_exists($file)) {
             return 0;
         }
@@ -77,50 +79,36 @@ class Shared {
         return 1;
     }
 
-    /*
-    // virker det her endnu? // lars
-    function addDependentModule($module) {
-        $this->dependent_module[] = $module;
+    /**
+     * @todo why is this not using getPath()
+     */
+    function includeSettingFile($file)
+    {
+        global $_setting; // this is also globalized other places
+        require(PATH_INCLUDE_SHARED . $this->shared_name . '/' . $file);
     }
 
-    function getDependentModules() {
-        return $this->dependent_module;
-    }
-    */
-
-    function includeSettingFile($file) {
-        global $_setting; // den globaliseres ogsï¿½ andre steder?
-        require(PATH_INCLUDE_SHARED.$this->shared_name."/".$file);
-    }
-
-    function getPath() {
+    /**
+     * @todo could this not resolve the path automatically, for instance through a dirname()
+     *       would do it possible to drop a constant.
+     */
+    function getPath()
+    {
         return(PATH_WWW_SHARED.$this->shared_name."/");
     }
 
     /**
      * Bruges til at tilføje en setting til et modul, som skal være hardcoded ind i Main[Modulnavn]
      */
-    function addSetting($key, $value) {
+    function addSetting($key, $value)
+    {
         $this->setting[$key] = $value;
     }
 
-    function getSetting($key) {
+    function getSetting($key)
+    {
         if(isset($this->setting[$key])) {
             return($this->setting[$key]);
         }
     }
-/*
-  function addTranslation($shortterm, $translation) {
-    $this->translation[$shortterm] = $translation;
-  }
-
-  function getTranslation($shortterm) {
-    if (!empty($this->translation[$shortterm])) {
-      return $this->translation[$shortterm];
-    }
-    return $shortterm;
-  }
-*/
 }
-
-?>
