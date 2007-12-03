3f9e5076587577ca56c67de6236ac9460da7cbbc
Sune T. Jensen <sj@sunet.dk>
2007-12-03 18:50:23 +0000

    Added new functionality

--- install/Helper/IntranetMaintenance.php
+++ install/Helper/IntranetMaintenance.php
@@ -2,13 +2,20 @@
 
 class Install_Helper_IntranetMaintenance {
     
+    private $kernel;
+    private $db;
     
-    public function loadPackages() {
+    public function __construct($kernel, $db) 
+    {
+        $this->kernel = $kernel;
+        $this->db = $db;
+    }
+    
+    public function loadPackages() 
+    {
         
-        $db = MDB2::singleton(DB_DSN);
-        
-        $sql_structure = file_get_contents(dirname(__FILE__) . '../database-module_package-values.sql');
-        $sql_arr = Install::splitSql($sql_structure);
+        $sql_structure = file_get_contents(dirname(__FILE__) . '/../database-module_package-values.sql');
+        $sql_arr = Intraface_Install::splitSql($sql_structure);
 
         foreach($sql_arr as $sql) {
             if(empty($sql)) { continue; }
@@ -18,6 +25,32 @@ class Install_Helper_IntranetMaintenance {
                 exit;
             }
         }
+        
+        require_once 'Intraface/modules/product/Product.php';
+        $product = new Product($this->kernel);
+        
+        $product->save(array('name' => 'Small CMS', 'unit' => 4, 'vat' => 1, 'price' => 10));
+        $product->save(array('name' => 'Medium CMS', 'unit' => 4, 'vat' => 1, 'price' => 30));
+        $product->save(array('name' => 'Complete CMS', 'unit' => 4, 'vat' => 1, 'price' => 60));
+        
+        
+    }
+    
+    public function createAlternativeIntranet() 
+    {
+        
+        require_once 'Intraface/modules/intranetmaintenance/IntranetMaintenance.php';
+        $intranet = new IntranetMaintenance();
+        $intranet->save(array('name' => 'Test', 'identifier' => 'test'), 1);
+        $intranet->setModuleAccess('administration', 2);
+        $intranet->setModuleAccess('modulepackage', 2);
+        
+        require_once 'Intraface/modules/intranetmaintenance/UserMaintenance.php';
+        $user = new UserMaintenance(1);
+        $user->setIntranetAccess(2);
+        $user->setModuleAccess('administration', 2);
+        $user->setModuleAccess('modulePackage', 2);
+        
     }
 }
 
