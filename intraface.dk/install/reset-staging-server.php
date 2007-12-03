88645cacec53eb8a4d8acc4f2405283d2381d4a2
Sune T. Jensen <sj@sunet.dk>
2007-12-03 15:06:02 +0000

    possibility for granting access to modules now possible to log the user in now possible to run helper functions to set up intranet Added Intranetmaintenacne Helper function

--- intraface.dk/install/reset-staging-server.php
+++ intraface.dk/install/reset-staging-server.php
@@ -11,6 +11,24 @@ require $install_class;
 $install = new Intraface_Install;
 
 if ($install->resetServer()) {
+    
+    if(!empty($_GET['modules'])) {
+        $install->grantModuleAccess($_GET['modules']);
+    }
+    
+    if(!empty($_GET['helper_function'])) {
+        $install->runHelperFunction($_GET['helper_function']);
+    }
+    
+    if(!empty($_GET['login'])) {
+        if($install->loginUser()) {
+            header('location: '.PATH_WWW.'/main/index.php');
+            exit;
+        }
+        else {
+            echo 'Error in login';
+        }
+    }
     echo 'staging server reset. Go to <a href="../main/login.php">login</a>.';
 }
 else {
