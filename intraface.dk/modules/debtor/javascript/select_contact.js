19e6afb12c75a373f43bae359e59d1535bcd2839
Lars Olesen <lars@intraface.dk>
2007-12-04 16:50:09 +0000

    Bugfix. Makes sure that it understands that the call is ajax on search.

--- intraface.dk/modules/debtor/javascript/select_contact.js
+++ intraface.dk/modules/debtor/javascript/select_contact.js
@@ -3,100 +3,80 @@ YAHOO.util.Event.addListener(window, "load", init);
 var compatible = document.getElementById;
 
 function init() {
-	if (!compatible) return;
-	var oSearch = document.getElementById("search");
-	if(!oSearch) return;
-	
-	/*
-	var oButton = document.getElementById("search_button");
-	if(oButton) oButton.style.display="none";
-	*/
-	
-	var oSelect = document.getElementById("contact_select");
-	if (oSelect) oSelect.style.display = "none";   
-	
-	/*  
-	var oNewButton = document.getElementById("new_contact");
-	if(oNewButton) oNewButton.onclick = function() { 
-  		var oType = document.getElementById("type_select");
-    	if (!oType) return false; 
-    	if (!oType.value) { 
-			return false; 
-   		} 
-    	else { 
-    		return true; 
-    	} 
-	}
-	*/
-  
-  	var oForm = document.getElementById("new");
-  	if (!oForm) return;
-	oForm.onsubmit = function() {
-		var oType = document.getElementById("type_select");
-  		if (!oType.value) {
-    		var oLabel = document.getElementById("type_label");
-     		oType.focus(); 
-			if (!oLabel) return;
-			oLabel.style.color = "red";
-    		return false;
-    	}
-	}
+    if (!compatible) return;
+    var oSearch = document.getElementById("search");
+    if(!oSearch) return;
 
-	oSearch.onkeyup = searchContact;
+    var oSelect = document.getElementById("contact_select");
+    if (oSelect) oSelect.style.display = "none";
+
+    var oForm = document.getElementById("new");
+    if (!oForm) return;
+
+    oForm.onsubmit = function() {
+    var oType = document.getElementById("type_select");
+    if (!oType.value) {
+        var oLabel = document.getElementById("type_label");
+        oType.focus();
+        if (!oLabel) return;
+            oLabel.style.color = "red";
+            return false;
+        }
+    }
+
+    oSearch.onkeyup = searchContact;
 }
 
 // det bør laves på en time i stedet for keyup
 function searchContact() {
-	var oSearch = document.getElementById("search");
-	if (!oSearch) return;
-	if(oSearch.value.length < 3) return true;
-	
-	var oXMLHttp = XMLHttp();
-	if (!oXMLHttp) return true;
-  
-	var oSelect = document.getElementById("contact_select");
-	if (!oSelect) return;
-	oSelect.style.display = "block";
-	 
-	oXMLHttp.onreadystatechange = function () {
-		oSelect.options.length = 0;
-		if(oXMLHttp.readyState == 4) {
-			if(oXMLHttp.status == 200) {
-				if(oXMLHttp.responseText != "") {
-					var xmldoc = XMLParser(oXMLHttp.responseText);
-					var items = xmldoc.getElementsByTagName("contact");
-					if (items.length > 0) {
-						for (i=0;i<items.length;i++) {
-							var name = items[i].getElementsByTagName("name");	
-							var id = items[i].getElementsByTagName("id");	
-							var entry = new Option(name[0].firstChild.nodeValue, id[0].firstChild.nodeValue, 0, 0);
-							oSelect.options[oSelect.options.length] = entry;
-						}
-          			}
-          			else {
-          				var entry = new Option("Ingen fundet...", 1, 0, 0);
-						oSelect.options[oSelect.options.length] = entry;      
-					}
-				}
-			}
-		}
-		else {
-			//oSelect.innerHTML = '<option value="">Søger...</option>';
-			var entry = new Option("Søger...", 1, 0, 0);
-			oSelect.options[oSelect.options.length] = entry;      
-		}
-	}
-	
+    var oSearch = document.getElementById("search");
+    if (!oSearch) return;
+    if(oSearch.value.length < 3) return true;
 
-	// if (!search) return true;
-	// var url = "/modules/debtor/select_contact.php";
-	var url = document.getElementById('new').action;
-	var var_send = "search="+encodeURIComponent(oSearch.value);  
-	oXMLHttp.open("POST", url, true);
-	oXMLHttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded;');
-	oXMLHttp.setRequestHeader('Content-Length', var_send.length);  
-	oXMLHttp.setRequestHeader('Accept','message/x-jl-formresult');
-	oXMLHttp.send(var_send);
-	
-	return false;
-}	
+    var oXMLHttp = XMLHttp();
+    if (!oXMLHttp) return true;
+
+    var oSelect = document.getElementById("contact_select");
+    if (!oSelect) return;
+    oSelect.style.display = "block";
+
+    oXMLHttp.onreadystatechange = function () {
+        oSelect.options.length = 0;
+        if(oXMLHttp.readyState == 4) {
+            if(oXMLHttp.status == 200) {
+                if(oXMLHttp.responseText != "") {
+                    var xmldoc = XMLParser(oXMLHttp.responseText);
+                    var items = xmldoc.getElementsByTagName("contact");
+                    if (items.length > 0) {
+                        for (i=0;i<items.length;i++) {
+                            var name = items[i].getElementsByTagName("name");
+                            var id = items[i].getElementsByTagName("id");
+                            var entry = new Option(name[0].firstChild.nodeValue, id[0].firstChild.nodeValue, 0, 0);
+                            oSelect.options[oSelect.options.length] = entry;
+                        }
+                    } else {
+                        var entry = new Option("Ingen fundet...", 1, 0, 0);
+                        oSelect.options[oSelect.options.length] = entry;
+                    }
+                }
+            }
+        } else {
+            //oSelect.innerHTML = '<option value="">Søger...</option>';
+            var entry = new Option("Søger...", 1, 0, 0);
+            oSelect.options[oSelect.options.length] = entry;
+        }
+    }
+
+
+    // if (!search) return true;
+    // var url = "/modules/debtor/select_contact.php";
+    var url = document.getElementById('new').action;
+    var var_send = "search="+encodeURIComponent(oSearch.value)+"&ajax=1";
+    oXMLHttp.open("POST", url, true);
+    oXMLHttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded;');
+    oXMLHttp.setRequestHeader('Content-Length', var_send.length);
+    oXMLHttp.setRequestHeader('Accept','message/x-jl-formresult');
+    oXMLHttp.send(var_send);
+
+    return false;
+}
--- intraface.dk/modules/debtor/select_contact.php
+++ intraface.dk/modules/debtor/select_contact.php
@@ -6,105 +6,101 @@ $contact_module = $kernel->useModule('contact');
 $error = new Error();
 
 if(isset($_POST["search"]) && $_POST["search"] != "") {
-	$contact = new Contact($kernel);
+    $contact = new Contact($kernel);
     $contact->createDBQuery();
 
-	$contact->dbquery->setFilter("search", $_POST["search"]);
-	$contacts = $contact->getList($_POST["search"]);
+    $contact->dbquery->setFilter("search", $_POST["search"]);
+    $contacts = $contact->getList($_POST["search"]);
 
+    $options = '';
 
-  $options = '';
+    if (isAjax()) {
+        header("Content-type: text/xml");
+        print("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\r\n");
+        print("<searchresult>\n");
+        for ($i = 0, $max = count($contacts); $i < $max; $i++) {
+            print("<contact>\n");
+            print("<id>".safeToHtml($contacts[$i]["id"])."</id>\n");
+            print("<name>".safeToHtml($contacts[$i]["name"])."</name>\n");
+            print("</contact>\n");
+        }
+        print("</searchresult>");
 
-  if (isAjax()) {
-		header("Content-type: text/xml");
-		print("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\r\n");
-		print("<searchresult>\n");
-
-		for($i = 0, $max = count($contacts); $i < $max; $i++) {
-			print("<contact>\n");
-			print("<id>".safeToHtml($contacts[$i]["id"])."</id>\n");
-			print("<name>".safeToHtml($contacts[$i]["name"])."</name>\n");
-			print("</contact>\n");
-		}
-		print("</searchresult>");
-
-    exit;
-  }
-  else {
-  	for($i = 0, $max = count($contacts); $i < $max; $i++) {
-  		$options .= '<option value="'.$contacts[$i]["id"].'">';
-  		$options .= $contacts[$i]["name"];
-  		$options .= "</option>\n";
-  	}
-  }
+        exit;
+    } else {
+        for($i = 0, $max = count($contacts); $i < $max; $i++) {
+            $options .= '<option value="'.$contacts[$i]["id"].'">';
+            $options .= $contacts[$i]["name"];
+            $options .= "</option>\n";
+        }
+    }
 
 }
-
 
 if(isset($_POST["go_search"])) {
-	/*
-	if($_POST["type"] == "reminder" && $kernel->user->hasModuleAccess("invoice")) {
-		header("Location: reminder_edit.php?contact_id=".$_POST["contact"]."");
-		exti;
-	}
-	*/
-	if(($_POST["type"] == "quotation" || $_POST["type"] == "order" || $_POST["type"] == "invoice") && $kernel->user->hasModuleAccess($_POST["type"])) {
-		if (empty($_POST['contact']) OR !is_numeric($_POST['contact'])) {
-			$error->set('Du skal vælge en kontakt');
-		}
-		if (!$error->isError()) {
-			header("Location: edit.php?type=".$_POST["type"]."&contact_id=".$_POST["contact"]."");
-			exit;
-		}
-		else {
-			$value = $_POST;
-			$type = $_POST['type'];
-		}
-	}
-	else {
-		trigger_error("Ugyldig type", E_USER_ERROR);
-	}
+    /*
+    if($_POST["type"] == "reminder" && $kernel->user->hasModuleAccess("invoice")) {
+        header("Location: reminder_edit.php?contact_id=".$_POST["contact"]."");
+        exti;
+    }
+    */
+    if(($_POST["type"] == "quotation" || $_POST["type"] == "order" || $_POST["type"] == "invoice") && $kernel->user->hasModuleAccess($_POST["type"])) {
+        if (empty($_POST['contact']) OR !is_numeric($_POST['contact'])) {
+            $error->set('Du skal vælge en kontakt');
+        }
+        if (!$error->isError()) {
+            header("Location: edit.php?type=".$_POST["type"]."&contact_id=".$_POST["contact"]."");
+            exit;
+        }
+        else {
+            $value = $_POST;
+            $type = $_POST['type'];
+        }
+    }
+    else {
+        trigger_error("Ugyldig type", E_USER_ERROR);
+    }
 }
 elseif(isset($_POST["new_contact"])) {
-	if($_POST["type"] == "quotation" || $_POST["type"] == "order" || $_POST["type"] == "invoice") {
+    if($_POST["type"] == "quotation" || $_POST["type"] == "order" || $_POST["type"] == "invoice") {
 
-		$redirect = new Redirect($kernel);
-		$url = $redirect->setDestination($contact_module->getPath().'contact_edit.php', $module->getPath().'edit.php?type='.$_POST['type']); // contact_id sættes på fra contact_edit
-		$redirect->askParameter('contact_id');
-		header('Location: ' . $url);
-		exit;
+        $redirect = new Redirect($kernel);
+        $url = $redirect->setDestination($contact_module->getPath().'contact_edit.php', $module->getPath().'edit.php?type='.$_POST['type']); // contact_id sættes på fra contact_edit
+        $redirect->askParameter('contact_id');
+        header('Location: ' . $url);
+        exit;
 
-		/*
-		header("location: /modules/contact/contact_edit.php?go_to=".$_POST["type"]);
-		exit;
-		*/
-	}
-	else {
-		trigger_error("Ugyldig type", E_USER_ERROR);
-	}
+        /*
+        header("location: /modules/contact/contact_edit.php?go_to=".$_POST["type"]);
+        exit;
+        */
+    }
+    else {
+        trigger_error("Ugyldig type", E_USER_ERROR);
+    }
 }
 else {
 
-	if(isset($_GET["type"])) {
+    if(isset($_GET["type"])) {
 
-		if($_GET["type"] == "reminder" && $kernel->user->hasModuleAccess("invoice")) {
-			$type = "reminder";
-		}
-		elseif(($_GET["type"] == "quotation" || $_GET["type"] == "order" || $_GET["type"] == "invoice") && $kernel->user->hasModuleAccess($_GET["type"])) {
-			$type = $_GET["type"];
-		}
-		else {
-			$type = "";
-		}
-	}
-	else {
-		$type = "";
-	}
+        if($_GET["type"] == "reminder" && $kernel->user->hasModuleAccess("invoice")) {
+            $type = "reminder";
+        }
+        elseif(($_GET["type"] == "quotation" || $_GET["type"] == "order" || $_GET["type"] == "invoice") && $kernel->user->hasModuleAccess($_GET["type"])) {
+            $type = $_GET["type"];
+        }
+        else {
+            $type = "";
+        }
+    }
+    else {
+        $type = "";
+    }
 }
 
 $page = new Page($kernel);
-$page->includeJavascript('module', 'select_contact.js');
 $page->includeJavascript('global', 'XMLHttp.js');
+$page->includeJavascript('module', 'select_contact.js');
 $page->start('Opret ny');
 ?>
 
@@ -116,20 +112,20 @@ $page->start('Opret ny');
 <table>
 <thead>
 <tr>
-	<th>Hvad?</th>
-	<th>Til?</th>
-	<th></th>
+    <th>Hvad?</th>
+    <th>Til?</th>
+    <th></th>
 </tr>
 </thead>
 <tbody>
 <tr>
   <td>
   <label id="type_label">Opret
-  	<select name="type" id="type_select">
+      <select name="type" id="type_select">
     <option value="">Vælg</option>
-    	<option value="quotation" <?php if($type == "quotation") print("selected=\"selected\""); ?>>Tilbud</option>
-    	<option value="order" <?php if($type == "order") print("selected=\"selected\""); ?>>Ordre</option>
-    	<option value="invoice" <?php if($type == "invoice") print("selected=\"selected\""); ?>>Faktura</option>
+        <option value="quotation" <?php if($type == "quotation") print("selected=\"selected\""); ?>>Tilbud</option>
+        <option value="order" <?php if($type == "order") print("selected=\"selected\""); ?>>Ordre</option>
+        <option value="invoice" <?php if($type == "invoice") print("selected=\"selected\""); ?>>Faktura</option>
     </select>
    </label>
  </td>
@@ -139,11 +135,11 @@ $page->start('Opret ny');
 <?php $contact = new Contact($kernel); if ($contact->isFilledIn()): ?>
 eller
 <label>søg
-		<input type="text" name="search" id="search" value="<?php if (!empty($_GET['search'])) echo safeToForm($_GET['search']); ?>" />
+        <input type="text" name="search" id="search" value="<?php if (!empty($_GET['search'])) echo safeToForm($_GET['search']); ?>" />
 </label>
 
-		<input type="submit" value="Søg" id="search_button" />
-		<select name="contact" size="4" id="contact_select" style="width:300px;"><?php if(isset($options)) echo $options; ?></select>
+        <input type="submit" value="Søg" id="search_button" />
+        <select name="contact" size="4" id="contact_select" style="width:300px;"><?php if(isset($options)) echo $options; ?></select>
 <?php endif; ?>
   </td>
 
