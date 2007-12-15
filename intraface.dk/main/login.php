91f8fdf08bb57784c71073d21d4d199e37facc5a
Lars Olesen <lars@intraface.dk>
2007-12-15 18:29:51 +0000

    Fixed link so it points to signup.

--- intraface.dk/main/login.php
+++ intraface.dk/main/login.php
@@ -6,29 +6,29 @@ $title = 'Intraface.dk -> Login';
 
 
 if(isset($_POST['email']) AND isset($_POST['password'])) {
-	session_start();
+    session_start();
 
-	$log = new KernelLog;
+    $log = new KernelLog;
 
-	$auth = new Auth(session_id());
-	$auth->attachObserver($log);
+    $auth = new Auth(session_id());
+    $auth->attachObserver($log);
 
 
-	$error = $auth->login($_POST['email'], $_POST['password']);
-	if ($error === true) {
-		header('Location: '.PATH_WWW.'main/index.php');
-		exit;
-	}
-	else {
-		switch ($error) {
-			case LOGIN_ERROR_ALREADY_LOGGED_IN:
-				$msg = 'already logged in as another user. please logout before logging in.';
-			break;
-			case LOGIN_ERROR_WRONG_CREDENTIALS:
-				$msg = 'wrong credentials';
-			break;
-		}
-	}
+    $error = $auth->login($_POST['email'], $_POST['password']);
+    if ($error === true) {
+        header('Location: '.PATH_WWW.'main/index.php');
+        exit;
+    }
+    else {
+        switch ($error) {
+            case LOGIN_ERROR_ALREADY_LOGGED_IN:
+                $msg = 'already logged in as another user. please logout before logging in.';
+            break;
+            case LOGIN_ERROR_WRONG_CREDENTIALS:
+                $msg = 'wrong credentials';
+            break;
+        }
+    }
 
 }
 include(PATH_INCLUDE_IHTML . 'outside/top.php');
@@ -39,45 +39,45 @@ include(PATH_INCLUDE_IHTML . 'outside/top.php');
 
 <form id="form-login" method="post" action="<?php echo basename($_SERVER['PHP_SELF']); ?>">
 
-	<fieldset>
-	<?php
-		if (!empty($msg)) {
-			echo '<p>'.htmlspecialchars(strip_tags($msg)).'</p>';
-		}
-		/*
-		else {
-			$actual = SystemDisturbance::getActual();
-			if(count($actual) > 0 && $actual['important'] == 1) {
-				echo '<p id="system_message">'.htmlspecialchars($actual['description']).'</p>';
-			}
-		}
-		*/
+    <fieldset>
+    <?php
+        if (!empty($msg)) {
+            echo '<p>'.htmlspecialchars(strip_tags($msg)).'</p>';
+        }
+        /*
+        else {
+            $actual = SystemDisturbance::getActual();
+            if(count($actual) > 0 && $actual['important'] == 1) {
+                echo '<p id="system_message">'.htmlspecialchars($actual['description']).'</p>';
+            }
+        }
+        */
 
-		?>
+        ?>
 
 
-		<div class="align-left">
-			<label for="email" id="email_label">E-mail:</label>
-			<input tabindex="1" type="text" name="email" id="email" value="<?php if (!empty($_COOKIE['username'])) echo htmlentities(strip_tags($_COOKIE['username'])); ?>" />
-		</div>
-		<div>
-			<label for="password" id="password_label">Adgangskode:</label>
-			<input tabindex="2" type="password" name="password" id="password" value="<?php if (!empty($_COOKIE['password'])) echo htmlentities(strip_tags($_COOKIE['password'])); ?>" />
-		</div>
+        <div class="align-left">
+            <label for="email" id="email_label">E-mail:</label>
+            <input tabindex="1" type="text" name="email" id="email" value="<?php if (!empty($_COOKIE['username'])) echo htmlentities(strip_tags($_COOKIE['username'])); ?>" />
+        </div>
+        <div>
+            <label for="password" id="password_label">Adgangskode:</label>
+            <input tabindex="2" type="password" name="password" id="password" value="<?php if (!empty($_COOKIE['password'])) echo htmlentities(strip_tags($_COOKIE['password'])); ?>" />
+        </div>
 
-		<div>
-			<input tabindex="3" type="submit" value="Login" id="submit" /> <a tabindex="4" href="forgotten_password.php">Glemt password?</a>
-		</div>
+        <div>
+            <input tabindex="3" type="submit" value="Login" id="submit" /> <a tabindex="4" href="forgotten_password.php">Glemt password?</a>
+        </div>
 
-	</fieldset>
+    </fieldset>
 
-	<p style="text-align: center;">
-		<a href="http://blog.intraface.dk/">Website</a> |
-		<a href="http://blog.intraface.dk/blog/">Nyheder</a> |
-		<a href="http://blog.intraface.dk/kontakt/">Kontakt</a> |
-		<a href="<?php echo PATH_WWW; ?>demo/">Prøv systemet</a>
-	</p>
-	<!--<p>Intraface.dk er et system målrettet til mindre virksomheder. Forskellige moduler klarer både webshop, fakturaer, regnskab og nyhedsbreve.</p>-->
+    <p style="text-align: center;">
+        <a href="http://blog.intraface.dk/">Website</a> |
+        <a href="http://blog.intraface.dk/blog/">Nyheder</a> |
+        <a href="http://blog.intraface.dk/kontakt/">Kontakt</a> |
+        <a href="<?php echo PATH_WWW; ?>signup/">Prøv systemet</a>
+    </p>
+    <!--<p>Intraface.dk er et system målrettet til mindre virksomheder. Forskellige moduler klarer både webshop, fakturaer, regnskab og nyhedsbreve.</p>-->
 
 </form>
 
