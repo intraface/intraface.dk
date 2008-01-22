0fee3a74622b731a44e87c769feae6148e63ce80
Sune T. Jensen <sj@sunet.dk>
2008-01-22 10:57:47 +0000

    Added setting for accounting

--- Intraface/modules/invoice/settings.php
+++ Intraface/modules/invoice/settings.php
@@ -3,5 +3,11 @@
  * @package Intraface_Invoice
  */
 
-$_setting["reminder.first.text"] = "Der ser ud til, at du endnu ikke har betalt nedenstående. Hvis du har betalt fakturaen, beder vi dig skrive datoen for betaling til os."
+$_setting["reminder.first.text"] = "Der ser ud til, at du endnu ikke har betalt nedenstående. Hvis du har betalt fakturaen, beder vi dig skrive datoen for betaling til os.";
+$_setting["payment.state.depriciation.account"] = 0;
+$_setting["payment.state.bank_transfer.account"] = 0;
+$_setting["payment.state.giro_transfer.account"] = 0;
+$_setting["payment.state.credit_card.account"] = 0;
+$_setting["payment.state.cash.account"] = 0;
+
 ?>
