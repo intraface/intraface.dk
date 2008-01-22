7ed8d0e888eb8cd9a4db7fac6c5ff9949d82df8f
Sune T. Jensen <sj@sunet.dk>
2008-01-22 10:44:54 +0000

    Updated testsuite

--- tests/unit/AllTests.php
+++ tests/unit/AllTests.php
@@ -43,21 +43,8 @@ class AllTests
 
         foreach ($tests AS $test) {
             require_once strtolower($test) . '/AllTests.php';
+            $suite->addTest(call_user_func(array($test.'_AllTests', 'suite')));
         }
-        $suite->addTest(Accounting_AllTests::suite());
-        $suite->addTest(CMS_AllTests::suite());
-        $suite->addTest(Common_AllTests::suite());
-        $suite->addTest(Contact_AllTests::suite());
-        $suite->addTest(Email_AllTests::suite());
-        $suite->addTest(Filehandler_AllTests::suite());
-        $suite->addTest(IntranetMaintenance_AllTests::suite());
-        $suite->addTest(Newsletter_AllTests::suite());
-        $suite->addTest(Product_AllTests::suite());
-        $suite->addTest(Webshop_AllTests::suite());
-        $suite->addTest(Debtor_AllTests::suite());
-        $suite->addTest(Shared_AllTests::suite());
-        $suite->addTest(Keyword_AllTests::suite());
-        $suite->addTest(Stock_AllTests::suite());
         return $suite;
     }
 }
