12bfde772ac817d1f7c1bcac6b3a56ccd216b6f1
Sune T. Jensen <sj@sunet.dk>
2008-01-25 13:00:44 +0000

    Added helper for debtor

---
+++ install/Helper/Debtor.php
@@ -0,0 +1,58 @@
+<?php
+class Install_Helper_Debtor {
+    
+    private $kernel;
+    private $db;
+
+    public function __construct($kernel, $db) {
+        $this->kernel = $kernel;
+        $this->db = $db;
+    }
+    
+    public function createInvoice() {
+        
+        require_once 'install/Helper/Contact.php';
+        $contact = new Install_Helper_Contact($this->kernel, $this->db);
+        $contact_id = $contact->create();
+        
+        require_once 'install/Helper/Product.php';
+        $product = new Install_Helper_Product($this->kernel, $this->db);
+        $product_id = $product->create();
+        
+        require_once 'Intraface/modules/invoice/Invoice.php';
+        $debtor = new Invoice($this->kernel);
+        $id = $debtor->update(array(
+            'contact_id' => $contact_id,
+            'description' => 'Test invoice',
+            'this_date' => date('d-m-Y'),
+            'due_date' => date('d-m-Y', time()+14*60*60*24)));
+        
+        $debtor->loadItem();
+        $debtor->item->save(array('product_id' => $product_id, 'quantity' => 3, 'description' => 'Test description on product'));
+        
+    }
+    
+    public function createOrder() {
+        
+        require_once 'install/Helper/Contact.php';
+        $contact = new Install_Helper_Contact($this->kernel, $this->db);
+        $contact_id = $contact->create();
+        
+        require_once 'install/Helper/Product.php';
+        $product = new Install_Helper_Product($this->kernel, $this->db);
+        $product_id = $product->create();
+        
+        require_once 'Intraface/modules/order/Order.php';
+        $debtor = new Order($this->kernel);
+        $id = $debtor->update(array(
+            'contact_id' => $contact_id,
+            'description' => 'Test invoice',
+            'this_date' => date('d-m-Y'),
+            'due_date' => date('d-m-Y', time()+14*60*60*24)));
+        
+        $debtor->loadItem();
+        $debtor->item->save(array('product_id' => $product_id, 'quantity' => 3, 'description' => 'Test description on product'));
+        
+    }
+}
+?>
