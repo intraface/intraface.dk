55f553a753e52810cd2bef81a2180302aa4f6cc7
Sune T. Jensen <sj@sunet.dk>
2008-01-29 17:29:23 +0000

    added redirect for create new contact

--- intraface.dk/modules/contact/select_contact.php
+++ intraface.dk/modules/contact/select_contact.php
@@ -6,7 +6,7 @@ $translation = $kernel->getTranslation('contact');
 
 $redirect = Redirect::factory($kernel, 'receive');
 
-if(isset($_GET['add'])) {
+if(!empty($_GET['add'])) {
 
 	$add_redirect = Redirect::factory($kernel, 'go');
 	$url = $add_redirect->setDestination($module->getPath()."contact_edit.php", $module->getPath()."select_contact.php?".$redirect->get('redirect_query_string'));
@@ -77,21 +77,20 @@ $page->start('Vælg kontakt');
 
 <?php echo $contact->error->view(); ?>
 
-<ul class="options">
-	<li><a href="select_contact.php?add=1">Opret kontakt</a></li>
-	<?php if(isset($_GET['last_contact_id']) && intval($_GET['last_contact_id']) != 0): ?>
-	<li><a href="select_contact.php?contact_id=<?php print(intval($_GET['last_contact_id'])); ?>">Vis valgte</a></li>
-	<?php endif; ?>
-
-</ul>
-
 <?php if (!$contact->isFilledIn()): ?>
 
-	<p>Der er ikke oprettet nogen kontakter. <a href="contact_edit.php">Opret en kontakt</a>.</p>
+	<p>Der er ikke oprettet nogen kontakter. <a href="select_contact.php?add=1">Opret en kontakt</a>.</p>
 
 <?php else: ?>
+    <ul class="options">
+        <li><a href="select_contact.php?add=1">Opret kontakt</a></li>
+        <?php if(isset($_GET['last_contact_id']) && intval($_GET['last_contact_id']) != 0): ?>
+        <li><a href="select_contact.php?contact_id=<?php print(intval($_GET['last_contact_id'])); ?>">Vis valgte</a></li>
+        <?php endif; ?>
+    
+    </ul>
 
-<form action="select_contact.php" method="get" class="search-filter">
+    <form action="select_contact.php" method="get" class="search-filter">
 	<fieldset>
 		<legend>Søgning</legend>
 
@@ -112,42 +111,42 @@ $page->start('Vælg kontakt');
 
 		<span><input type="submit" value="Afsted!" /></span>
 	</fieldset>
-</form>
-
-<?php echo $contact->dbquery->display('character'); ?>
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	<table summary="Kontakter" class="stripe">
-		<caption>Kontakter</caption>
-		<thead>
-			<tr>
-				<th>&nbsp;</th>
-				<th>Nr.</th>
-				<th>Navn</th>
-				<th>E-mail</th>
-			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td colspan="4"><?php echo $contact->dbquery->display('paging'); ?></td>
-			</tr>
-		</tfoot>
-		<tbody>
-			<?php foreach ($contacts AS $c) { ?>
-			<tr>
-				<td>
-					<input type="radio" value="<?php echo $c['id']; ?>" name="selected" <?php if((isset($_GET['contact_id']) && $_GET['contact_id'] == $c['id']) || (isset($_GET['last_contact_id']) && $_GET['last_contact_id'] == $c['id'])) print("checked=\"checked\""); ?> />
-				</td>
-				<td><?php echo $c['number']; ?></td>
-				<td><a href="contact.php?id=<?php echo $c['id']; ?>"><?php echo $c['name']; ?></a></td>
-				<td><?php echo $c['email']; ?></td>
-			</tr>
-			<?php } // end foreach ?>
-		</tbody>
-	</table>
-
-	<input type="submit" name="submit" value="<?php echo $translation->get('choose', 'common'); ?>" /> eller <a href="<?php print($redirect->getRedirect("index.php")); ?>">Fortryd</a>
-</form>
+    </form>
+    
+    <?php echo $contact->dbquery->display('character'); ?>
+    
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    	<table summary="Kontakter" class="stripe">
+    		<caption>Kontakter</caption>
+    		<thead>
+    			<tr>
+    				<th>&nbsp;</th>
+    				<th>Nr.</th>
+    				<th>Navn</th>
+    				<th>E-mail</th>
+    			</tr>
+    		</thead>
+    		<tfoot>
+    			<tr>
+    				<td colspan="4"><?php echo $contact->dbquery->display('paging'); ?></td>
+    			</tr>
+    		</tfoot>
+    		<tbody>
+    			<?php foreach ($contacts AS $c) { ?>
+    			<tr>
+    				<td>
+    					<input type="radio" value="<?php echo $c['id']; ?>" name="selected" <?php if((isset($_GET['contact_id']) && $_GET['contact_id'] == $c['id']) || (isset($_GET['last_contact_id']) && $_GET['last_contact_id'] == $c['id'])) print("checked=\"checked\""); ?> />
+    				</td>
+    				<td><?php echo $c['number']; ?></td>
+    				<td><a href="contact.php?id=<?php echo $c['id']; ?>"><?php echo $c['name']; ?></a></td>
+    				<td><?php echo $c['email']; ?></td>
+    			</tr>
+    			<?php } // end foreach ?>
+    		</tbody>
+    	</table>
+    
+    	<input type="submit" name="submit" value="<?php echo $translation->get('choose', 'common'); ?>" /> eller <a href="<?php print($redirect->getRedirect("index.php")); ?>">Fortryd</a>
+    </form>
 
 <?php endif; ?>
 <?php
