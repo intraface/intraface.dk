24a842e149eca88c1e14f4a6deb38fbada74b492
Lars Olesen <lars@intraface.dk>
2008-02-09 11:57:12 +0000

    Made $data private

--- Intraface/tools/Date.php
+++ Intraface/tools/Date.php
@@ -1,46 +1,44 @@
 <?php
+class Intraface_Date
+{
+    private $date;
 
-class Intraface_Date {
-	var $date;
+    function __construct($date)
+    {
+        $this->date = $date;
+    }
 
-	function __construct($date) {
-		$this->date = $date;
-	}
+    function convert2db($default_year = "")
+    {
 
-	function convert2db($default_year = "") {
+        /**
+         * HUSK AT RETTE I BÅDE VALIDATOR OG DATE
+         */
 
+        $d = "([0-3]?[0-9])";
+        $m = "([0-1]?[0-9])";
+        $y = "([0-9][0-9][0-9][0-9]|[0-9]?[0-9])";
+        $s = "(-|\.|/| )";
 
-		/**
-		 * HUSK AT RETTE I BÅDE VALIDATOR OG DATE
-		 */
+        if($default_year == "") {
+            $default_year = date("Y");
+        }
 
-		$d = "([0-3]?[0-9])";
-		$m = "([0-1]?[0-9])";
-		$y = "([0-9][0-9][0-9][0-9]|[0-9]?[0-9])";
-		$s = "(-|\.|/| )";
+        if(ereg("^".$d.$s.$m.$s.$y."$", $this->date, $parts)) {
+            // true
+        } elseif(ereg("^".$d.$s.$m."$", $this->date, $parts)) {
+            $parts[5] = $default_year;
+            // true
+        } else {
+            return false;
+        }
 
-		if($default_year == "") {
-			$default_year = date("Y");
-		}
+        $this->date = $parts[5]."-".$parts[3]."-".$parts[1];
+        return true;
+    }
 
-		if(ereg("^".$d.$s.$m.$s.$y."$", $this->date, $parts)) {
-			// true
-		}
-		elseif(ereg("^".$d.$s.$m."$", $this->date, $parts)) {
-			$parts[5] = $default_year;
-			// true
-		}
-		else {
-			return false;
-		}
-
-		$this->date = $parts[5]."-".$parts[3]."-".$parts[1];
-		return(true);
-	}
-
-	function get() {
-		return($this->date);
-	}
+    function get()
+    {
+        return $this->date;
+    }
 }
-
-?>
