f7d92f3a77b9546a0e487fad9cfa4a2f8e6aa079
Lars Olesen <lars@intraface.dk>
2008-02-27 17:04:26 +0000

    Now suppresses errors if testSuite.html is not found.

--- tests/selenium/generate_test_suite.php
+++ tests/selenium/generate_test_suite.php
@@ -16,45 +16,45 @@ $f = '.';
 
 foreach(scandir( $f ) as $folder ){
     if(!strcmp(substr($folder, 0, 1), '.' )) continue;
-    
+
     if(is_dir($folder)) {
         foreach(scandir($f.DIRECTORY_SEPARATOR.$folder) as $file) {
             if(!strcmp(substr($file, 0, 1), '.' )) continue;
-            
-            // substr($file, 0, 4) == 'test' && 
-            
+
+            // substr($file, 0, 4) == 'test' &&
+
             if($file == 'generate' && is_dir($f.DIRECTORY_SEPARATOR.$folder.DIRECTORY_SEPARATOR.$file)) {
                 $generate_dir = $f.DIRECTORY_SEPARATOR.$folder.DIRECTORY_SEPARATOR.$file;
-                
+
                 foreach(scandir($generate_dir) as $generate_file) {
                     if(substr($generate_file, strlen($generate_file) - 5) == '.html') {
-                        
+
                         $generate_file_content = file_get_contents($generate_dir.DIRECTORY_SEPARATOR.$generate_file);
                         $generate_file_content = str_replace('##path_test_root##', realpath(dirname(__FILE__)), $generate_file_content);
                         if(!is_dir($f.DIRECTORY_SEPARATOR.'.tmp')) {
-                            mkdir($f.DIRECTORY_SEPARATOR.'.tmp');    
+                            mkdir($f.DIRECTORY_SEPARATOR.'.tmp');
                         }
                         if(!is_dir($f.DIRECTORY_SEPARATOR.'.tmp'.DIRECTORY_SEPARATOR.$folder)) {
                             mkdir($f.DIRECTORY_SEPARATOR.'.tmp'.DIRECTORY_SEPARATOR.$folder);
                         }
                         file_put_contents($f.DIRECTORY_SEPARATOR.'.tmp'.DIRECTORY_SEPARATOR.$folder.DIRECTORY_SEPARATOR.$generate_file, $generate_file_content);
                         $test_name = $folder.':generated:'.substr($generate_file, 0, strlen($generate_file) - 5);
-                    
+
                         $write .= "\r\n\t\t\t<tr>" .
                                   "\r\n\t\t\t\t<td><a href=\"".$f.DIRECTORY_SEPARATOR.'.tmp'.DIRECTORY_SEPARATOR.$folder.DIRECTORY_SEPARATOR.$generate_file."\">".$test_name."</a></td>" .
                                   "\r\n\t\t\t</tr>";
                     }
                 }
             } elseif(substr($file, strlen($file) - 5) == '.html') {
-                
+
                 $test_name = $folder.':'.substr($file, 0, strlen($file) - 5);
-                    
+
                 $write .= "\r\n\t\t\t<tr>" .
                           "\r\n\t\t\t\t<td><a href=\"".$folder.DIRECTORY_SEPARATOR.$file."\">".$test_name."</a></td>" .
                           "\r\n\t\t\t</tr>";
-              
+
             }
-        }   
+        }
     }
 }
 
@@ -62,7 +62,7 @@ $write .= "\r\n\t\t</table>" .
           "\r\n\t</body>" .
           "\r\n</html>";
 
-unlink('testSuite.html');
+@unlink('testSuite.html');
 
 if (!$handle = fopen('testSuite.html', 'a')) {
     echo "Cannot open file ($filename)";
