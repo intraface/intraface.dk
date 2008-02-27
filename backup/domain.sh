ec7b282777237a3aba1f5df83e42ee7d3ce3279d
Sune T. Jensen <sj@sunet.dk>
2008-02-27 16:52:07 +0000

    Removed backup of intraface code Removed backun of kundelogin

--- backup/domain.sh
+++ backup/domain.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 suffix=$(date +%y%m%d)
 cd /home/intraface/.snapshot/nightly.0/
-tar -zcvf /home/intraface/backup/archives/intraface.$suffix.tar intraface*/
+# tar -zcvf /home/intraface/backup/archives/intraface.$suffix.tar intraface*/
 # tar -zcvf /home/intraface/backup/archives/upload.$suffix.tar upload/
 tar --exclude='*/tempdir*' --exclude='*/instance*' -zcvf /home/intraface/backup/archives/upload.$suffix.tar.gz upload/
-tar -zcvf /home/intraface/backup/archives/kundelogin.$suffix.tar kundelogin*/
+# tar -zcvf /home/intraface/backup/archives/kundelogin.$suffix.tar kundelogin*/
 tar -zcvf /home/intraface/backup/archives/certificates.$suffix.tar certificates*/
