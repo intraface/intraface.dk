f77c5fe6e6eb264541ff344e1712264cb3d2441b
Lars Olesen <lars@intraface.dk>
2008-02-27 10:25:16 +0000

    Removed uncommented stuff.

--- Intraface/modules/cms/element/Flickr.php
+++ Intraface/modules/cms/element/Flickr.php
@@ -10,13 +10,13 @@
  *
  * @package Intraface_CMS
  */
-
 // use this http://www.airtightinteractive.com/simpleviewer/auto_server_instruct.html
 
 require_once 'phpFlickr/phpFlickr.php';
 require_once 'Intraface/modules/cms/Element.php';
 
-class CMS_Flickr extends CMS_Element {
+class CMS_Flickr extends CMS_Element
+{
 
     public $allowed_sizes = array(
         'square' => 'Små firkanter',
@@ -30,12 +30,11 @@ class CMS_Flickr extends CMS_Element {
         'flickr' => 'flickr'
     );
 
-    function __construct(& $section, $id = 0)
+    function __construct($section, $id = 0)
     {
         $this->value['type'] = 'flickr';
         parent::__construct($section, $id);
     }
-
 
     // tror måske bare jeg skal returnere links med billeder og så
     // lave et eller andet end den her pictobrowser
@@ -87,16 +86,8 @@ class CMS_Flickr extends CMS_Element {
             }
             $this->value['pictures'] = array();
 
-            /*
-            $nsid = $f->people_findByUsername($this->parameter->get('user'));
-            // Get the friendly URL of the user's photos
-            $photos_url = $f->urls_getUserPhotos($nsid);
-            $photos = $f->people_getPublicPhotos($nsid, NULL, 36);
-            $recent = $f->photos_search(array('user_id' =>$nsid, 'tags' => $this->parameter->get('tags')));
-            */
-            //print_r($flickr_photos);
             $photos = $flickr_photos['photo'];
-            //print_r($photos);
+
             $i = 0;
             /*
             foreach ($photos as $photo) {
@@ -112,8 +103,6 @@ class CMS_Flickr extends CMS_Element {
         }
 
         if ($this->value['service'] == 'flickr') {
-            // &userName='.$this->value['user']. could be i FlashVars
-            // '&userName='.$this->value['user'].'
             $this->value['pictobrowser'] = '
                 <object codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="500" height="580" id="photo_browser02" align="middle">
                     <param name="FlashVars" VALUE="currentSet='.$this->value['photoset_id'].'&UserName=lsolesen"></param>
@@ -124,18 +113,9 @@ class CMS_Flickr extends CMS_Element {
                     <param name="bgcolor" value="#000000"></param>
                     <embed src="http://www.db798.com/work/photo_browser/photo_browser.swf" FlashVars="currentSet='.$this->value['photoset_id'].'&UserName=lsolesen" loop="false" quality="best" scale="noscale" bgcolor="#000000" width="500" height="580" name="photo_browser" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed>
                 </object>';
-        }
-        else {
+        } else {
             $this->value['pictobrowser'] = '';
         }
-
-        /*
-        <iframe align=center src=
-http://www.flickr.com/slideShow/index.gne?user_id=12345678@N00&tags=foo
-frameBorder=0 width=500 scrolling=no height=500></iframe>
-
-Change user_id from 12345678@N00 to your Flickr id, which you can find out with this idGettr. Also replace foo with the tags of the photos you wish to display. Other parameters include "contacts=", "text=", "tag_mode=", "favorites=", "group_id=", "frifam=", "nsid=", "single=", "firstIndex=", "set_id" and "firstId=". If you want to use one of these extra parameters just add it after your user_id with a separating &.
-        */
     }
 
     function validate_element($var)
