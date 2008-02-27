d7645bcebf36999c99af0aee2abcced1e9c7edb6
Lars Olesen <lars@intraface.dk>
2008-02-27 21:14:50 +0000

    Added test for hasSubAccess and getAddress

--- tests/unit/common/UserTest.php
+++ tests/unit/common/UserTest.php
@@ -15,23 +15,22 @@ class UserTest extends PHPUnit_Framework_TestCase
 
     function setUp()
     {
-        
         $db = MDB2::singleton(DB_DSN);
         $db->exec('TRUNCATE user');
         $db->exec('TRUNCATE intranet');
         $db->exec('TRUNCATE modules');
         $db->exec('TRUNCATE permission');
-        
-        
+
+
         // @todo this has the notion with the standard database setup
         $u = new UserMaintenance();
         $u->update(array('email' => 'start@intraface.dk', 'password' => '123456', 'confirm_password' => '123456', 'disable' => 0));
-        
+
         $i = new IntranetMaintenance();
         $i->save(array('name' => 'intraface', 'identifier' => 'intraface'));
-        
+
         $this->user = new User(1);
-        
+
         $m = new ModuleMaintenance();
         $m->register();
     }
@@ -52,7 +51,7 @@ class UserTest extends PHPUnit_Framework_TestCase
     {
         $u = new UserMaintenance(1);
         $u->setIntranetAccess(1);
-        
+
         $this->assertTrue($this->user->hasIntranetAccess(1));
         $this->assertFalse($this->user->hasIntranetAccess(2));
     }
@@ -61,7 +60,7 @@ class UserTest extends PHPUnit_Framework_TestCase
     {
         $u = new UserMaintenance(1);
         $u->setModuleAccess('intranetmaintenance', 1);
-        
+
         // TODO how should we handle unknown modules
         // TODO and setup modules we can count on for the test
         $this->assertFalse($this->user->hasModuleAccess('intranetmaintenance'));
@@ -71,19 +70,18 @@ class UserTest extends PHPUnit_Framework_TestCase
         $this->assertFalse($this->user->hasModuleAccess('todo'));
     }
 
-    /*
-    function testSubAccess()
+    function testSubAccessReturnsFalseOnANotInitializedSubAccess()
     {
-        // @todo This test should be completed
-        $this->markTestIncomplete('This test should be completed');
+        $u = new UserMaintenance(1);
+        $u->setIntranetAccess(1);
+        $this->assertFalse($this->user->hasSubAccess(1, 1));
     }
-    */
 
     function testSetActiveIntranetReturnsAValueLargerThanZero()
     {
         $u = new UserMaintenance(1);
         $u->setModuleAccess('intranetmaintenance', 1);
-        
+
         $this->assertTrue($this->user->setActiveIntranetId(1) > 0);
     }
 
@@ -96,7 +94,7 @@ class UserTest extends PHPUnit_Framework_TestCase
     {
         $u = new UserMaintenance(1);
         $u->setModuleAccess('intranetmaintenance', 1);
-        
+
         $this->user->setIntranetId(1);
         $this->assertTrue($this->user->hasModuleAccess('intranetmaintenance'));
     }
@@ -113,11 +111,15 @@ class UserTest extends PHPUnit_Framework_TestCase
     {
         $u = new UserMaintenance(1);
         $u->setIntranetAccess(1);
-        
+
         $this->user->setIntranetId(1);
         $this->user->clearCachedPermission();
         $this->assertEquals(0, count($this->user->getPermissions()));
         $this->assertFalse($this->user->permissionsLoaded());
     }
+
+    function testGetAddressReturnsObject()
+    {
+        $this->assertTrue(is_object($this->user->getAddress()));
+    }
 }
-?>
