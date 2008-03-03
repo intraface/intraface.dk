5cadc1892c458e27114a3082cd29ea8981e2cbc5
Lars Olesen <lars@intraface.dk>
2008-03-03 03:11:59 +0000

    Now deletes the upload directory when installing.

--- install/Install.php
+++ install/Install.php
@@ -8,7 +8,7 @@ class Intraface_Install {
      * @var object database connection
      */
     private $db;
-    
+
     /**
      * constructor. Checks if the script can be run. Connects to database.
      */
@@ -19,25 +19,25 @@ class Intraface_Install {
         elseif (!empty($_SERVER['HTTP_HOST']) AND $_SERVER['HTTP_HOST'] == 'www.intraface.dk') {
             die('Can not be performed on www.intraface.dk');
         }
-        
+
         $this->db = MDB2::singleton(DB_DSN);
-        
+
         if(PEAR::isError($this->db)) {
             trigger_error($this->db->getUserInfo(), E_USER_ERROR);
             exit;
         }
-        
+
 
     }
 
     function dropDatabase() {
-        
+
         $result = $this->db->query("SHOW TABLES FROM " . DB_NAME);
         if(PEAR::isError($result)) {
             trigger_error($result->getUserInfo(), E_USER_ERROR);
             exit;
         }
-        
+
         while ($line = $result->fetchRow(MDB2_FETCHMODE_ASSOC)) {
             $drop = $this->db->exec('DROP TABLE ' . $line['Tables_in_'.DB_NAME]);
             if (PEAR::IsError($drop)) {
@@ -61,7 +61,7 @@ class Intraface_Install {
                 exit;
             }
         }
-        
+
         $sql_structure = file_get_contents(dirname(__FILE__) . '/database-update.sql');
         $sql_arr = Intraface_Install::splitSql($sql_structure);
 
@@ -77,9 +77,9 @@ class Intraface_Install {
         return true;
 
     }
-    
+
     function emptyDatabase() {
-        
+
         $result = $this->db->query("SHOW TABLES FROM " . DB_NAME);
         if(PEAR::isError($result)) {
             trigger_error($result->getUserInfo(), E_USER_ERROR);
@@ -123,7 +123,7 @@ class Intraface_Install {
             exit;
         }
         */
-        
+
         if (!$this->emptyDatabase()) {
             trigger_error('could not empty database', E_USER_ERROR);
             exit;
@@ -134,10 +134,28 @@ class Intraface_Install {
             exit;
         }
 
+        $this->deleteUploadDirectory(PATH_UPLOAD);
+
+        mkdir(PATH_UPLOAD);
+
         return true;
 
     }
-    
+
+    function deleteUploadDirectory($f)
+    {
+        if( is_dir( $f ) ){
+            foreach( scandir( $f ) as $item ){
+                if( !strcmp( $item, '.' ) || !strcmp( $item, '..' ) )
+                    continue;
+                $this->deleteUploadDirectory( $f . "/" . $item );
+            }
+            rmdir( $f );
+        } else{
+            @unlink( $f );
+        }
+    }
+
     /**
      * grants access to given modules
      */
@@ -145,18 +163,18 @@ class Intraface_Install {
     {
         $this->registerModules();
         $modules = explode(',', $modules);
-        
+
         require_once 'Intraface/modules/intranetmaintenance/IntranetMaintenance.php';
         // The moduleaccess only goes for intranet_id 1
         $intranet = new IntranetMaintenance(1);
         require_once 'Intraface/modules/intranetmaintenance/UserMaintenance.php';
         $user = new UserMaintenance(1);
         $user->setIntranetAccess(1);
-        
+
         require_once 'Intraface/modules/intranetmaintenance/ModuleMaintenance.php';
         foreach($modules AS $module_name) {
             $module = ModuleMaintenance::factory($module_name);
-            
+
             if($module->get('id') == 0) {
                 trigger_error('Invalid module '.$module_name, E_USER_ERROR);
                 exit;
@@ -168,31 +186,31 @@ class Intraface_Install {
                 $user->setSubAccess($module->get('id'), $sub_access['id'], 1);
             }
         }
-        
-        return true;        
-    
+
+        return true;
+
     }
-    
+
     /**
      * login the user
      */
     function loginUser() {
-        
+
         session_start();
         require_once 'Intraface/Auth.php';
         $auth = new Auth(session_id());
         return $auth->login('start@intraface.dk', 'startup');
-        
+
     }
-    
+
     /**
      * run helper functions
      */
-    
+
     public function runHelperFunction($functions)
     {
         $functions = explode(',', $functions);
-        
+
         // We create kernel so it can be used in the helper functions
         if (session_id() != '') {
             $kernel = new Kernel(session_id());
@@ -204,23 +222,23 @@ class Intraface_Install {
         $kernel->user->setIntranetId(1);
         $kernel->intranet = new Intranet(1);
         $kernel->setting = new Setting(1, 1);
-        
-        
+
+
         foreach($functions AS $function) {
-            
+
             $object_method = explode(':', trim($function));
             $object_method[0] = str_replace('/', '', $object_method[0]);
             $object_method[0] = str_replace('\\', '', $object_method[0]);
-            
+
             require_once 'install/Helper/'.$object_method[0].'.php';
             $object_name = 'Install_Helper_'.$object_method[0];
             $object = new $object_name($kernel, $this->db);
             $object->$object_method[1]();
-            
+
         }
-        
+
     }
-    
+
     /**
      * register modules
      */
@@ -229,11 +247,11 @@ class Intraface_Install {
         $modulemaintenance = new ModuleMaintenance;
         $modulemaintenance->register();
     }
-    
+
     /**
      * splits a mysql export into separate
      */
-    static function splitSql($sql) 
+    static function splitSql($sql)
     {
         if(strpos($sql, "\r\n")) {
             $str_sep = "\r\n";
@@ -248,7 +266,7 @@ class Intraface_Install {
         $parts = split(";( )*".$str_sep, $sql);
         $parts = array_map('trim', $parts);
         return $parts;
-        
+
     }
 }
 ?>
