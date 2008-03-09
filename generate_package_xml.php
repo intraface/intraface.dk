67f143109b82df3c7d6c9e7e2f50fb42a8c85124
Lars Olesen <lars@intraface.dk>
2008-03-09 16:50:18 +0000

    Bumped version number for Ilib_Position.

--- generate_package_xml.php
+++ generate_package_xml.php
@@ -120,7 +120,7 @@ $pfm->addPackageDepWithChannel('required', 'ErrorHandler', 'public.intraface.dk'
 $pfm->addPackageDepWithChannel('required', 'MDB2_Debug_ExplainQueries', 'public.intraface.dk', '0.1.1');
 $pfm->addPackageDepWithChannel('required', 'File', 'pear.php.net', '1.3.0');
 $pfm->addPackageDepWithChannel('required', 'Ilib_RandomKeyGenerator', 'public.intraface.dk', '0.0.3');
-$pfm->addPackageDepWithChannel('required', 'Ilib_Position', 'public.intraface.dk', '0.2.0');
+$pfm->addPackageDepWithChannel('required', 'Ilib_Position', 'public.intraface.dk', '0.3.0');
 
 
 // Ilib
