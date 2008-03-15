1d629fc829a89972fb11ae71895bbd0515330733
Lars Olesen <lars@intraface.dk>
2008-03-15 16:10:31 +0000

    Coding standard improvement.

--- tests/unit/stubs/Setting.php
+++ tests/unit/stubs/Setting.php
@@ -1,21 +1,20 @@
 <?php
-class FakeSetting {
-    
-    var $setting;
-    
-    function get($type, $setting) {
-        
+class FakeSetting
+{
+    public $setting;
+
+    function get($type, $setting)
+    {
         if(!isset($this->setting[$type][$setting])) {
             trigger_error('You need to create the setting '.$type.':'.$setting.' in with set(type, key, value) before use', E_USER_ERROR);
             exit;
         }
-        
+
         return $this->setting[$type][$setting];
     }
-    
-    function set($type, $key, $value) {
+
+    function set($type, $key, $value)
+    {
         $this->setting[$type][$key] = $value;
     }
-    
 }
-?>
