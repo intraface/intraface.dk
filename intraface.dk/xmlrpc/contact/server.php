37c7b318efaa250410595ae451557263a2442c93
Lars Olesen <lars@intraface.dk>
2008-03-23 09:24:22 +0000

    Coding standard improvements.

--- intraface.dk/xmlrpc/contact/server.php
+++ intraface.dk/xmlrpc/contact/server.php
@@ -1,10 +1,11 @@
 <?php
 require_once '../../common.php';
 require_once 'Intraface/XMLRPC/Contact/Server.php';
+
 XML_RPC2_Backend::setBackend('php');
 $HTTP_RAW_POST_DATA = file_get_contents('php://input');
+
 $options = array('prefix' => 'contact.', 'encoding' => 'iso-8859-1');
 
 $server = XML_RPC2_Server::create(new Intraface_XMLRPC_Contact(), $options);
 $server->handleCall();
-?>
