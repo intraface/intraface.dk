fb64ba163bcb05cac9b6b09cc22a8031a320533b
Lars Olesen <lars@intraface.dk>
2008-03-28 11:02:18 +0000

    Fixed coding standard issues.

--- Intraface/common.php
+++ Intraface/common.php
@@ -11,18 +11,18 @@
 //configuration - this should never be edited - only edit intraface.dk/config.local.php
 
 // paths
-if(!defined('PATH_INCLUDE_IHTML')) define('PATH_INCLUDE_IHTML', PATH_ROOT.'Intraface/ihtml' . DIRECTORY_SEPARATOR);
-if(!defined('PATH_INCLUDE_MODULE')) define('PATH_INCLUDE_MODULE', PATH_ROOT.'Intraface/modules' . DIRECTORY_SEPARATOR);
-if(!defined('PATH_INCLUDE_SHARED')) define('PATH_INCLUDE_SHARED', PATH_ROOT.'Intraface/shared' . DIRECTORY_SEPARATOR);
-if(!defined('PATH_INCLUDE_CONFIG')) define('PATH_INCLUDE_CONFIG', PATH_ROOT.'/Intraface/config'.DIRECTORY_SEPARATOR);
-if(!defined('PATH_UPLOAD')) define('PATH_UPLOAD', PATH_ROOT . 'upload/'); // Directory for upload of files.
-if(!defined('PATH_UPLOAD_TEMPORARY')) define('PATH_UPLOAD_TEMPORARY', 'tempdir/'); // Mappen i Upload_path, under intranetid, hvor temp-filer placeres.
-if(!defined('PATH_CAPTCHA')) define('PATH_CAPTCHA', PATH_ROOT . 'captcha/'); // remember trailing slash - used for the demo formular
-if(!defined('PATH_CACHE')) define('PATH_CACHE', PATH_ROOT . 'cache/'); // remember trailing slash - path to cache
-if(!defined('PATH_INCLUDE_BACKUP')) define('PATH_INCLUDE_BACKUP', PATH_ROOT . 'backup/');
+if (!defined('PATH_INCLUDE_IHTML')) define('PATH_INCLUDE_IHTML', PATH_ROOT.'Intraface/ihtml' . DIRECTORY_SEPARATOR);
+if (!defined('PATH_INCLUDE_MODULE')) define('PATH_INCLUDE_MODULE', PATH_ROOT.'Intraface/modules' . DIRECTORY_SEPARATOR);
+if (!defined('PATH_INCLUDE_SHARED')) define('PATH_INCLUDE_SHARED', PATH_ROOT.'Intraface/shared' . DIRECTORY_SEPARATOR);
+if (!defined('PATH_INCLUDE_CONFIG')) define('PATH_INCLUDE_CONFIG', PATH_ROOT.'/Intraface/config'.DIRECTORY_SEPARATOR);
+if (!defined('PATH_UPLOAD')) define('PATH_UPLOAD', PATH_ROOT . 'upload/'); // Directory for upload of files.
+if (!defined('PATH_UPLOAD_TEMPORARY')) define('PATH_UPLOAD_TEMPORARY', 'tempdir/'); // Mappen i Upload_path, under intranetid, hvor temp-filer placeres.
+if (!defined('PATH_CAPTCHA')) define('PATH_CAPTCHA', PATH_ROOT . 'captcha/'); // remember trailing slash - used for the demo formular
+if (!defined('PATH_CACHE')) define('PATH_CACHE', PATH_ROOT . 'cache/'); // remember trailing slash - path to cache
+if (!defined('PATH_INCLUDE_BACKUP')) define('PATH_INCLUDE_BACKUP', PATH_ROOT . 'backup/');
 
 // paths on www
-if(defined('NET_SCHEME') && defined('NET_HOST') && defined('NET_DIRECTORY')) {
+if (defined('NET_SCHEME') && defined('NET_HOST') && defined('NET_DIRECTORY')) {
     define('PATH_WWW', NET_SCHEME.NET_HOST.NET_DIRECTORY);
     define('PATH_WWW_MODULE', PATH_WWW.'modules/');
     define('PATH_WWW_SHARED', PATH_WWW.'shared/');
@@ -41,8 +41,8 @@ if (!defined('MDB2_DEBUG')) {
 if (!defined('IMAGE_LIBRARY')) define('IMAGE_LIBRARY', 'GD');
 
 // timezone and local
-if(!defined('COUNTRY_LOCAL')) define('COUNTRY_LOCAL', 'da_DK');
-if(!defined('TIMEZONE')) define('TIMEZONE', 'Europe/Copenhagen');
+if (!defined('COUNTRY_LOCAL')) define('COUNTRY_LOCAL', 'da_DK');
+if (!defined('TIMEZONE')) define('TIMEZONE', 'Europe/Copenhagen');
 setlocale(LC_CTYPE, COUNTRY_LOCAL);
 putenv("TZ=".TIMEZONE);
 
@@ -105,7 +105,7 @@ if ($db->getOption('debug')) {
 
 }
 
-if(defined('TIMEZONE')) {
+if (defined('TIMEZONE')) {
     $db->exec('SET time_zone=\''.TIMEZONE.'\'');
 }
 
