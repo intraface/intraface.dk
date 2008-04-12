d79176588c8f46ddc82e557a6fc28ba9828b190e
Lars Olesen <lars@intraface.dk>
2008-04-12 12:34:11 +0000

    Removed unused code

--- intraface.dk/xmlrpc/todo/TodoServer.php
+++ intraface.dk/xmlrpc/todo/TodoServer.php
@@ -4,121 +4,122 @@
  *
  * @author Lars Olesen <lars@legestue.net>
  */
-
+/*
 require('../../common.php');
 require_once('IXR/IXR.php');
 
 class TodoServer extends IXR_Server {
 
-	var $kernel;
+    var $kernel;
 
-	function TodoServer() {
-		$this->IXR_Server(array(
-				'todo.get' => 'this:get',
-				'todo.setDone' => 'this:setDone',
-				'todo.getList' => 'this:getList'
-			)
-		);
-	}
-
+    function TodoServer() {
+        $this->IXR_Server(array(
+                'todo.get' => 'this:get',
+                'todo.setDone' => 'this:setDone',
+                'todo.getList' => 'this:getList'
+            )
+        );
+    }
+*/
   /**
-	 * Tjekker om forespørgslen må foretages
-	 *
-	 * @param struct $credentials
-	 * 	- intranet_id = integer
-	 *  - key_code = session_id
-	 * @return true ved succes ellers object med fejlen
-	 */
+     * Tjekker om forespørgslen må foretages
+     *
+     * @param struct $credentials
+     * 	- intranet_id = integer
+     *  - key_code = session_id
+     * @return true ved succes ellers object med fejlen
+     */
+    /*
+    function checkCredentials($credentials) {
+        if (count($credentials) != 1) {
+            return new IXR_Error(-2, 'Der er et forkert antal argumenter i credentials');
+        }
+        if (empty($credentials['private_key'])) {
+            return new IXR_Error(-2, 'Du skal skrive en kode');
+        }
 
-	function checkCredentials($credentials) {
-		if (count($credentials) != 1) {
-			return new IXR_Error(-2, 'Der er et forkert antal argumenter i credentials');
-		}
-		if (empty($credentials['private_key'])) {
-			return new IXR_Error(-2, 'Du skal skrive en kode');
-		}
+        $this->kernel = new Kernel('weblogin');
+        $this->kernel->weblogin('private', $credentials['private_key'], md5(session_id()));
 
-		$this->kernel = new Kernel('weblogin');
-		$this->kernel->weblogin('private', $credentials['private_key'], md5(session_id()));
+        if (!is_object($this->kernel->intranet) AND get_class($this->kernel->intranet) != 'intranet') {
+            return new IXR_Error(-2, 'Du har ikke adgang til intranettet');
+        }
 
-		if (!is_object($this->kernel->intranet) AND get_class($this->kernel->intranet) != 'intranet') {
-			return new IXR_Error(-2, 'Du har ikke adgang til intranettet');
-		}
+        $this->kernel->module('todo');
 
-		$this->kernel->module('todo');
+    }
+    */
 
-	}
+    /**
+     * @return true ved succes ellers object med fejlen
+     */
+     /*
+     function get($arg) {
 
-	/**
-	 * @return true ved succes ellers object med fejlen
-	 */
+        $credentials = $arg[0];
+        $id = $arg[1];
 
-	 function get($arg) {
+       if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-		$credentials = $arg[0];
-		$id = $arg[1];
+        $db = new DB_Sql;
+        $db->query("SELECT * FROM todo_list WHERE id = ".$id." AND intranet_id = " . $this->kernel->intranet->get('id'));
+        if (!$db->nextRecord()) {
+            return new IXR_Error(-2, 'Der er ikke nogen liste');
+        }
 
-   	if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+        $todo = new TodoList($this->kernel, $db->f('id'));
 
-		$db = new DB_Sql;
-		$db->query("SELECT * FROM todo_list WHERE id = ".$id." AND intranet_id = " . $this->kernel->intranet->get('id'));
-		if (!$db->nextRecord()) {
-	    	return new IXR_Error(-2, 'Der er ikke nogen liste');
-		}
+        return array('name'=>$todo->get('list_name'), 'items'=>$todo->item->getList('undone'));
+    }
 
-		$todo = new TodoList($this->kernel, $db->f('id'));
-
-		return array('name'=>$todo->get('list_name'), 'items'=>$todo->item->getList('undone'));
-	}
-
-	function getList($arg) {
-		$credentials = $arg[0];
-		$contact_id = $arg[1];
+    function getList($arg) {
+        $credentials = $arg[0];
+        $contact_id = $arg[1];
 
 
-   	if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+       if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-		$db = new DB_Sql;
-		$db->query("SELECT todo_list.name, todo_list.id FROM todo_list
-			INNER JOIN todo_contact ON todo_contact.list_id = todo_list.id
-			WHERE todo_contact.contact_id = " . $contact_id . "
-				AND todo_list.intranet_id=".$this->kernel->intranet->get('id'));
-		$i = 0;
-		$list = array();
+        $db = new DB_Sql;
+        $db->query("SELECT todo_list.name, todo_list.id FROM todo_list
+            INNER JOIN todo_contact ON todo_contact.list_id = todo_list.id
+            WHERE todo_contact.contact_id = " . $contact_id . "
+                AND todo_list.intranet_id=".$this->kernel->intranet->get('id'));
+        $i = 0;
+        $list = array();
 
-		while ($db->nextRecord()) {
-			$list[$i]['id'] = $db->f('id');
-			$list[$i]['name'] = $db->f('name');
-		}
-		return $list;
-	}
+        while ($db->nextRecord()) {
+            $list[$i]['id'] = $db->f('id');
+            $list[$i]['name'] = $db->f('name');
+        }
+        return $list;
+    }
 
-	function setDone($arg) {
+    function setDone($arg) {
 
-		$credentials = $arg[0];
-		$list_id = $arg[1];
-		$id = $arg[2];
+        $credentials = $arg[0];
+        $list_id = $arg[1];
+        $id = $arg[2];
 
-   	if (is_object($return = $this->checkCredentials($credentials))) {
-			return $return;
-		}
+       if (is_object($return = $this->checkCredentials($credentials))) {
+            return $return;
+        }
 
-		$db = new DB_Sql;
-		$db->query("SELECT * FROM todo_list WHERE id = " . $list_id . " AND intranet_id = " . $this->kernel->intranet->get('id'));
-		if (!$db->nextRecord()) {
-			return new IXR_Error(-2, 'Der er ikke nogen liste');
-		}
+        $db = new DB_Sql;
+        $db->query("SELECT * FROM todo_list WHERE id = " . $list_id . " AND intranet_id = " . $this->kernel->intranet->get('id'));
+        if (!$db->nextRecord()) {
+            return new IXR_Error(-2, 'Der er ikke nogen liste');
+        }
 
-		$todo = new TodoList($this->kernel, $db->f('id'));
-		$todo->loadItem($id);
-		return $todo->item->delete();
+        $todo = new TodoList($this->kernel, $db->f('id'));
+        $todo->loadItem($id);
+        return $todo->item->delete();
   }
 
 }
 
 $server = new TodoServer();
-?>
+*/
