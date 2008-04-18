801fe51082d4de506ddd5735e4d9b284692bdd68
Lars Olesen <lars@intraface.dk>
2008-04-18 07:32:44 +0000

    Added tests.

--- tests/unit/cms/PageElementTest.php
+++ tests/unit/cms/PageElementTest.php
@@ -46,6 +46,63 @@ class PageElementTest extends PHPUnit_Framework_TestCase {
         $this->assertTrue($pagelist->save($data) > 0);
     }
 
+    function testSaveWhenUpdating()
+    {
+        $pagelist = $this->createPageList();
+
+        $data = array(
+            'elm_properties' => 'none',
+            'elm_adjust' => 'left',
+            'elm_width' => '100px',
+            'headline' => 'Test',
+            'show_type' => 'article',
+            'keyword' => array(1),
+            'show' => 1,
+            'lifetime' => 20,
+            'no_results_text' => 'none',
+            'read_more_text' => 'none'
+        );
+
+        $this->assertTrue($pagelist->save($data) > 0);
+
+        $data = array(
+            'elm_properties' => 'none',
+            'elm_adjust' => 'left',
+            'elm_width' => '100px',
+            'headline' => 'Test',
+            'show_type' => 'article',
+            'keyword' => array(1),
+            'show' => 1,
+            'lifetime' => 20,
+            'no_results_text' => 'none',
+            'read_more_text' => 'none'
+        );
+
+        $pagelist->save($data);
+    }
+
+    function testValidateFailsWhenInvalidValues()
+    {
+        $pagelist = $this->createPageList();
+
+        $data = array(
+            'elm_properties' => 'wrongvalue',
+            'elm_adjust' => 'wrongvalue',
+            'elm_width' => '100wrongvalue',
+            'elm_box' => 'wrongvalue',
+            'headline' => 'Test',
+            'show_type' => 'article',
+            'keyword' => array(1),
+            'show' => 1,
+            'lifetime' => 20,
+            'no_results_text' => 'none',
+            'read_more_text' => 'none'
+        );
+
+        $this->assertTrue($pagelist->save($data) == 0);
+
+    }
+
     function testDeleteReturnsTrue()
     {
         $pagelist = $this->createPageList();
