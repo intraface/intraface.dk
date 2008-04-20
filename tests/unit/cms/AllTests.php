89935a1486e1bca399bb6acfc6390c7cc9c7c12b
Lars Olesen <lars@intraface.dk>
2008-04-20 19:16:12 +0000

    Added the new TemplateTests.

--- tests/unit/cms/AllTests.php
+++ tests/unit/cms/AllTests.php
@@ -8,7 +8,7 @@ class CMS_AllTests {
     {
         $suite = new PHPUnit_Framework_TestSuite('Intraface_CMS');
 
-        $tests = array('Site', 'Page', 'Parameter', 'Section', 'PageElement', 'GalleryElement', 'SectionLongText', 'TemplateSection');
+        $tests = array('Template', 'Site', 'Page', 'Parameter', 'Section', 'PageElement', 'GalleryElement', 'SectionLongText', 'TemplateSection');
 
         foreach ($tests AS $test) {
             require_once $test . 'Test.php';
