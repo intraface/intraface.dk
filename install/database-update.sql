97b653516b4df051823a31fe6276c3d8937c90bb
Sune T. Jensen <sj@sunet.dk>
2008-04-20 18:26:09 +0000

    Updated sql after som mess with merge...

--- install/database-update.sql
+++ install/database-update.sql
@@ -7,27 +7,12 @@ CREATE TABLE `project` (
   `description` text NOT NULL,
   `intranet_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
-
-CREATE TABLE `project_task` (
-  `id` int(11) NOT NULL auto_increment,
-  `date_created` datetime NOT NULL,
-  `date_updated` datetime NOT NULL,
-  `project_id` int(11) NOT NULL,
-  `intranet_id` int(11) NOT NULL,
-  `item` text NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `position` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-<<<<<<< .mine
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
-ALTER TABLE `newsletter_list` ADD `optin_link` VARCHAR( 255 ) NOT NULL ;
-ALTER TABLE `newsletter_list` ADD `subscribe_subject` VARCHAR( 255 ) NOT NULL ;
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
 
 ALTER TABLE `cms_template` ADD `for_page_type` INT NOT NULL AFTER `site_id` ;
 UPDATE cms_template SET for_page_type = 7 ;
+
+ALTER TABLE `newsletter_list` ADD `optin_link` VARCHAR( 255 ) NOT NULL ;
+ALTER TABLE `newsletter_list` ADD `subscribe_subject` VARCHAR( 255 ) NOT NULL ;
 
 ALTER TABLE `redirect` ADD `cancel_url` VARCHAR( 255 ) NOT NULL ;
