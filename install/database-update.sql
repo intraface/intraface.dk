7fb7f0eeb10f87cc2d16732e79c3d7f4a93b5f6e
Lars Olesen <lars@intraface.dk>
2008-05-30 10:57:35 +0000

    Added project database to database-updates.sql

--- install/database-update.sql
+++ install/database-update.sql
@@ -1,2 +1,25 @@
 ALTER TABLE `basket` ADD `shop_id` INT( 11 ) NOT NULL ;
 ALTER TABLE `basket_details` ADD `shop_id` INT( 11 ) NOT NULL ;
+
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL auto_increment,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `intranet_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE `project_task` (
+  `id` int(11) NOT NULL auto_increment,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `intranet_id` int(11) NOT NULL,
+  `item` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `position` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
