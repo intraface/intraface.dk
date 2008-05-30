7b2f9cafb4b404b03dab9df0a59f78fe70e92366
Lars Olesen <lars@intraface.dk>
2008-05-30 13:44:51 +0000

    Removed already created project.

--- install/database-update.sql
+++ install/database-update.sql
@@ -1,16 +1,6 @@
 ALTER TABLE `basket` ADD `shop_id` INT( 11 ) NOT NULL ;
 ALTER TABLE `basket_details` ADD `shop_id` INT( 11 ) NOT NULL ;
 
-CREATE TABLE `project` (
-  `id` int(11) NOT NULL auto_increment,
-  `date_created` datetime NOT NULL,
-  `date_updated` datetime NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `intranet_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
 CREATE TABLE `project_task` (
   `id` int(11) NOT NULL auto_increment,
   `date_created` datetime NOT NULL,
