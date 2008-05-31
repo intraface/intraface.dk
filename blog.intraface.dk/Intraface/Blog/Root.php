4b1d772fc7855346c2aef31f9bb059e2cd525b16
Lars Olesen <lars@intraface.dk>
2008-05-31 05:28:21 +0000

    Changed title to Intraface.dk.

--- blog.intraface.dk/Intraface/Blog/Root.php
+++ blog.intraface.dk/Intraface/Blog/Root.php
@@ -7,7 +7,7 @@ class Intraface_Blog_Root extends k_Dispatcher
     {
         parent::__construct();
         //$this->document->template = dirname(__FILE__) . '/templates/main.tpl.php';
-        $this->document->title = 'Monaskim.dk';
+        $this->document->title = 'Intraface.dk';
         //$this->document->styles[] = $this->url('/style.css');
     }
 
