e59bc10110bfd81eddf5d25c0cd7b8b80963015c
Lars Olesen <lars@intraface.dk>
2008-05-31 05:47:40 +0000

    Now uses Ilib_ClassLoader

--- intraface.dk/demo/newshop/index.php
+++ intraface.dk/demo/newshop/index.php
@@ -4,26 +4,8 @@ require_once 'config.local.php';
 set_include_path(INTRAFACEPUBLIC_SHOP_INCLUDE_PATH);
 
 require_once 'k.php';
+require_once 'Ilib/ClassLoader.php';
 require_once 'root.php';
-
-class intraface_ClassLoader extends k_ClassLoader
-{
-  /**
-    * Default autoloader for Konstrukt naming scheme.
-    */
-  static function autoload($classname) {
-    $filename = str_replace('_', '/', $classname).'.php';
-    if (self::SearchIncludePath($filename)) {
-      require_once($filename);
-    }
-    else {
-        throw new Exception('Unable to include file '.$filename);
-    }
-  }
-}
-
-spl_autoload_register(Array('intraface_ClassLoader', 'autoload'));
-
 
 $application = new Demo_Shop_Root();
 
@@ -31,7 +13,6 @@ $application->registry->registerConstructor('admin', create_function(
   '$className, $args, $registry',
   'return XML_RPC2_Client::create(INTRAFACE_XMLPRC_SERVER_PATH . "admin/server.php", array("prefix" => "intraface."));'
 ));
-
 
 $application->registry->registerConstructor('cache', create_function(
   '$className, $args, $registry',
