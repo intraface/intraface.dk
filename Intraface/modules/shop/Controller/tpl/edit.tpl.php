d44e28ef431424baeed3c3936dcb259ecc615000
Lars Olesen <lars@intraface.dk>
2008-05-31 19:32:00 +0000

    Checks whether variable is empty before outputting it.

--- Intraface/modules/shop/Controller/tpl/edit.tpl.php
+++ Intraface/modules/shop/Controller/tpl/edit.tpl.php
@@ -1,14 +1,14 @@
 <h1><?php e('Edit'); ?></h1>
 
-<form action="<?php e(url()); ?>" method="post">
+<form action="<?php e(url(null)); ?>" method="post">
     <fieldset>
         <label for="name"><?php e(t('Name')); ?></label>
-        <input type="text" name="name" id="name" value="<?php e($data['name']); ?>" />
+        <input type="text" name="name" id="name" value="<?php if(!empty($data['name'])) e($data['name']); ?>" />
 
         <br />
 
         <label for="identifier"><?php e(t('Identifier')); ?></label>
-        <input type="text" name="identifier" id="identifier" value="<?php e($data['identifier']); ?>" />
+        <input type="text" name="identifier" id="identifier" value="<?php if(!empty($data['identifier'])) e($data['identifier']); ?>" />
 
     </fieldset>
 
@@ -32,7 +32,7 @@
         <legend><?php e(t('Order confirmation - including warranty and right of cancellation')); ?></legend>
         <div>
         <label for="confirmation_text"><?php e(t('Text')); ?></label><br />
-        <textarea name="confirmation" cols="80" rows="10"><?php e($data['confirmation']); ?></textarea>
+        <textarea name="confirmation" cols="80" rows="10"><?php  if(!empty($data['confirmation'])) e($data['confirmation']); ?></textarea>
         </div>
     </fieldset>
 
@@ -40,7 +40,7 @@
         <legend><?php e(t('Webshop receipt')); ?></legend>
         <div>
         <label for="webshop_receipt"><?php e(t('text')); ?></label><br />
-        <textarea name="receipt" cols="80" rows="10"><?php e($data['receipt']); ?></textarea>
+        <textarea name="receipt" cols="80" rows="10"><?php  if(!empty($data['receipt'])) e($data['receipt']); ?></textarea>
         </div>
     </fieldset>
 
@@ -48,7 +48,7 @@
         <legend><?php e(t('Description')); ?></legend>
         <div>
         <label for="description"><?php e(t('text')); ?></label><br />
-        <textarea name="description" cols="80" rows="10"><?php e($data['description']); ?></textarea>
+        <textarea name="description" cols="80" rows="10"><?php  if(!empty($data['description'])) e($data['description']); ?></textarea>
         </div>
     </fieldset>
 
