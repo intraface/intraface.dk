5b3ba7a754c939bdafe2cf601bad6ac5a83da38d
Lars Olesen <lars@intraface.dk>
2008-06-01 08:06:38 +0000

    Now adds shop to basketevaluation.

--- Intraface/modules/shop/Controller/EvaluationEdit.php
+++ Intraface/modules/shop/Controller/EvaluationEdit.php
@@ -1,18 +1,24 @@
 <?php
 class Intraface_modules_shop_Controller_EvaluationEdit extends k_Controller
 {
+    
+	function getShop()
+	{
+		$doctrine = $this->registry->get('doctrine');
+		return Doctrine::getTable('Intraface_modules_shop_Shop')->findOneById($this->context->name);
+	}
+	
     function GET()
     {
         if (!empty($this->GET['delete'])) {
-            $basketevaluation = new Intraface_modules_shop_BasketEvaluation($this->registry->get('db'), $this->registry->get('intranet'), (int)$this->GET['delete']);
+            $basketevaluation = new Intraface_modules_shop_BasketEvaluation($this->registry->get('db'), $this->registry->get('intranet'), $this->getShop(), (int)$this->GET['delete']);
             $basketevaluation->delete();
             throw new k_http_Redirect($this->url('../'));
         } elseif (isset($this->GET['id'])) {
-            $basketevaluation = new Intraface_modules_shop_BasketEvaluation($this->registry->get('db'), $this->registry->get('intranet'), (int)$this->GET['id']);
+            $basketevaluation = new Intraface_modules_shop_BasketEvaluation($this->registry->get('db'), $this->registry->get('intranet'), $this->getShop(), (int)$this->GET['id']);
             $value = $basketevaluation->get();
         } else {
-			$doctrine = $this->registry->get('doctrine');
-            $basketevaluation = new Intraface_modules_shop_BasketEvaluation($this->registry->get('db'), $this->registry->get('intranet'), new Intraface_modules_shop_Shop);
+            $basketevaluation = new Intraface_modules_shop_BasketEvaluation($this->registry->get('db'), $this->registry->get('intranet'), $this->getShop());
             $value = array();
         }
         $settings = $basketevaluation->get('settings');
@@ -25,7 +31,7 @@ class Intraface_modules_shop_Controller_EvaluationEdit extends k_Controller
 
     function POST()
     {
-        $basketevaluation = new Intraface_modules_shop_BasketEvaluation($this->registry->get('db'), $this->registry->get('intranet'), (int)$this->POST['id']);
+        $basketevaluation = new Intraface_modules_shop_BasketEvaluation($this->registry->get('db'), $this->registry->get('intranet'), $this->getShop(), (int)$this->POST['id']);
 
         if (!$basketevaluation->save($this->POST->getArrayCopy())) {
             throw new Exception('Could not save values');
