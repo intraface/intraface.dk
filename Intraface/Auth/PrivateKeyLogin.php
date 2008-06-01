577b8cb7ffe81b1d7e0deb6fb73b6778af6fc9f1
Lars Olesen <lars@intraface.dk>
2008-06-01 07:38:50 +0000

    Made sure that the returned Intraface_Weblogin was correctly created with intranet.

--- Intraface/Auth/PrivateKeyLogin.php
+++ Intraface/Auth/PrivateKeyLogin.php
@@ -50,15 +50,15 @@ class Intraface_Auth_PrivateKeyLogin
      */
     public function auth()
     {
-    	$result = $this->db->query("SELECT id FROM intranet WHERE private_key = " . $this->db->quote($this->key, 'text'));
+        $result = $this->db->query("SELECT id FROM intranet WHERE private_key = " . $this->db->quote($this->key, 'text'));
         if (PEAR::isError($result)) {
-			throw new Exception($result->getUserInfo());
+            throw new Exception($result->getUserInfo());
         }
         if($result->numRows() == 0) {
-        	return false;
+            return false;
         }
         $row = $result->fetchRow(MDB2_FETCHMODE_ASSOC);
-        
-        return new Intraface_Weblogin($row['id']);
+
+        return new Intraface_Weblogin($this->session_id, new Intraface_Intranet($row['id']));
     }
 }
