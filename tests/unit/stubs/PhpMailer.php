74f2c18b679f341b127a7a8673cc45d4838a2565
Sune T. Jensen <sj@sunet.dk>
2008-06-18 14:43:17 +0000

    Added FakePhpMailer

---
+++ tests/unit/stubs/PhpMailer.php
@@ -0,0 +1,57 @@
+<?php
+
+class FakePhpMailer {
+    
+    public $Mailer;
+    public $Wordwrap;
+    public $From;
+    public $FromName;
+    public $Subject;
+    public $Body;
+    public $ErrorInfo;
+    
+    private $is_send;
+    
+    public function __construct()
+    {
+        $this->ErrorInfo = '';
+        $this->is_send = false;
+    }
+    
+    public function setLanguage($language) 
+    {
+        
+    }
+    
+    public function addAddress($email, $name) 
+    {
+        
+    }
+    
+    public function addBcc($email, $name) 
+    {
+        
+    }
+    
+    public function addAttachment($file, $filename)
+    {
+        
+    }
+    
+    public function send() 
+    {
+        $this->is_send = true;
+    }
+    
+    public function clearAddresses()
+    {
+        
+    }
+    
+    public function isSend()
+    {
+        return $this->is_send;
+    }
+}
+
+?>
