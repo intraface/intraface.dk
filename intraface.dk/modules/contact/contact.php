9f91cc772d36b0dba9b86f152ca40525d01b87b9
Lars Olesen <lars@intraface.dk>
2008-06-25 14:28:33 +0000

    Put in e() and t() for readability.

--- intraface.dk/modules/contact/contact.php
+++ intraface.dk/modules/contact/contact.php
@@ -75,47 +75,47 @@ $page->start(safeToHtml($translation->get('contact information') . ' ' .$contact
 
 <div class="box">
 
-    <h1>#<?php echo safeToHtmL($value['number']); ?> <?php echo safeToHtml($address['name']); ?></h1>
+    <h1>#<?php e($value['number']); ?> <?php e($address['name']); ?></h1>
 
     <?php echo $contact->error->view(); ?>
 
     <ul class="options">
-        <li><a href="contact_edit.php?id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('edit', 'common')); ?></a></li>
-        <li><a href="index.php?from_contact_id=<?php print($contact->get("id")); ?>&amp;use_stored=true"><?php echo safeToHtml($translation->get('close', 'common')); ?></a></li>
-        <li><a class="vcard" href="vcard.php?id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('vcard')); ?></a></li>
+        <li><a href="contact_edit.php?id=<?php print($contact->get("id")); ?>"><?php e(t('edit', 'common')); ?></a></li>
+        <li><a href="index.php?from_contact_id=<?php print($contact->get("id")); ?>&amp;use_stored=true"><?php e(t('close', 'common')); ?></a></li>
+        <li><a class="vcard" href="vcard.php?id=<?php print($contact->get("id")); ?>"><?php e(t('vcard')); ?></a></li>
     </ul>
 
     <?php if($kernel->user->hasModuleAccess("debtor")): ?>
         <ul class="options">
         <?php if($kernel->user->hasModuleAccess("quotation")): ?>
             <?php if ($quotation->any('contact', $contact->get("id"))): ?>
-            <li><a href="<?php print($debtor->getPath()); ?>list.php?type=quotation&amp;contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('quotation', 'debtor')); ?></a></li>
+            <li><a href="<?php print($debtor->getPath()); ?>list.php?type=quotation&amp;contact_id=<?php print($contact->get("id")); ?>"><?php e(t('quotation', 'debtor')); ?></a></li>
             <?php else: ?>
-            <li class="inactive"><a href="<?php print($debtor->getPath()); ?>edit.php?type=quotation&amp;contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('create quotation', 'debtor')); ?></a></li>
+            <li class="inactive"><a href="<?php print($debtor->getPath()); ?>edit.php?type=quotation&amp;contact_id=<?php print($contact->get("id")); ?>"><?php e(t('create quotation', 'debtor')); ?></a></li>
             <?php endif; ?>
         <?php endif; ?>
 
         <?php if($kernel->user->hasModuleAccess("order")): ?>
             <?php if($order->any('contact', $contact->get("id"))): ?>
-                <li><a href="<?php print($debtor->getPath()); ?>list.php?type=order&amp;contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('orders', 'debtor')); ?></a></li>
+                <li><a href="<?php print($debtor->getPath()); ?>list.php?type=order&amp;contact_id=<?php print($contact->get("id")); ?>"><?php e(t('orders', 'debtor')); ?></a></li>
             <?php else: ?>
-                <li class="inactive"><a href="<?php print($debtor->getPath()); ?>edit.php?type=order&amp;contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('create order', 'debtor')); ?></a></li>
+                <li class="inactive"><a href="<?php print($debtor->getPath()); ?>edit.php?type=order&amp;contact_id=<?php print($contact->get("id")); ?>"><?php e(t('create order', 'debtor')); ?></a></li>
             <?php endif; ?>
         <?php endif; ?>
 
         <?php if($kernel->user->hasModuleAccess("invoice")): ?>
             <?php if ($invoice->any('contact', $contact->get("id"))): ?>
-                <li><a href="<?php print($debtor->getPath()); ?>list.php?type=invoice&amp;contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('invoices', 'debtor')); ?></a></li>
+                <li><a href="<?php print($debtor->getPath()); ?>list.php?type=invoice&amp;contact_id=<?php print($contact->get("id")); ?>"><?php e(t('invoices', 'debtor')); ?></a></li>
             <?php else: ?>
-                <li class="inactive"><a href="<?php print($debtor->getPath()); ?>edit.php?type=invoice&amp;contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('create invoice', 'debtor')); ?></a></li>
+                <li class="inactive"><a href="<?php print($debtor->getPath()); ?>edit.php?type=invoice&amp;contact_id=<?php print($contact->get("id")); ?>"><?php e(t('create invoice', 'debtor')); ?></a></li>
             <?php endif; ?>
             <?php if ($creditnote->any('contact', $contact->get("id"))): ?>
-                <li><a href="<?php print($debtor->getPath()); ?>list.php?type=credit_note&amp;contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('credit notes', 'debtor')); ?></a></li>
+                <li><a href="<?php print($debtor->getPath()); ?>list.php?type=credit_note&amp;contact_id=<?php print($contact->get("id")); ?>"><?php e(t('credit notes', 'debtor')); ?></a></li>
             <?php endif; ?>
             <?php if($reminder->any($contact->get("id"))): ?>
-                <li><a href="<?php print($debtor->getPath()); ?>reminders.php?contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('reminders', 'debtor')); ?></a></li>
+                <li><a href="<?php print($debtor->getPath()); ?>reminders.php?contact_id=<?php print($contact->get("id")); ?>"><?php e(t('reminders', 'debtor')); ?></a></li>
             <?php elseif($invoice->anyDue($contact->get("id"))): ?>
-                <li class="inactive"><a href="<?php print($debtor->getPath()); ?>reminder_edit.php?contact_id=<?php print($contact->get("id")); ?>"><?php echo safeToHtml($translation->get('create reminder', 'debtor')); ?></a></li>
+                <li class="inactive"><a href="<?php print($debtor->getPath()); ?>reminder_edit.php?contact_id=<?php print($contact->get("id")); ?>"><?php e(t('create reminder', 'debtor')); ?></a></li>
             <?php endif; ?>
         <?php endif; ?>
 
@@ -125,7 +125,7 @@ $page->start(safeToHtml($translation->get('contact information') . ' ' .$contact
             $procurement = new Procurement($kernel);
             if($procurement->any($contact->get('id'))) {
                 ?>
-                <li><a href="<?php print($procurement_module->getPath()."index.php?contact_id=".$contact->get('id')); ?>"><?php echo safeToHtml($translation->get('procurement', 'procurement')); ?></a></li>
+                <li><a href="<?php print($procurement_module->getPath()."index.php?contact_id=".$contact->get('id')); ?>"><?php e(t('procurement', 'procurement')); ?></a></li>
                 <?php
             }
             ?>
@@ -144,54 +144,54 @@ $page->start(safeToHtml($translation->get('contact information') . ' ' .$contact
     <form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
     <input type="hidden" name="id" value="<?php echo $contact->get('id'); ?>" />
     <table>
-        <caption><?php echo safeToHtml($translation->get('contact information')); ?></caption>
+        <caption><?php e(t('contact information')); ?></caption>
         <tbody>
             <tr class="vcard">
-                <th><?php echo safeToHtml($translation->get('address', 'address')); ?></th>
+                <th><?php e(t('address', 'address')); ?></th>
                 <td class="adr">
-                    <span class="fn"><?php echo safeToHtml($address['name']); ?></span>
+                    <span class="fn"><?php e($address['name']); ?></span>
                     <div class="adr">
                         <div class="street-address"><?php echo nl2br(safeToHtml($address['address'])); ?></div>
-                        <span class="postal-code"><?php echo safeToHtml($address['postcode']); ?></span>
-                        <span class="locality"><?php echo safeToHtml($address['city']); ?></span>
-                        <div class="country"><?php echo safeToHtml($address['country']); ?></div>
+                        <span class="postal-code"><?php e($address['postcode']); ?></span>
+                        <span class="locality"><?php e($address['city']); ?></span>
+                        <div class="country"><?php e($address['country']); ?></div>
                     </div>
                 </td>
             </tr>
             <?php if (is_object($contact->delivery_address) AND !empty($delivery_address['address'])): ?>
             <tr class="vcard">
-                <th><?php echo safeToHtml($translation->get('delivery address')); ?></th>
+                <th><?php e(t('delivery address')); ?></th>
                 <td>
-                <span class="fn"><?php echo safeToHtml($delivery_address['name']); ?></span>
+                <span class="fn"><?php e($delivery_address['name']); ?></span>
                 <div class="adr">
                     <div class="street-address"><?php echo nl2br(safeToHtmL($delivery_address['address'])); ?></div>
-                    <span class="postal-code"><?php echo safeToHtml($delivery_address['postcode']); ?></span>
-                    <span class="locality"><?php echo safeToHtml($delivery_address['city']); ?></span>
-                    <div class="country"><?php echo safeToHtml($delivery_address['country']); ?></div>
+                    <span class="postal-code"><?php e($delivery_address['postcode']); ?></span>
+                    <span class="locality"><?php e($delivery_address['city']); ?></span>
+                    <div class="country"><?php e($delivery_address['country']); ?></div>
                 </div>
                 </td>
             </tr>
             <?php endif; ?>
             <tr>
-                <th><?php echo safeToHtml($translation->get('phone', 'address')); ?></th>
-                <td class="tel"><?php echo safeToHtml($address['phone']); ?></td>
+                <th><?php e(t('phone', 'address')); ?></th>
+                <td class="tel"><?php e($address['phone']); ?></td>
             </tr>
             <tr>
-                <th><?php echo safeToHtml($translation->get('email', 'address')); ?></th>
-                <td class="email"><?php echo safeToHtml($address['email']); ?></td>
+                <th><?php e(t('email', 'address')); ?></th>
+                <td class="email"><?php e($address['email']); ?></td>
             </tr>
             <tr>
-                <th><?php echo safeToHtml($translation->get('website', 'address')); ?></th>
-                <td class="url"><?php echo safeToHtml($address['website']); ?></td>
+                <th><?php e(t('website', 'address')); ?></th>
+                <td class="url"><?php e($address['website']); ?></td>
             </tr>
             <?php if ($kernel->intranet->get('identifier')): ?>
             <tr>
-                <th><?php echo safeToHtml($translation->get('code')); ?></th>
+                <th><?php e(t('code')); ?></th>
                 <td>
-                    <?php echo safeToHtml($contact->get('password')); ?>
-                    <input type="submit" value="<?php echo safeToHtml($translation->get('new', 'common')); ?>" class="confirm" name="new_password" />
+                    <?php e($contact->get('password')); ?>
+                    <input type="submit" value="<?php e(t('new', 'common')); ?>" class="confirm" name="new_password" />
                     <?php if (!empty($address['email'])): ?>
-                        <input type="submit" name="send_email" value="<?php echo safeToHtml($translation->get('send e-mail with login')); ?>" class="confirm" title="Er du sikker på, at du vil sende e-mail?" />
+                        <input type="submit" name="send_email" value="<?php e(t('send e-mail with login')); ?>" class="confirm" title="Er du sikker på, at du vil sende e-mail?" />
                     <?php endif; ?>
                 </td>
             </tr>
@@ -208,7 +208,7 @@ $page->start(safeToHtml($translation->get('contact information') . ' ' .$contact
   <?php if (count($persons) > 0) { ?>
 
     <table>
-    <caption><?php echo safeToHtml($translation->get('contact persons')); ?></caption>
+    <caption><?php e(t('contact persons')); ?></caption>
     <tbody>
     <?php
     foreach ($persons AS $person) {
@@ -224,7 +224,7 @@ $page->start(safeToHtml($translation->get('contact information') . ' ' .$contact
     </table>
   <?php } ?>
     <ul class="options">
-        <li><a href="contactperson_edit.php?contact_id=<?php echo intval($value['id']); ?>"><?php echo safeToHtml($translation->get('add contact person')); ?></a></li>
+        <li><a href="contactperson_edit.php?contact_id=<?php echo intval($value['id']); ?>"><?php e(t('add contact person')); ?></a></li>
     </ul>
 <?php endif; ?>
 
@@ -234,14 +234,14 @@ $reminder->createDbquery();
 $reminders = $reminder->getList();
 if(count($reminders) > 0) {
     ?>
-    <h2><?php echo safeToHtml($translation->get('reminders')); ?></h2>
+    <h2><?php e(t('reminders')); ?></h2>
 
     <table class="stripe">
-        <caption><?php echo safeToHtml($translation->get('reminders')); ?></caption>
+        <caption><?php e(t('reminders')); ?></caption>
         <thead>
         <tr>
-            <th><?php echo safeToHtml($translation->get('date')); ?></th>
-            <th><?php echo safeToHtml($translation->get('subject')); ?></th>
+            <th><?php e(t('date')); ?></th>
+            <th><?php e(t('subject')); ?></th>
             <th></th>
         </tr>
         </thead>
@@ -256,12 +256,12 @@ if(count($reminders) > 0) {
                         echo '<span class="due">'.safeToHtml($reminder_item['dk_reminder_date']).'</span>';
                     }
                     else {
-                        echo safeToHtml($reminder_item['dk_reminder_date']);
+                        e($reminder_item['dk_reminder_date']);
                     }
                     ?>
                 </td>
-                <td><a href="reminder.php?id=<?php echo intval($reminder_item['id']); ?>"><?php echo safeToHtml($reminder_item['subject']); ?></a></td>
-                <td class="buttons"><a href="reminder_edit.php?id=<?php echo intval($reminder_item['id']); ?>" class="edit"><?php echo safeToHtml($translation->get('edit', 'common')); ?></a></td>
+                <td><a href="reminder.php?id=<?php echo intval($reminder_item['id']); ?>"><?php e($reminder_item['subject']); ?></a></td>
+                <td class="buttons"><a href="reminder_edit.php?id=<?php echo intval($reminder_item['id']); ?>" class="edit"><?php e(t('edit', 'common')); ?></a></td>
             </tr>
             <?php
         }
@@ -273,7 +273,7 @@ if(count($reminders) > 0) {
 ?>
 
 <ul class="options">
-    <li><a href="reminder_edit.php?contact_id=<?php echo intval($value['id']); ?>"><?php echo safeToHtml($translation->get('add reminder')); ?></a></li>
+    <li><a href="reminder_edit.php?contact_id=<?php echo intval($value['id']); ?>"><?php e(t('add reminder')); ?></a></li>
 </ul>
 
 
@@ -283,40 +283,40 @@ if(count($reminders) > 0) {
 <?php if ($value['type'] == "corporation"): ?>
 <?php //if ($kernel->user->hasModuleAccess('debtor') AND !empty($address['cvr'])): ?>
 <div id="paymentinformation" class="box">
-<h2><?php echo safeToHtml($translation->get('payment information')); ?></h2>
+<h2><?php e(t('payment information')); ?></h2>
 <table class="stripe">
-<caption><?php echo safeToHtml($translation->get('payment information')); ?></caption>
+<caption><?php e(t('payment information')); ?></caption>
 <?php if ($value['type'] != "private"): ?>
 
     <?php if (!empty($address['cvr'])): ?>
 <tr>
-    <td><?php echo safeToHtml($translation->get('cvr number', 'address')); ?></td>
+    <td><?php e(t('cvr number', 'address')); ?></td>
     <td><?php echo '<a href="http://www.cvr.dk/Site/Forms/PublicService/DisplayCompany.aspx?cvrnr='.rawurlencode($address['cvr']).'">' . safeToHtml($address['cvr']); ?></a></td>
 </tr>
     <?php endif; ?>
     <?php if (!empty($address['ean'])): ?>
 <tr>
-    <td><?php echo safeToHtml($translation->get('ean number', 'address')); ?></td>
-    <td><?php echo safeToHtml($address['ean']); ?></td>
+    <td><?php e(t('ean number', 'address')); ?></td>
+    <td><?php e($address['ean']); ?></td>
 </tr>
     <?php endif; ?>
 <?php endif; ?>
 <tr>
-    <td><?php echo safeToHtml($translation->get('payment conditions')); ?></td>
+    <td><?php e(t('payment conditions')); ?></td>
      <td><?php
     foreach ($contact_module->getSetting("paymentcondition") AS $key=>$v) {
-        if (isset($value['paymentcondition']) AND $v == $value['paymentcondition']) { echo safeToHtml($v); }
+        if (isset($value['paymentcondition']) AND $v == $value['paymentcondition']) { e($v); }
     }
-?> <?php echo safeToHtml($translation->get('days')); ?></td>
+?> <?php e(t('days')); ?></td>
     </tr>
 </table>
 </div>
 <?php endif; ?>
 
 <div id="keywords" class="box <?php if (!empty($_GET['from']) AND $_GET['from'] == 'keywords') echo ' fade'; ?>">
- <h2><?php echo safeToHtml($translation->get('keywords', 'keyword')); ?></h2>
+ <h2><?php e(t('keywords', 'keyword')); ?></h2>
     <?php if ($contact->get('locked') == 0) { ?>
-    <ul class="button"><li><a href="<?php echo PATH_WWW; ?>/shared/keyword/connect.php?contact_id=<?php echo $contact->get('id'); ?>"><?php echo safeToHtml($translation->get('add keywords', 'keyword')); ?></a></li></ul>
+    <ul class="button"><li><a href="<?php echo PATH_WWW; ?>/shared/keyword/connect.php?contact_id=<?php echo $contact->get('id'); ?>"><?php e(t('add keywords', 'keyword')); ?></a></li></ul>
     <?php } ?>
     <?php
         $keyword = $contact->getKeywordAppender();
@@ -355,9 +355,9 @@ if(count($reminders) > 0) {
 
 <?php if ($kernel->intranet->get('identifier')): ?>
 <div class="box">
-    <h2><?php echo safeToHtml($translation->get('tools')); ?></h2>
+    <h2><?php e(t('tools')); ?></h2>
 <ul>
-    <li><a href="<?php echo $contact->getLoginUrl(); ?>"><?php echo safeToHtml($translation->get('see contact login')); ?></a></li>
+    <li><a href="<?php echo $contact->getLoginUrl(); ?>"><?php e(t('see contact login')); ?></a></li>
 </ul>
 </div>
 <?php endif; ?>
