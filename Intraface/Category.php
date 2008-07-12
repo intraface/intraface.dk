4842d4c634d0d99e8cc5819e17e842203f5e5250
Sune T. Jensen <sj@sunet.dk>
2008-07-12 22:16:04 +0000

    Bug fix with intranet_id

--- Intraface/Category.php
+++ Intraface/Category.php
@@ -6,7 +6,7 @@ class Intraface_Category extends Ilib_Category
     
     public function __construct($kernel, $db, $type, $id = NULL)
     {
-        $options = array('intranet_id = '.$kernel->intranet->getId());
+        $options = array('extra_condition' => array('intranet_id = '.$kernel->intranet->getId()));
         parent::__construct($db, $type, $id, $options);
     }
 }
