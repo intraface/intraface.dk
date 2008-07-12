a31b6ae8563f0e91c1cdfe2ccc29b9ccef2a24df
Sune T. Jensen <sj@sunet.dk>
2008-07-12 21:23:29 +0000

    Bug fix

--- Intraface/Category.php
+++ Intraface/Category.php
@@ -7,7 +7,7 @@ class Intraface_Category extends Ilib_Category
     public function __construct($kernel, $db, $type, $id = NULL)
     {
         $options = array('intranet_id = '.$kernel->intranet->getId());
-        parent::__contruct($db, $type, $id, $options);
+        parent::__construct($db, $type, $id, $options);
     }
 }
 
