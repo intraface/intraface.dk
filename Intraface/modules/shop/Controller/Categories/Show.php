8decf21c9c9ef5fbf5e319ca350cbe986d6186d1
Lars Olesen <lars@intraface.dk>
2008-07-15 09:10:28 +0000

    Added getModel() to Show, so it is possible to edit a category.

--- Intraface/modules/shop/Controller/Categories/Show.php
+++ Intraface/modules/shop/Controller/Categories/Show.php
@@ -1,6 +1,10 @@
 <?php
 class Intraface_modules_shop_Controller_Categories_Show extends k_Controller
 {
+    function getModel($id = 0)
+    {
+        return $this->context->getModel($id);
+    }
     
     function forward($name) 
     {
