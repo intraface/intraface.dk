4c05bb5522452e00654960e2740547607eeae329
Lars Olesen <lars@intraface.dk>
2008-07-15 09:15:17 +0000

    Coding standard improvements.

--- Intraface/modules/product/Variation/Gateway.php
+++ Intraface/modules/product/Variation/Gateway.php
@@ -32,19 +32,19 @@ class Intraface_modules_product_Variation_Gateway
      */
     public function __construct($product)
     {
-        if(!$product->get('has_variation')) {
+        if (!$product->hasVariation()) {
             throw new Exception('You can not get variation for a product without variations!');
         }
         
         $this->product = $product;
         $this->groups = $product->getAttributeGroups();
         
-        if(count($this->groups) == 0) {
+        if (count($this->groups) == 0) {
             throw new Exception('No groups is added to the product');
-        } elseif(count($this->groups) == 1) {
+        } elseif (count($this->groups) == 1) {
             $this->variation = new Intraface_modules_product_Variation_OneAttributeGroup;
             
-        } elseif(count($this->groups) == 2) {
+        } elseif (count($this->groups) == 2) {
             $this->variation =  new Intraface_modules_product_Variation_TwoAttributeGroups;
         } else {
             throw new Exception('At the moment the system only supports up to two attribute groups!');
@@ -72,14 +72,14 @@ class Intraface_modules_product_Variation_Gateway
         $query = $this->variation->getTable()->createQuery();
         
         $select = get_class($this->variation).'.*, detail.*, a1.*, a1_attribute.*, a1_attribute_group.*';
-        if(count($this->groups) == 2) $select .= ', a2.*, a2_attribute.*, a2_attribute_group.*';
+        if (count($this->groups) == 2) $select .= ', a2.*, a2_attribute.*, a2_attribute_group.*';
         $query = $query->select($select)
             ->leftJoin(get_class($this->variation).'.detail detail')
             ->innerJoin(get_class($this->variation).'.attribute1 a1 WITH a1.attribute_number = 1')
             ->innerJoin('a1.attribute a1_attribute')
             ->innerJoin('a1_attribute.group a1_attribute_group');
         
-        if(count($this->groups) == 2) {
+        if (count($this->groups) == 2) {
             $query->innerJoin(get_class($this->variation).'.attribute2 a2 WITH a2.attribute_number = 2')
                 ->innerJoin('a2.attribute a2_attribute')
                 ->innerJoin('a2_attribute.group a2_attribute_group');
@@ -87,10 +87,10 @@ class Intraface_modules_product_Variation_Gateway
         
         $collection = $query->where(get_class($this->variation).'.product_id = ? AND id = ?', array($this->product->getId(), $id))->execute();
         
-        if(!$collection || $collection->count() == 0) {
+        if (!$collection || $collection->count() == 0) {
             throw new Intraface_Gateway_Exception('Unable to find variation');
         }
-        if($collection->count() > 1) {
+        if ($collection->count() > 1) {
             throw new Exception('More than one entry found!');
         }
         return $collection->getFirst();    
@@ -108,16 +108,16 @@ class Intraface_modules_product_Variation_Gateway
             ->leftJoin(get_class($this->variation).'.detail detail')
             ->innerJoin(get_class($this->variation).'.attribute1 a1 WITH a1.attribute_number = 1 AND a1.product_attribute_id = ?', array($value['attribute1']));
         
-        if(count($this->groups) == 2) {
+        if (count($this->groups) == 2) {
             $query->innerJoin(get_class($this->variation).'.attribute2 a2 WITH a2.attribute_number = 2 AND a2.product_attribute_id = ?', array($value['attribute2']));
         }
         
         $collection = $query->where(get_class($this->variation).'.product_id = ?', $this->product->getId())->execute();
         
-        if(!$collection || $collection->count() == 0) {
+        if (!$collection || $collection->count() == 0) {
             throw new Intraface_Gateway_Exception('Unable to find variation');
         }
-        if($collection->count() > 1) {
+        if ($collection->count() > 1) {
             throw new Exception('More than one entry found!');
         }
         return $collection->getFirst();
@@ -134,7 +134,7 @@ class Intraface_modules_product_Variation_Gateway
         $query = $this->variation->getTable()->createQuery();
         
         $select = get_class($this->variation).'.*, detail.*, a1.*, a1_attribute.*, a1_attribute_group.*';
-        if(count($this->groups) == 2) $select .= ', a2.*, a2_attribute.*, a2_attribute_group.*';
+        if (count($this->groups) == 2) $select .= ', a2.*, a2_attribute.*, a2_attribute_group.*';
         
         $query = $query->select($select)
             ->leftJoin(get_class($this->variation).'.detail detail')
@@ -143,7 +143,7 @@ class Intraface_modules_product_Variation_Gateway
             ->innerJoin('a1_attribute.group a1_attribute_group')
             ->orderBy('a1_attribute.position');
         
-        if(count($this->groups) == 2) {
+        if (count($this->groups) == 2) {
             $query->innerJoin(get_class($this->variation).'.attribute2 a2 WITH a2.attribute_number = 2')
                 ->innerJoin('a2.attribute a2_attribute')
                 ->innerJoin('a2_attribute.group a2_attribute_group')
@@ -154,12 +154,10 @@ class Intraface_modules_product_Variation_Gateway
         // echo $query->getSqlQuery();
         $collection = $query->execute();
         
-        if(!$collection || $collection->count() == 0) {
+        if (!$collection || $collection->count() == 0) {
             throw new Intraface_Gateway_Exception('Unable to find variation');
         }
         
         return $collection;
     } 
 }
-
-?>
