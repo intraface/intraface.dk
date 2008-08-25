d89a2be196d4835ad0a9fd9391ce0292d4b2e169
Lars Olesen <lars@intraface.dk>
2008-08-25 17:51:27 +0000

    Now sends using smtp.

--- Intraface/Mail.php
+++ Intraface/Mail.php
@@ -16,7 +16,14 @@ class Intraface_Mail
         require_once 'phpmailer/class.phpmailer.php';
         $phpmailer = new Phpmailer;
         // opsætning
-        $phpmailer->Mailer   = 'mail'; // Alternative to IsSMTP()
+        //$phpmailer->Mailer   = 'mail'; // Alternative to IsSMTP()
+        $phpmailer->isSMTP();//
+        $phpmailer->Host = 'mail.dev.intraface.dk';//smpt.domain.com
+        $phpmailer->Port = 25;//usually 25
+        $phpmailer->SMTPAuth = true;  // Auth Type
+        $phpmailer->Username = SMTP_USERNAME;
+        $phpmailer->Password = SMTP_PASSWORD;        
+        $phpmailer->SMTPKeepAlive = false; 
         $phpmailer->WordWrap = 75;
         $phpmailer->setLanguage('en', 'phpmailer/language/');
         return $phpmailer;
