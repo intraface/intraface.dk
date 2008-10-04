e69fcf24b95e5b09aa3e65941c099ef8cfa4c1d6
Sune T. Jensen <sj@sunet.dk>
2008-10-04 12:52:58 +0000

    Now changed mail delivery back to sendmail.

--- Intraface/Mail.php
+++ Intraface/Mail.php
@@ -16,14 +16,14 @@ class Intraface_Mail
         require_once 'phpmailer/class.phpmailer.php';
         $phpmailer = new Phpmailer;
         // opsætning
-        //$phpmailer->Mailer   = 'mail'; // Alternative to IsSMTP()
-        $phpmailer->isSMTP();//
-        $phpmailer->Host = SMTP_HOST; //smpt.domain.com
-        $phpmailer->Port = 25;//usually 25
-        $phpmailer->SMTPAuth = true;  // Auth Type
-        $phpmailer->Username = SMTP_USERNAME;
-        $phpmailer->Password = SMTP_PASSWORD;        
-        $phpmailer->SMTPKeepAlive = false; 
+        $phpmailer->Mailer   = 'mail'; // Alternative to IsSMTP()
+        // $phpmailer->isSMTP();//
+        // $phpmailer->Host = SMTP_HOST; //smpt.domain.com
+        // $phpmailer->Port = 25;//usually 25
+        // $phpmailer->SMTPAuth = true;  // Auth Type
+        // $phpmailer->Username = SMTP_USERNAME;
+        // $phpmailer->Password = SMTP_PASSWORD;        
+        // $phpmailer->SMTPKeepAlive = false; 
         $phpmailer->WordWrap = 75;
         $phpmailer->setLanguage('en');
         return $phpmailer;
