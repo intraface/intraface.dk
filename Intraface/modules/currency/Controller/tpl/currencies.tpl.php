b6252d29830f3ea79e21b665d6e5084eda6ecf75
Lars Olesen <lars@intraface.dk>
2008-10-06 09:27:02 +0000

    Now selenium test for currencies passes again.

--- Intraface/modules/currency/Controller/tpl/currencies.tpl.php
+++ Intraface/modules/currency/Controller/tpl/currencies.tpl.php
@@ -12,8 +12,8 @@
     <tr>
         <td><?php e($currency->getType()->getDescription()); ?></td>
         <td><?php e($currency->getType()->getIsoCode()); ?></td>
-        <td><?php  $rate = $currency->getProductPriceExchangeRate();  if($rate === false): e(t('Not given')); else: e($rate->getRate()->get('da_dk').' ('.$rate->getDateUpdated()->get('da_dk').')'); endif; ?> <a class="edit" href="<?php e($this->url($currency->getId().'/exchangerate/productprice/update')) ?>"><?php e(t('Update', 'common')); ?></a></td>
-        <td><?php  $rate = $currency->getPaymentExchangeRate();  if($rate === false): e(t('Not given')); else: e($rate->getRate()->get('da_dk').' ('.$rate->getDateUpdated()->get('da_dk').')'); endif; ?> <a class="edit" href="<?php e($this->url($currency->getId().'/exchangerate/payment/update')) ?>"><?php e(t('Update', 'common')); ?></a></td>
+        <td><?php  $rate = $currency->getProductPriceExchangeRate();  if($rate === false): e(t('Not given')); else: e($rate->getRate()->getAsLocal('da_dk').' ('.$rate->getDateUpdated()->getAsLocal('da_dk').')'); endif; ?> <a class="edit" href="<?php e($this->url($currency->getId().'/exchangerate/productprice/update')) ?>"><?php e(t('Update', 'common')); ?></a></td>
+        <td><?php  $rate = $currency->getPaymentExchangeRate();  if($rate === false): e(t('Not given')); else: e($rate->getRate()->getAsLocal('da_dk').' ('.$rate->getDateUpdated()->getAsLocal('da_dk').')'); endif; ?> <a class="edit" href="<?php e($this->url($currency->getId().'/exchangerate/payment/update')) ?>"><?php e(t('Update', 'common')); ?></a></td>
     </tr>
     <?php endforeach; ?>
     </tbody>
