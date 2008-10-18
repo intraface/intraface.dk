dc32afe08d02a19ea66ee6bcf0f3936d6229d88a
Sune T. Jensen <sj@sunet.dk>
2008-10-18 17:54:54 +0000

    changed prefix back again

--- intraface.dk/xmlrpc/onlinepayment/server0001.php
+++ intraface.dk/xmlrpc/onlinepayment/server0001.php
@@ -4,7 +4,7 @@ XML_RPC2_Backend::setBackend('php');
 $HTTP_RAW_POST_DATA = file_get_contents('php://input');
 
 $options = array(
-    'prefix' => 'onlinepayment.',
+    'prefix' => 'shop.',
     'encoding' => 'utf-8');
 
 $server = XML_RPC2_ServerFixedEncodingObject::create(new Intraface_XMLRPC_OnlinePayment_Server0001(), $options);
