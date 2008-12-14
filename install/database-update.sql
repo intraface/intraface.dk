3384296e1b41222cc22207a159a0d5be44a66227
Lars Olesen <lars@intraface.dk>
2008-12-14 15:47:01 +0000

    Added new sql for languages.

--- install/database-update.sql
+++ install/database-update.sql
@@ -3,3 +3,13 @@
 ALTER TABLE `dbquery_result` ADD INDEX `search` ( `id` , `intranet_id` , `session_id` , `name` )  
 ALTER TABLE `dbquery_result` DROP INDEX `intranet_id`  
 ALTER TABLE `file_handler` ADD INDEX `simple_find` ( `id` , `intranet_id` )  
+
+
+
+CREATE TABLE IF NOT EXISTS `language` (
+  `id` int(11) NOT NULL auto_increment,
+  `intranet_id` int(11) NOT NULL,
+  `type_key` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `intranet_id` (`intranet_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
