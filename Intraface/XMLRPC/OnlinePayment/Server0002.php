e7d2e2899d3c31e5d69678a37c21e50cdf6eb6c4
Lars Olesen <lars@intraface.dk>
2008-12-28 08:21:41 +0000

    Added stuff to make sure, that the confirmation email is written with language.

--- Intraface/XMLRPC/OnlinePayment/Server0002.php
+++ Intraface/XMLRPC/OnlinePayment/Server0002.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * OnlinePayment Server version 0.2.0
- * Class named with version XXYY from version numbering XX.YY.ZZ 
+ * Class named with version XXYY from version numbering XX.YY.ZZ
  *
  * @category XMLRPC_Server
  * @package  Intraface_XMLRPC_OnlinePayment
@@ -21,15 +21,15 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
     public function getPaymentTarget($credentials, $identifier_key)
     {
         $this->checkCredentials($credentials);
-        
+
         $identifier_key = $this->processRequestData($identifier_key);
-        
+
         if (trim($identifier_key) == '') {
             throw new XML_RPC2_FaultException('No valid identifier key was given', -4);
         }
-        
+
         $debtor = $this->debtorFactory($identifier_key);
-        
+
         if ($debtor->get('id') == 0) {
             throw new XML_RPC2_FaultException('No valid debtor was found from the identifier key', -4);
         }
@@ -38,16 +38,16 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
         $onlinepayment->getDBQuery()->setFilter('belong_to', $debtor->get("type"));
         $onlinepayment->getDBQuery()->setFilter('belong_to_id', $debtor->get('id'));
         $onlinepayment->getDBQuery()->setFilter('status', 2);
-            
-        $parameter['payment_online'] = 0;    
+
+        $parameter['payment_online'] = 0;
         foreach($onlinepayment->getlist() AS $p) {
             $parameter['payment_online'] += $p["amount"];
         }
-        
+
         $total_price['DKK'] = $debtor->getTotal()->getAsIso(2);
         $arrears['DKK'] = $debtor->getArrears()->getAsIso(2);
         $default_currency = 'DKK';
-        
+
         if($this->kernel->intranet->hasModuleAccess('currency') && false != ($currency = $debtor->getCurrency())) {
             $default_currency = $currency->getType()->getIsoCode();
             $total_price[$default_currency] = $debtor->getTotalInCurrency()->getAsIso(2);
@@ -81,7 +81,7 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
      * @param string $currency Currrency
      * @param string $text A short description to the payment
      * @param integer $id Id on payment if wanted to update existing payment
-     * 
+     *
      * @return integer $payment_id
      */
     public function saveOnlinePayment($credentials, $identifier_key, $transaction_number, $transaction_status, $pbs_status, $amount, $currency, $text = '', $id = 0)
@@ -93,13 +93,13 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
         if($onlinepayment->get('id') != $id) {
             throw new XML_RPC2_FaultException('The given payment id '.$id.' is not valid', -4);
         }
-        
+
         $identifier_key = $this->processRequestData($identifier_key);
         $debtor = $this->debtorFactory($identifier_key);
         if ($debtor->get('id') == 0) {
             throw new XML_RPC2_FaultException('No valid debtor was found from the identifier key when trying to save onlinepayment', -4);
         }
-        
+
         // currency
         if ($currency != 'DKK' && $this->kernel->intranet->hasModuleAccess('currency')) {
             $this->kernel->useModule('currency', true); /* true: ignore user access */
@@ -109,7 +109,7 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
                 $values['currency'] = $currency;
             }
         }
-        
+
         $values['belong_to'] = $debtor->get('type');
         $values['belong_to_id'] = $debtor->get('id');
         $values['transaction_number'] = $this->processRequestData($transaction_number);
@@ -117,7 +117,7 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
         $values['amount'] = number_format($this->processRequestData($amount), 2, ',', '');
         $values['text'] = $this->processRequestData($text);
         $values['pbs_status'] = $this->processRequestData($pbs_status);
-        
+
         if (!$payment_id = $onlinepayment->save($values)) {
             // this is probably a little to hard reaction.
             throw new XML_RPC2_FaultException('Onlinebetaling kunne ikke blive gemt ' . strtolower(implode(', ', $onlinepayment->error->getMessage())), -4);
@@ -135,15 +135,31 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
         if ($mailer === null) {
             $mailer = Intraface_Mail::factory();
         }
-        
+
         $this->kernel->useShared('email');
         $email = new Email($this->kernel);
 
-        $subject = 'Payment confirmation / betalingsbekræftelse (#' . $payment_id . ')';
-        $body    = 'We have received your payment for order #' .$debtor->getId(). '.' . "\n\n";
-        $body   .= 'Vi har modtaget din betaling for ordre #' .$debtor->getId(). '.';
-        $body   .= "\n\nYours sincerely / Venlig hilsen\n".  $this->kernel->intranet->address->get('name');    
-        
+        $shop = Doctrine::getTable('Intraface_modules_shop_Shop')->findById($debtor->getFromShopId());
+
+        $settings = Doctrine::getTable('Intraface_modules_onlinepayment_Language')->findByIntranetId($kernel->intranet->getId());
+        if (!empty($settings[0])) {
+            $settings = $settings[0];
+        }
+
+        $subject = $settings->getConfirmationEmailSubject($shop->getLanguage());
+        $body = $settings->getConfirmationEmailBody($shop->getLanguage());
+
+        if (empty($subject)) {
+            $subject = 'Payment confirmation / betalingsbekræftelse (#' . $payment_id . ')';
+        }
+
+        if (empty($body)) {
+            $body    = 'We have received your payment for order #' .$debtor->getId(). '.' . "\n\n";
+            $body   .= 'Vi har modtaget din betaling for ordre #' .$debtor->getId(). '.';
+            $body   .= "\n\nYours sincerely / Venlig hilsen\n".  $this->kernel->intranet->address->get('name');
+        }
+
+
         $data = array('contact_id' => $debtor->getContact()->getId(),
                       'subject'    => $subject,
                       'body'       => $body,
@@ -151,7 +167,7 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
                       'from_name'  => $this->kernel->intranet->address->get('name'),
                       'type_id'    => 13, // onlinepayment
                       'belong_to'  => $payment_id);
-        
+
         if (!$email->save($data)) {
             trigger_error('Could not save email to onlinepayment', E_USER_NOTICE);;
             return false;
@@ -187,7 +203,7 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
 
         return $this->prepareResponseData($payment_id);
     }
-    
+
     /**
      * Initialize the webshop
      *
@@ -195,16 +211,16 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
      */
     private function onlinePaymentFactory($id = 0)
     {
-        
+
         if (!$this->kernel->intranet->hasModuleAccess('onlinepayment')) {
             throw new XML_RPC2_FaultException('The intranet did not have access to OnlinePayment', -4);
         }
-        
+
         /**
          * This is needed to load the modules settings.
          */
         $this->kernel->useModule('onlinepayment');
-        
+
         require_once 'Intraface/modules/onlinepayment/OnlinePayment.php';
         if (!empty($id)) {
             return OnlinePayment::factory($this->kernel, 'id', intval($id));
@@ -212,7 +228,7 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
             return OnlinePayment::factory($this->kernel);
         }
     }
-    
+
     /**
      * Initialize Debtor
      *
@@ -226,6 +242,6 @@ class Intraface_XMLRPC_OnlinePayment_Server0002 extends Intraface_XMLRPC_Server
         }
 
         require_once 'Intraface/modules/debtor/Debtor.php';
-        return Debtor::factory($this->kernel, $identifier_key); 
+        return Debtor::factory($this->kernel, $identifier_key);
     }
 }
