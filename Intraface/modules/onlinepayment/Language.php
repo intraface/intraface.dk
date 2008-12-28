22ab1deb8847c8b8903286f27a7e1547153f64f5
Lars Olesen <lars@intraface.dk>
2008-12-28 08:20:27 +0000

    Added language for confirmation text.

---
+++ Intraface/modules/onlinepayment/Language.php
@@ -0,0 +1,27 @@
+<?php
+class Intraface_modules_onlinepayment_Language extends Doctrine_Record
+{
+    public function setTableDefinition()
+    {
+        $this->setTableName('onlinepayment_settings');
+        $this->hasColumn('intranet_id', 'integer', 11, array('greaterthan' => 0));
+        $this->hasColumn('email', 'string',  65555);
+        $this->hasColumn('subject', 'string',  255);
+    }
+
+    public function setUp()
+    {
+        $this->actAs('Intraface_Doctrine_Template_Intranet');
+        $this->actAs('I18n', array('fields' => array('email', 'subject')));
+    }
+
+    function getConfirmationEmailSubject($language)
+    {
+    	return $this->Translation[$language]->subject;
+    }
+
+    function getConfirmationEmailBody($language)
+    {
+    	return $this->Translation[$language]->email;
+    }
+}
--- intraface.dk/modules/onlinepayment/settings.php
+++ intraface.dk/modules/onlinepayment/settings.php
@@ -1,32 +1,32 @@
 <?php
-/**
- * QuickPay:
- * md5_secret
- * merchant_id
- */
-require('../../include_first.php');
+require '../../include_first.php';
 
 $onlinepayment_module = $kernel->module('onlinepayment');
 $implemented_providers = $onlinepayment_module->getSetting('implemented_providers');
 
 $onlinepayment = OnlinePayment::factory($kernel);
-
+$language = new Intraface_modules_language_Languages;
+$settings = Doctrine::getTable('Intraface_modules_onlinepayment_Language')->findByIntranetId($kernel->intranet->getId());
+$settings = $settings[0];
 
 if (!empty($_POST)) {
+
+    $settings->Translation['da']->email = $_POST['email']['da'];
+    $settings->Translation['da']->subject = $_POST['subject']['da'];
+    foreach ($language->getChosenAsArray() as $lang) {
+        $settings->Translation[$lang->getIsoCode()]->email = $_POST['email'][$lang->getIsoCode()];
+        $settings->Translation[$lang->getIsoCode()]->subject = $_POST['subject'][$lang->getIsoCode()];
+    }
+    //$settings->save();
 
 	if ($onlinepayment->setSettings($_POST)) {
 		header('Location: index.php');
 		exit;
-	}
-	else {
+	} else {
 		$value = $_POST;
 	}
-
-}
-else {
-
+} else {
 	$value = $onlinepayment->getSettings();
-
 }
 
 $page = new Intraface_Page($kernel);
@@ -42,9 +42,9 @@ $page->start('Onlinebetalinger');
 		<p>Du har valgt <strong><?php e($implemented_providers[$kernel->setting->get('intranet', 'onlinepayment.provider_key')]); ?></strong> som din udbyder. <a href="choose_provider.php">Vælg en anden udbyder</a>.</p>
 	</fieldset>
 
-	<?php 
+	<?php
 	switch($implemented_providers[$kernel->setting->get('intranet', 'onlinepayment.provider_key')]):
-		case 'quickpay': 
+		case 'quickpay':
 			?>
 			<fieldset>
 				<legend>Indstillinger</legend>
@@ -52,15 +52,15 @@ $page->start('Onlinebetalinger');
 					<label for="merchant_id">Merchant id</label>
 					<input type="text" name="merchant_id" id="merchant_id" value="<?php e($value['merchant_id']); ?>" />
 				</div>
-		
+
 				<div class="formrow">
 					<label for="md5_secret">MD5 Secret</label>
 					<input type="text" name="md5_secret" id="md5_secret" value="<?php e($value['md5_secret']); ?>" />
 				</div>
 			</fieldset>
-			<?php 
-			break; 
-		case 'dandomain':	
+			<?php
+			break;
+		case 'dandomain':
 			?>
 			<fieldset>
 				<legend>Indstillinger</legend>
@@ -68,7 +68,7 @@ $page->start('Onlinebetalinger');
 					<label for="merchant_id">Merchant id</label>
 					<input type="text" name="merchant_id" id="merchant_id" value="<?php e($value['merchant_id']); ?>" />
 				</div>
-		
+
 				<div class="formrow">
 					<label for="password">Password</label>
 					<input type="text" name="password" id="password" value="<?php e($value['password']); ?>" />
@@ -77,13 +77,37 @@ $page->start('Onlinebetalinger');
 			<?php
 			break;
 		case 'default':
-			echo '<p>Her behøver du ikke sætte nogen indstillinger.</p>';
+			echo '<p>Her behøver du ikke sætte nogen særlige indstillinger for denne provider.</p>';
 			break;
 		default:
 			trigger_error('Ugyldig provider');
 			break;
 	endswitch;
 	?>
+    <fieldset>
+        <legend>Tekst på e-mail på dansk</legend>
+            <label for="language_da_subject">Subject</label><br />
+            <input type="text" id="language_da_subject" name="subject[da]" value="<?php e($settings->Translation['da']->subject); ?>" />
+            <br>
+
+            <label for="language_da">Body</label><br />
+            <textarea cols="80" id="language_da" name="email[da]"><?php e($settings->Translation['da']->email); ?></textarea>
+            <br>
+        </fieldset>
+        <?php foreach ($language->getChosenAsArray() as $lang): ?>
+            <fieldset>
+        <legend>Tekst på e-mail på <?php e($lang->getDescription()); ?></legend>
+            <label for="language_da_subject">Subject</label><br />
+            <input type="text" id="language_<?php e($lang->getIsoCode()); ?>_subject" name="subject[<?php e($lang->getIsoCode()); ?>]" value="<?php e($settings->Translation[$lang->getIsoCode()]->subject); ?>" />
+            <br>
+
+            <label for="language_<?php e($lang->getIsoCode()); ?>">Subject</label><br />
+            <textarea cols="80" id="language_<?php e($lang->getIsoCode()); ?>" name="email[<?php e($lang->getIsoCode()); ?>]"><?php e($settings->Translation[$lang->getIsoCode()]->email); ?></textarea>
+            <br>
+          </fieldset>
+        <?php endforeach; ?>
+
+
 
 	<div>
 		<input type="submit" value="Gem" />
@@ -92,7 +116,5 @@ $page->start('Onlinebetalinger');
 <form>
 
 <?php
-
 $page->end();
-
 ?>
