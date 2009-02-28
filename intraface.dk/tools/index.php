1e3f0b2705db0500e9de878dfb25fba3fe1bdd72
Sune T. Jensen <sj@sunet.dk>
2009-02-28 09:28:58 +0000

    Added Tools section. Still needs some work.

---
+++ intraface.dk/tools/index.php
@@ -0,0 +1,82 @@
+<?php
+require '../config.local.php';
+
+set_include_path(PATH_INCLUDE_PATH);
+define('DB_DSN', 'mysql://' . DB_USER . ':' . DB_PASS . '@' . DB_HOST . '/' . DB_NAME);
+
+require 'Ilib/ClassLoader.php';
+require 'k.php';
+
+$application = new Intraface_Tools_Controller_Root();
+
+$application->registry->registerConstructor('errorlist', create_function(
+  '$className, $args, $registry',
+  'return new Intraface_Tools_ErrorList(ERROR_LOG);'
+));
+
+$application->registry->registerConstructor('database', create_function(
+  '$className, $args, $registry',
+  'return MDB2::singleton(DB_DSN);'
+));
+
+$application->registry->registerConstructor('db_sql', create_function(
+  '$className, $args, $registry',
+  'return new DB_Sql;'
+));
+
+$application->registry->registerConstructor('user', create_function(
+  '$className, $args, $registry',
+  'return new Intraface_Tools_User($registry->SESSION);'
+));
+
+$application->registry->registerConstructor('translation_admin', create_function(
+  '$className, $args, $registry',
+  '
+    $driver = "mdb2";
+    $options = array(
+        "hostspec" => DB_HOST,
+        "database" => DB_NAME,
+        "phptype" => "mysql",
+        "username" => DB_USER,
+        "password" => DB_PASS
+    );
+    $params = array(
+        "langs_avail_table" => "core_translation_langs",
+        "strings_default_table" => "core_translation_i18n"
+    );
+    
+    $translation = Translation2_Admin::factory($driver, $options, $params);
+    if (PEAR::isError($translation)) {
+        exit($translation->getMessage());
+    }
+    return $translation;
+ '
+));
+
+$application->registry->registerConstructor('translation', create_function(
+  '$className, $args, $registry',
+  '
+    $driver = "mdb2";
+    $options = array(
+        "hostspec" => DB_HOST,
+        "database" => DB_NAME,
+        "phptype" => "mysql",
+        "username" => DB_USER,
+        "password" => DB_PASS
+    );
+    $params = array(
+        "langs_avail_table" => "core_translation_langs",
+        "strings_default_table" => "core_translation_i18n"
+    );
+    
+    $translation = Translation2::factory($driver, $options, $params);
+    if (PEAR::isError($translation)) {
+        exit($translation->getMessage());
+    }
+    $translation->setLang("dk");
+    return $translation;
+ '
+));
+
+
+$application->dispatch();
