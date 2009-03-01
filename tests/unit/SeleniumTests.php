02249d806954f712fa4dc49db289f5f039e57392
Lars Olesen <lars@intraface.dk>
2009-03-01 09:05:02 +0000

    Now the selenium tests are also run and results are collected when doing a regular build. With cruisecontrol that would make our test suite more complete.

--- tests/unit/SeleniumTests.php
+++ tests/unit/SeleniumTests.php
@@ -4,11 +4,17 @@ require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 class SeleniumTests extends PHPUnit_Extensions_SeleniumTestCase
 {
     public static $seleneseDirectory = '../selenium/';
-    protected $coverageScriptUrl = 'http://workspace/phpunit_coverage.php';
+    //protected $coverageScriptUrl = 'http://workspace/phpunit_coverage.php';
 
     protected function setUp()
     {
         $this->setBrowser('*firefox');
         $this->setBrowserUrl('http://workspace/intraface/intraface.dk/');
+    }
+
+    function assertConfirmation()
+    {
+    	$this->assertConfirmationPresent();
+        $this->click();
     }
 }
