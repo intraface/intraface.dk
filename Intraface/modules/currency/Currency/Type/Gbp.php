109319a9cb29cb5f35a13ca6da2e3dd947f454cb
Sune T. Jensen <sj@sunet.dk>
2009-06-16 08:33:49 +0000

    Added GBP

---
+++ Intraface/modules/currency/Currency/Type/Gbp.php
@@ -0,0 +1,23 @@
+<?php
+class Intraface_modules_currency_Currency_Type_Gbp
+{
+    function getKey()
+    {
+        return 3;
+    }
+
+    function getIsoCode()
+    {
+        return 'GBP';
+    }
+
+    function getDescription()
+    {
+        return 'Great British Pounds';
+    }
+
+    function getHtmlSymbol()
+    {
+        return '&#163;';
+    }
+}
