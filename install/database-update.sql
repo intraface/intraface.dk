e23400de5c6bc8a7963b8b2869c43e98792d080b
Sune T. Jensen <sj@sunet.dk>
2009-06-16 10:59:40 +0000

    Changed delete_at fields to null

--- install/database-update.sql
+++ install/database-update.sql
@@ -1,19 +1,19 @@
 
 ALTER TABLE `module_package_action` CHANGE `order_debtor_id` `order_debtor_identifier` VARCHAR( 255 ) NOT NULL DEFAULT '0';
 
-ALTER TABLE `currency` ADD `deleted_at` TIMESTAMP NOT NULL DEFAULT 0;
+ALTER TABLE `currency` ADD `deleted_at` TIMESTAMP NULL DEFAULT NULL;
 UPDATE `currency` SET deleted_at = NOW() WHERE deleted = 1;
 ALTER TABLE `currency` CHANGE `deleted` `_old_deleted` INT( 1 ) NOT NULL ;
 
-ALTER TABLE `product_attribute` ADD `deleted_at` TIMESTAMP NOT NULL DEFAULT 0;
+ALTER TABLE `product_attribute` ADD `deleted_at` TIMESTAMP NULL DEFAULT NULL;
 UPDATE `product_attribute` SET deleted_at = NOW() WHERE deleted = 1;
 ALTER TABLE `product_attribute` CHANGE `deleted` `_old_deleted` TINYINT( 1 ) NOT NULL DEFAULT '0';
 
-ALTER TABLE `product_attribute_group` ADD `deleted_at` TIMESTAMP NOT NULL DEFAULT 0;
+ALTER TABLE `product_attribute_group` ADD `deleted_at` TIMESTAMP NULL DEFAULT NULL;
 UPDATE `product_attribute_group` SET deleted_at = NOW() WHERE deleted = 1;
 ALTER TABLE `product_attribute_group` CHANGE `deleted` `_old_deleted` TINYINT( 1 ) NOT NULL DEFAULT '0';
 
-ALTER TABLE `product_variation` ADD `deleted_at` TIMESTAMP NOT NULL DEFAULT 0;
+ALTER TABLE `product_variation` ADD `deleted_at` TIMESTAMP NULL DEFAULT NULL;
 UPDATE `product_variation` SET deleted_at = NOW() WHERE deleted = 1;
 ALTER TABLE `product_variation` CHANGE `deleted` `_old_deleted` TINYINT( 1 ) NOT NULL DEFAULT '0';
 
