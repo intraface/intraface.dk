0c0cf1416ce659f6f88c49f604593bd5afb9252b
Lars Olesen <lars@intraface.dk>
2009-06-18 17:23:02 +0000

    Added test for language.

---
+++ tests/unit/Language/Test.php
@@ -0,0 +1,36 @@
+<?php
+require_once dirname(__FILE__) . '/../config.test.php';
+
+class Language_Test extends PHPUnit_Framework_TestCase
+{
+    function testLanguageSave()
+    {
+        $intranet_id = 1;
+        Intraface_Doctrine_Intranet::singleton($intranet_id);
+        $settings = new Intraface_modules_onlinepayment_Language;
+        $settings->Translation['da']->email = 'danish email';
+        $settings->Translation['da']->subject = 'danish subject';
+        $settings->Translation['en']->subject = 'english subject';
+        $settings->Translation['en']->email = 'english email';
+        $settings->save();
+
+        $settings = Doctrine::getTable('Intraface_modules_onlinepayment_Language')->findOneByIntranetId($intranet_id);
+        $settings->Translation['da']->email = 'danish email 2';
+        $settings->Translation['da']->subject = 'danish subject 2';
+        $settings->Translation['en']->subject = 'english subject 2';
+        $settings->Translation['en']->email = 'english email 2';
+        $settings->save();
+
+        $language = 'da';
+
+        $this->assertEquals('danish email 2', $settings->getConfirmationEmailBody($language));
+        $this->assertEquals('danish subject 2', $settings->getConfirmationEmailSubject($language));
+
+        $language = 'en';
+
+        $this->assertEquals('english email 2', $settings->getConfirmationEmailBody($language));
+        $this->assertEquals('english subject 2', $settings->getConfirmationEmailSubject($language));
+
+    }
+
+}
