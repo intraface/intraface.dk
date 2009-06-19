6912ca5dd7536913dc83409d11723b0207797820
Lars Olesen <lars@intraface.dk>
2009-06-19 05:38:47 +0000

    Coding standards.

--- intraface.dk/modules/filemanager/_old_export.php
+++ intraface.dk/modules/filemanager/_old_export.php
@@ -13,7 +13,7 @@ $db = new DB_Sql;
 $i = 0;
 
 $db->query("SELECT * FROM file_handler WHERE intranet_id = ".$kernel->intranet->get('id')." AND active = 1 ORDER BY file_name");
-while($db->nextRecord()) {
+while ($db->nextRecord()) {
 	$i++;
 	print("INSERT INTO images SET file_name = \"".$db->f('file_name')."\", access_key = \"".$db->f('access_key')."\", placement = ".$i.";\n");
 }
