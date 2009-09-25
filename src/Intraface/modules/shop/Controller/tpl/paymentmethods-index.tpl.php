52d6c25b3e613746117b561df8a210ebe260ce5c
Lars Olesen <lars@intraface.dk>
2009-09-25 13:06:30 +0000

    Work on issue #0000797

--- src/Intraface/modules/shop/Controller/tpl/paymentmethods-index.tpl.php
+++ src/Intraface/modules/shop/Controller/tpl/paymentmethods-index.tpl.php
@@ -1,12 +1,23 @@
+<p><?php e(__('Choose payment method. If you choose more than one payment method, your customers will be able to choose between them when using your online shop.')); ?></p>
+
 <form action="<?php e(url('./')); ?>" method="post">
 <table>
+<thead>
+	<tr>
+		<th></th>
+		<th><?php e(__('Payment method')); ?></th>
+		<th><?php e(__('Alternative text')); ?></th>
+	</tr>
+</thead>
+<tbody>
 <?php foreach ($methods as $key => $method): ?>
     <tr>
         <td><input id="payment-method-<?php e($key); ?>" type="checkbox" value="<?php e($key); ?>" name="method[<?php e($key); ?>]" <?php if (isset($chosen[$key])) echo ' checked="checked"'; ?>/></td>
         <td><label for="payment-method-<?php e($key); ?>"><?php e($method->getDescription()); ?></label></td>
-        <td><input type="text" value="<?php if (isset($chosen[$key])) e($chosen[$key]['text']); ?>" name="text[<?php e($key); ?>]" /></td>        
+        <td><input type="text" value="<?php if (isset($chosen[$key])) e($chosen[$key]['text']); ?>" name="text[<?php e($key); ?>]" /></td>
     </tr>
 <?php endforeach; ?>
+</tbody>
 </table>
 <p><input type="submit" value="<?php e(__('Save')); ?>" /></p>
 </form>
