6653e2461d24bc66ae6586d739b41c65ba714d28
Sune T. Jensen <sj@sunet.dk>
2009-10-09 20:30:11 +0000

    Corrected test

--- tests/unit/Product/VariationDetailTest.php
+++ tests/unit/Product/VariationDetailTest.php
@@ -3,6 +3,24 @@ require_once dirname(__FILE__) . '/../config.test.php';
 
 Intraface_Doctrine_Intranet::singleton(1);
 
+class FakeVariationDetailTestProduct
+{
+    function getAttributeGroups()
+    {
+        return array('some data');
+    }
+    
+    function hasVariation()
+    {
+        return 1;
+    }
+    
+    function getId()
+    {
+        return 1;
+    }
+}
+
 class VariationDetailTest extends PHPUnit_Framework_TestCase
 {
 
@@ -13,6 +31,8 @@ class VariationDetailTest extends PHPUnit_Framework_TestCase
         $db = MDB2::factory(DB_DSN);
         $db->query('TRUNCATE product_variation');
         $db->query('TRUNCATE product_variation_detail');
+        $db->query('TRUNCATE product_attribute');
+        $db->query('TRUNCATE product_attribute_group');
         
         
     }
@@ -20,6 +40,11 @@ class VariationDetailTest extends PHPUnit_Framework_TestCase
     function createVariation() 
     {
         if ($this->object === NULL) {
+            $group = new Intraface_modules_product_Attribute_Group;
+            $group->name = 'color';
+            $group->attribute[0]->name = 'red';
+            $group->save();
+            
             $object = new Intraface_modules_product_Variation_OneAttributeGroup;
             $object->product_id = 1;
             $object->setAttributesFromArray(array('attribute1' => 1));
@@ -27,7 +52,10 @@ class VariationDetailTest extends PHPUnit_Framework_TestCase
             return $this->object = $object;
         }
         
-        return Doctrine::getTable('Intraface_modules_product_Variation_OneAttributeGroup')->find(1);
+        $gateway = new Intraface_modules_product_Variation_Gateway(new FakeVariationDetailTestProduct);
+        return $gateway->findById(1);
+        
+        // return Doctrine::getTable('Intraface_modules_product_Variation_OneAttributeGroup')->find(1);
     }
     
     ///////////////////////////////////////////////////////
@@ -96,7 +124,7 @@ class VariationDetailTest extends PHPUnit_Framework_TestCase
     
     function testSaveDetailSavesNewOnNewDetails()
     {
-        $object = $object = $this->createVariation();
+        $object = $this->createVariation();
         $detail = $object->getDetail();
         $detail->price_difference = 10;
         $detail->weight_difference = 10;
@@ -110,6 +138,8 @@ class VariationDetailTest extends PHPUnit_Framework_TestCase
         $detail->save();
         
         $object = $object = $this->createVariation();
+        // $object->refresh(true);
+        
         $detail = $object->getDetail();
         $this->assertEquals(2, $detail->getId());
         $this->assertEquals(20, $detail->getPriceDifference());
