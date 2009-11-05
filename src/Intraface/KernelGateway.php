faa8dd3250a279967a4a9349eedc910e1551e304
Lars Olesen <lars@intraface.dk>
2009-11-05 18:16:10 +0000

    Added gateways for kernel and user usable by k2.

---
+++ src/Intraface/KernelGateway.php
@@ -0,0 +1,28 @@
+<?php
+class Intraface_KernelGateway
+{
+    protected $translation;
+
+    function __construct(Translation2 $translation)
+    {
+        $this->translation = $translation;
+    }
+
+    function findByUserobject($user)
+    {
+        $kernel = new Intraface_Kernel(session_id());
+        $kernel->user = $user;
+        if (!$intranet_id = $kernel->user->getActiveIntranetId()) {
+            throw new Exception('no active intranet_id');
+        }
+
+        $kernel->intranet = new Intraface_Intranet($intranet_id);
+        $kernel->translation = $this->translation;
+        $kernel->translation->setLang($kernel->user->getLanguage());
+
+        // @todo why are we setting the id?
+        $kernel->user->setIntranetId($kernel->intranet->get('id'));
+        $kernel->setting = new Intraface_Setting($kernel->intranet->get('id'), $kernel->user->get('id'));
+        return $kernel;
+    }
+}
---
+++ src/Intraface/UserGateway.php
@@ -0,0 +1,30 @@
+<?php
+ini_set('display_errors', true);
+class Intraface_UserGateway
+{
+    protected $db;
+
+    function __construct(DB_Sql $db)
+    {
+        $this->db = $db;
+    }
+
+    function findByUsername($id)
+    {
+        $result = $this->db->query('select id from user where email = "'.$id.'"');
+        $this->db->nextRecord();
+        return new Intraface_User($this->db->f('id'));
+    }
+
+
+    function findById($id)
+    {
+        return new Intraface_User($id);
+    }
+
+    function getAll()
+    {
+        $user = new Intraface_User();
+        return $user->getAll();
+    }
+}
