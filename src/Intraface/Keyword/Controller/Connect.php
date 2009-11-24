4a083af151975f9c4538a97ec5323e1813493e12
Lars Olesen <lars@intraface.dk>
2009-11-24 12:08:42 +0000

    Reverted to the old way to handle keywords on.

--- src/Intraface/Keyword/Controller/Connect.php
+++ src/Intraface/Keyword/Controller/Connect.php
@@ -1,12 +1,9 @@
 <?php
 class Intraface_Keyword_Controller_Connect extends k_Component
 {
-    public function getObject()
+    function getKernel()
     {
-        if (!method_exists($this->context, 'getObject')) {
-        	throw new Exception('The context has to implement getObject()');
-        }
-    	return $this->context->getObject();
+        return $this->context->getKernel();
     }
 
     function t($phrase)
@@ -14,72 +11,64 @@ class Intraface_Keyword_Controller_Connect extends k_Component
         return $phrase;
     }
 
-    function __($phrase)
-    {
-        return $phrase;
-    }
-
     function renderHtml()
     {
-        $kernel = $this->context->getKernel();
-        $kernel->useShared('keyword');
-        $translation = $kernel->getTranslation('keyword');
+        $this->getKernel()->useShared('keyword');
 
-        $object = $this->getObject();
-
-        if (!empty($this->GET['delete']) AND is_numeric($this->GET['delete'])) {
-            $keyword = new Ilib_Keyword($object, $this->GET['delete']);
+        if (!empty($_GET['delete']) AND is_numeric($_GET['delete'])) {
+            $keyword = new Keyword($this->context->getObject(), $_GET['delete']);
             $keyword->delete();
         }
 
-        $keyword = $object->getKeyword();
-        $appender = $object->getKeywordAppender(); // starter objektet
+        $keyword = $this->context->getObject()->getKeywordAppender(); // starter objektet
         $keywords = $keyword->getAllKeywords(); // henter alle keywords
-        $keyword_string = $appender->getConnectedKeywordsAsString();
+        $keyword_string = $keyword->getConnectedKeywordsAsString();
 
         // finder dem der er valgt
         $checked = array();
-        foreach($appender->getConnectedKeywords() as $key) {
+        foreach ($keyword->getConnectedKeywords() AS $key) {
             $checked[] = $key['id'];
         }
+        $data = array(
+        	'object' => $this->context->getObject(),
+        	'keyword' => $keyword,
+        	'keywords' => $keywords,
+        	'checked' => $checked);
 
-        $data = array('object' => $object, 'keyword' => $keyword, 'keywords' => $keywords, 'checked' => $checked);
         $smarty = new k_Template(dirname(__FILE__) . '/../templates/connect.tpl.php');
         return $smarty->render($this, $data);
     }
 
-    function POST()
+    function postForm()
     {
-        $kernel = $this->context->getKernel();
-        $kernel->useShared('keyword');
-        $translation = $kernel->getTranslation('keyword');
+        $this->getKernel()->useShared('keyword');
+        $appender = $this->context->getObject()->getKeywordAppender(); // starter keyword objektet
 
-        $object = $this->getObject();
-
-        $keyword = $object->getKeywordAppender(); // starter keyword objektet
-
-        if (!$keyword->deleteConnectedKeywords()) {
-            $keyword->error->set('Kunne ikke slette keywords.');
+        if (!$appender->deleteConnectedKeywords()) {
+            $appender->error->set('Kunne ikke slette keywords.');
         }
 
         // strengen med keywords
         if (!empty($_POST['keywords'])) {
-            $appender = new Ilib_Keyword_StringAppender(new Ilib_Keyword($object), $keyword);
-            $appender->addKeywordsByString($_POST['keywords']);
+            $string_appender = new Intraface_Keyword_StringAppender($this->context->getObject()->getKeyword(), $appender);
+            $string_appender->addKeywordsByString($_POST['keywords']);
         }
 
         // listen med keywords
-        if (!empty($_POST['keywords']) AND is_array($_POST['keywords']) AND count($_POST['keywords']) > 0) {
-            for($i=0, $max = count($_POST['keywords']); $i < $max; $i++) {
-                $keyword->addKeyword(new Ilib_Keyword($object, $this->POST['keyword'][$i]));
+        if (!empty($_POST['keyword']) AND is_array($_POST['keyword']) AND count($_POST['keyword']) > 0) {
+            for ($i=0, $max = count($_POST['keyword']); $i < $max; $i++) {
+                $appender->addKeyword(new Keyword($this->context->getObject(), $_POST['keyword'][$i]));
             }
         }
 
         if (!empty($_POST['close'])) {
             return new k_SeeOther($this->url('../../'));
-        } else {
-            return new k_SeeOther($this->url('./'));
         }
+          if (!$appender->error->isError()) {
+            //header('Location: connect.php?'.$id_name.'='.$this->context->getObject()->get('id'));
+            //exit;
+        }
+
         return $this->render();
     }
 }
--- src/Intraface/Keyword/templates/connect.tpl.php
+++ src/Intraface/Keyword/templates/connect.tpl.php
@@ -1,33 +1,37 @@
-<h1><?php e(__('Add keywords to') . ' ' . $object->get('name')); ?></h1>
+<h1><?php e(t('add keywords to') . ' ' . $object->get('name')); ?></h1>
 
 <?php echo $keyword->error->view(); ?>
 
-<form action="<?php e(url('./')); ?>" method="post">
+<form action="<?php e(url()); ?>" method="post">
     <?php if (is_array($keywords) AND count($keywords) > 0): ?>
     <fieldset>
-        <legend><?php e(__('choose keywords')); ?></legend>
+        <legend><?php e(t('choose keywords')); ?></legend>
+        <input type="hidden" name="<?php e($id_name); ?>" value="<?php e($object->get('id')); ?>" />
         <?php
             $i = 0;
-            foreach ($keywords as $k) {
-                print '<input type="checkbox" name="keyword[]" id="k'.$k['id'].'" value="'.$k['id'].'"';
+            foreach ($keywords AS $k) { ?>
+                <input type="checkbox" name="keyword[]" id="k<?php e($k['id']); ?>" value="<?php e($k['id']); ?>"
+                <?php
                 if (in_array($k['id'], $checked)) {
                     print ' checked="checked" ';
-                }
-                print ' />';
-                print ' <label for="k'.$k["id"].'">' . htmlentities($k['keyword']) . ' (#'.$k["id"].')</a></label> - <a href="'.$this->url('./', array('delete' => $k["id"])).'" class="confirm">' .__('Delete', 'common'). '</a><br />'. "\n";
-        }
+                } ?>
+                />
+                <label for="k<?php e($k["id"]); ?>"><a href="edit.php?<?php e($id_name); ?>=<?php e($object->get('id')); ?>&amp;id=<?php e($k['id']); ?>"><?php e($k['keyword']); ?> (#<?php e($k["id"]); ?>)</a></label> - <a href="<?php e($_SERVER['PHP_SELF']); ?>?<?php e($id_name); ?>=<?php e($object->get('id')); ?>&amp;delete=<?php e($k["id"]); ?>" class="confirm"><?php e(t('delete', 'common')); ?></a><br />
+        <?php }
         ?>
     </fieldset>
         <div style="clear: both; margin-top: 1em; width:100%;">
-            <input type="submit" value="<?php e(__('Choose')); ?>" name="submit" class="save" /> <input type="submit" value="<?php e(__('Choose and close')); ?>" name="close" class="save" />
+            <input type="submit" value="<?php e(t('choose')); ?>" name="submit" class="save" id="submit-save" />
+            <input type="submit" value="<?php e(t('choose and close')); ?>" name="close" class="save" id="submit-close" />
         </div>
 
     <?php endif; ?>
     <fieldset>
-        <legend><?php e(__('Create keyword')); ?></legend>
-        <p><?php e(__('Separate keywords by comma')); ?></samp></p>
-        <label for="keyword"><?php e(__('Keywords')); ?></label>
-        <input type="text" name="keywords" id="keyword" value="<?php // echo $keyword_string; ?>" />
-        <input type="submit" value="<?php e(__('Save', 'common')); ?>" name="submit" name="close" />
+        <legend><?php e(t('create keyword')); ?></legend>
+        <p><?php e(t('separate keywords by comma')); ?></p>
+        <input type="hidden" name="<?php e($id_name); ?>" value="<?php e($object->get('id')); ?>" />
+        <label for="keyword"><?php e(t('keywords')); ?></label>
+        <input type="text" name="keywords" id="keyword" value="<?php //e($keyword_string); ?>" />
+        <input type="submit" value="<?php e(t('save', 'common')); ?>" name="submit" id="submit-save-new" />
     </fieldset>
 </form>
