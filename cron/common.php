492304613cb824318f137cc9eed95267fb8179a7
Lars Olesen <lars@intraface.dk>
2009-11-27 13:22:40 +0000

    Added comment.

--- cron/common.php
+++ cron/common.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Set environment for the crontabs
+ */
 $config_file = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config.local.php';
 if (!file_exists($config_file)) {
     die('The config.local.php file is missing. Please create it.');
@@ -8,5 +10,3 @@ require_once $config_file;
 
 set_include_path(PATH_INCLUDE_PATH.get_include_path());
 require('Intraface/common.php');
-
-?>
