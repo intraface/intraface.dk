bc81a1b9b31dbba54e480404a4c32615a36dea3f
Lars Olesen <lars@intraface.dk>
2009-11-29 11:05:41 +0000

    Now setting encoding according to INTRAFACE_K2 constant.

--- src/Intraface/ihtml/intraface/top.php
+++ src/Intraface/ihtml/intraface/top.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html xmlns="http://www.w3.org/1999/xhtml" lang="da">
 <head>
 	<title><?php e($title); ?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php if (defined('INTRAFACE_K2')): e('utf-8'); else: e('iso-8859-1'); endif;  ?>" />
 	<style type="text/css">
 		@import "<?php e(url('/css/stylesheet.php', array('theme' => $this->theme_key, 'fontsize' => $this->fontsize))); ?>";
 	</style>
