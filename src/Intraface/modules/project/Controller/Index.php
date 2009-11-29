3134b055e7abe370088944e3c45034b855ceb0c1
Lars Olesen <lars@intraface.dk>
2009-11-29 23:41:00 +0000

    Removed $registry

--- src/Intraface/modules/project/Controller/Index.php
+++ src/Intraface/modules/project/Controller/Index.php
@@ -1,20 +1,19 @@
 <?php
 class Intraface_modules_project_Controller_Index extends k_Component
 {
-    protected $registry;
+    protected $doctrine;
     protected $user_gateway;
     protected $gateway;
 
-    function __construct(k_Registry $registry, Intraface_UserGateway $user_gateway, Intraface_Doctrine_IntranetGateway $gateway)
+    function __construct(Doctrine_Connection_Common $doctrine, Intraface_UserGateway $user_gateway, Intraface_Doctrine_IntranetGateway $gateway)
     {
-        $this->registry = $registry;
         $this->gateway = $gateway;
         $this->user_gateway = $user_gateway;
-        }
+        $this->doctrine = $doctrine;
+    }
 
     function renderHtml()
     {
-        $doctrine = $this->registry->get('doctrine');
         $this->gateway->findByIntranetId($this->user_gateway->findByUsername($this->context->identity()->user())->getActiveIntranetId());
 
         $shops = Doctrine::getTable('Intraface_modules_shop_Shop')->findAll();
