2e0bd40523813bb35139ecdf4bfac9da0601f888
Lars Olesen <lars@intraface.dk>
2009-11-29 11:08:13 +0000

    Bugfixes.

--- src/Intraface/modules/administration/MainAdministration.php
+++ src/Intraface/modules/administration/MainAdministration.php
@@ -29,4 +29,10 @@ class MainAdministration extends Intraface_Main
         $this->addSubMenuItem("Registrér moduler", "register_modules.php");
         */
     }
+
+    function getPath()
+    {
+        return url('/core/restricted/module/administration/') . '/';
+    }
 }
+
--- src/Intraface/modules/administration/include_frontpage.php
+++ src/Intraface/modules/administration/include_frontpage.php
@@ -10,7 +10,7 @@ $intranet = new IntranetAdministration($kernel);
 if (!$intranet->isFilledIn()):
     $_advice[] = array(
         'msg' => 'all information about the intranet has not been filled in',
-        'link' => $administration_module->getPath() . 'intranet_edit.php',
+        'link' => $administration_module->getPath() . 'intranet?edit',
         'module' => $administration_module->getName()
     );
 endif;
@@ -18,7 +18,7 @@ endif;
 if (!$intranet->get('identifier')):
     $_attention_needed[] = array(
         'msg' => 'identifier for the intranet is missing',
-        'link' => $administration_module->getPath() . 'intranet_edit.php',
+        'link' => $administration_module->getPath() . 'intranet?edit',
         'module' => $administration_module->getName()
     );
 endif;
