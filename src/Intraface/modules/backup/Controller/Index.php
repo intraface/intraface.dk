43593a3d49f2a8ed8e65ee3a4f632467b9df8673
Lars Olesen <lars@intraface.dk>
2009-11-29 23:39:08 +0000

    Added backup as k2.

--- src/Intraface/modules/backup/Controller/Index.php
+++ src/Intraface/modules/backup/Controller/Index.php
@@ -1,8 +1,30 @@
 <?php
 class Intraface_modules_backup_Controller_Index extends k_Component
 {
+    protected $template;
+
+    function __construct(k_TemplateFactory $template)
+    {
+        $this->template = $template;
+    }
+
     function renderHtml()
     {
-        return new k_SeeOther($this->url('../../../../modules/backup'));
+        $tpl = $this->template->create(dirname(__FILE__) . '/templates/index');
+        return $tpl->render($this);
+    }
+
+    function postForm()
+    {
+        if (!empty($_POST['mysql'])) {
+            if (!exec('bash /home/intraface/backup/mysql.sh')) {
+                die('no success');
+            }
+        } elseif (!empty($_POST['domain'])) {
+            if (!exec('bash /home/intraface/backup/domain.sh')) {
+                die('no success');
+            }
+        }
+        return $this->render();
     }
 }
---
+++ src/Intraface/modules/backup/Controller/templates/index.tpl.php
@@ -0,0 +1,41 @@
+<h1>Backup af database</h1>
+
+<form action="<?php e(url()); ?>" method="post">
+    <fieldset>
+        <legend>Backup</legend>
+        <p><strong>Backup</strong>. På denne side kan du lave en backup af enten filerne i domænet eller systemets databaser. </p>
+        <input type="submit" name="mysql" value="Database" />
+        <input type="submit" name="domain" value="Filer" />
+    </fieldset>
+</form>
+<?php
+/*
+<table>
+    <caption>Backups</caption>
+    <thead>
+        <tr>
+            <th>Tid</th>
+            <th>Filnavn</th>
+            <th>Download</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+        if (is_dir(BACKUP_PATH) AND ($dh = opendir(BACKUP_PATH))) {
+            while (($filename = readdir($dh)) !== false) {
+                if (!isset($filename)) continue;
+                if ($filename == '.' OR $filename=='..') continue;
+    ?>
+        <tr>
+            <td><?php echo date('d-m-Y H:i:s', filemtime(BACKUP_PATH . '/' . $filename)); ?></td>
+            <td><?php e($filename); ?></td>
+            <td><a href="file.php?file=<?php e($filename); ?>">Download</a></td>
+        </tr>
+    <?php
+            }
+            closedir($dh);
+        }
+    ?>
+    </tbody>
+</table>
+*/?>
