3cda5b849ed582418b75dfa2cb58fd54eeda11e9
Lars Olesen <lars@intraface.dk>
2010-01-03 20:34:11 +0000

    Fixed encoding.

--- src/Intraface/modules/backup/MainBackup.php
+++ src/Intraface/modules/backup/MainBackup.php
@@ -1,31 +1,21 @@
 <?php
 /**
- * Til backup
+ * Backup
  *
  * @package Intraface_Backup
  * @author	Lars Olesen
  * @since	1.0
  * @version	1.0
  */
-class MainBackup extends Intraface_Main 
+class MainBackup extends Intraface_Main
 {
     function __construct()
     {
         $this->module_name = 'backup';
-        $this->menu_label = 'backup'; // Navnet der vil stå i menuen
-        $this->show_menu = 1; // Skal modulet vises i menuen.
-        $this->active = 1; // Er modulet aktivt.
+        $this->menu_label = 'backup'; // menu name
+        $this->show_menu = 1; // show in menu?
+        $this->active = 1; // is module active
         $this->menu_index = 520;
         $this->frontpage_index = 200;
-
-        // Tilføj undermenu punkter.
-        // $this->addSubMenuItem("Underside 1", "underside1.php");
-        // $this->addSubMenuItem("Underside 2", "underside2.php");
-
-        // Tilføj subaccess punkter
-        // opretkunde: et kort navn der er sigende
-        // Opret ny kunde: En beskrivelse af subaccess.
-        //$this->addSubAccessItem("opretkunde", "Opret ny kunde");
-
     }
 }
