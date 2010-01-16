6189879d931c7d431e9a535100592701dadae511
Sune T. Jensen <sj@sunet.dk>
2010-01-16 19:24:27 +0000

    Changed backup server name to pyongyang

--- backup/upload.sh
+++ backup/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 BACKUP_DIR=/home/intrafac/upload/
-TARGET=intraface_upload@olive.dreamhost.com:/home/intraface_upload/solar/upload
+TARGET=intraface_upload@pyongyang.dreamhost.com:/home/intraface_upload/solar/upload
 
 rsync -e ssh -rltv --exclude="tempdir/"  $BACKUP_DIR $TARGET/dayly.0
