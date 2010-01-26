dbe349d8c8a966ee6ee5cfe6eb0d3144bf98d6ef
Lars Olesen <lars@intraface.dk>
2010-01-26 11:19:28 +0000

    Now does only tell that the old stuff does not work anymore :)

--- src/Intraface/modules/webshop/Controller/Index.php
+++ src/Intraface/modules/webshop/Controller/Index.php
@@ -3,6 +3,6 @@ class Intraface_modules_webshop_Controller_Index extends k_Component
 {
     function renderHtml()
     {
-        return new k_SeeOther($this->url('../../../../modules/webshop/'));
+        return '<h1>Webshop</h1><p>Der er ingen frontend til webshop længere. Rettelser skal laves direkte i databasen.</p>';
     }
 }
