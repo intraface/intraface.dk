2f81ab869707265ab9f5d802bc87cde5328232c4
Lars Olesen <lars@intraface.dk>
2010-01-27 10:57:39 +0000

    Fixed issue #962.

--- src/Intraface/Mail.php
+++ src/Intraface/Mail.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Factory for returning the mailer class
- * 
+ *
  * @package Intraface
  * @author  Sune Jensen <sj@sunet.dk>
  * @author  Lars Olesen <lars@legestue.net>
@@ -18,9 +18,9 @@ class Intraface_Mail
     private $attachments = array();
 
     function __construct()
-    {  
+    {
     }
-    
+
     function setSubject($text)
     {
         $this->text = $text;
@@ -30,48 +30,49 @@ class Intraface_Mail
     {
         $this->text_body = $text;
     }
-    
+
     function setHtmlBody($html)
     {
         $this->text_body = $html;
     }
-    
+
     function addRecipient($email)
     {
         $this->recipients[] = $email;
     }
-    
+
     function addCC($email)
     {
         $this->cc[] = $email;
     }
-    
+
     function addBCC($email)
     {
         $this->bcc[] = $email;
     }
-    
+
     function addAttachment($attachment)
     {
         $this->attachments[] = $attachment;
     }
-    
+
     public static function factory()
     {
         require_once 'phpmailer/class.phpmailer.php';
         $phpmailer = new Phpmailer;
-        // opsætning
+        // opsï¿½tning
         $phpmailer->Mailer   = 'mail'; // Alternative to IsSMTP()
         // $phpmailer->isSMTP();//
         // $phpmailer->Host = SMTP_HOST; //smpt.domain.com
         // $phpmailer->Port = 25;//usually 25
         // $phpmailer->SMTPAuth = true;  // Auth Type
         // $phpmailer->Username = SMTP_USERNAME;
-        // $phpmailer->Password = SMTP_PASSWORD;        
-        // $phpmailer->SMTPKeepAlive = false; 
+        // $phpmailer->Password = SMTP_PASSWORD;
+        // $phpmailer->SMTPKeepAlive = false;
         $phpmailer->WordWrap = 75;
         $phpmailer->setLanguage('en');
+        $phpmailer->CharSet = 'UTF-8';
         return $phpmailer;
     }
-    
+
 }
