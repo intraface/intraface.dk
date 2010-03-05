be5e9251149a4a488234c49938b4c5c1605d3f85
Sune T. Jensen <sj@sunet.dk>
2010-03-05 15:56:53 +0000

    Removed definition of intranet_id as it is defined with intranet template

--- src/Intraface/modules/onlinepayment/Language.php
+++ src/Intraface/modules/onlinepayment/Language.php
@@ -4,7 +4,7 @@ class Intraface_modules_onlinepayment_Language extends Doctrine_Record
     public function setTableDefinition()
     {
         $this->setTableName('onlinepayment_settings');
-        $this->hasColumn('intranet_id', 'integer', 11, array('greaterthan' => 0));
+        // $this->hasColumn('intranet_id', 'integer', 11, array('greaterthan' => 0)); // defined in intranet template
         $this->hasColumn('email', 'string',  65555);
         $this->hasColumn('subject', 'string',  255);
     }
