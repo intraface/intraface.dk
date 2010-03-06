5a634d373589fdfd23034b1a1861ff41abecf571
Sune T. Jensen <sj@sunet.dk>
2010-03-06 13:41:50 +0000

    Hided module in menu

--- src/Intraface/modules/fileimport/MainFileimport.php
+++ src/Intraface/modules/fileimport/MainFileimport.php
@@ -7,8 +7,8 @@ class MainFileimport extends Intraface_Main
     function __construct()
     {
         $this->module_name = 'fileimport';
-        $this->menu_label = 'fileimport'; // Navnet der vil st� i menuen
-        $this->show_menu = 1; // Skal modulet vises i menuen.
+        $this->menu_label = 'fileimport'; // Navnet der vil stå i menuen
+        $this->show_menu = 0; // Skal modulet vises i menuen.
         $this->active = 1; // Er modulet aktivt.
         $this->menu_index = 210;
         $this->frontpage_index = 200;
