5a44f5f33ec8d0ee8567903e6e082b051401ca81
Lars Olesen <lars@intraface.dk>
2010-03-07 22:46:17 +0000

    Removed old files.

--- src/intraface.dk/modules/debtor/reminder_pdf.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('../../include_first.php');
-
-$kernel->module("debtor");
-$mainInvoice = $kernel->useModule("invoice");
-
-$reminder = new Reminder($kernel, intval($_GET["id"]));
-$reminder->pdf();
