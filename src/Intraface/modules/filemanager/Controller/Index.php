eeb0c307f888f3ac923df2bbcce60a5ed5ab37fc
Lars Olesen <lars@intraface.dk>
2010-03-07 13:02:00 +0000

    Removed uncommented method.

--- src/Intraface/modules/filemanager/Controller/Index.php
+++ src/Intraface/modules/filemanager/Controller/Index.php
@@ -1,10 +1,4 @@
 <?php
 class Intraface_modules_filemanager_Controller_Index extends Intraface_Filehandler_Controller_Index
 {
-    /*
-    function renderHtml()
-    {
-        return new k_SeeOther($this->url('../../../../modules/filemanager/'));
-    }
-    */
 }
