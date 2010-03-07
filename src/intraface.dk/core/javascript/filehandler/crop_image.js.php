a09f7ba592741fb2ff74634041d5462fed4b8564
Lars Olesen <lars@intraface.dk>
2010-03-07 22:51:57 +0000

    Removed include_first.php and added function e();

--- src/intraface.dk/core/javascript/filehandler/crop_image.js.php
+++ src/intraface.dk/core/javascript/filehandler/crop_image.js.php
@@ -1,8 +1,11 @@
 <?php
-require('../../../include_first.php');
+function e($phrase)
+{
+    echo htmlentities($phrase);
+}
 ?>
 Event.observe( window, 'load', function() {
-        
+
     document.getElementById('width').readOnly = true;
     document.getElementById('width').style.backgroundColor = '#EEEEEE';
 	document.getElementById('height').readOnly = true;
@@ -11,13 +14,10 @@ Event.observe( window, 'load', function() {
 	document.getElementById('x').style.backgroundColor = '#EEEEEE';
 	document.getElementById('y').readOnly = true;
 	document.getElementById('y').style.backgroundColor = '#EEEEEE';
-        
-        
+
     new Cropper.Img(
         'image',
-        { 
-            
-            
+        {
             minWidth: <?php if (isset($_GET['max_width'])): e($_GET['max_width']); else: echo 0; endif; ?>,
             minHeight: <?php if (isset($_GET['max_height'])): e($_GET['max_height']); else: echo 0; endif; ?>,
             <?php if (isset($_GET['unlock_ratio']) && intval($_GET['unlock_ratio']) == 0): ?>
@@ -28,16 +28,15 @@ Event.observe( window, 'load', function() {
             <?php endif; ?>
             displayOnInit: true,
             onEndCrop: onEndCrop
-         
+
          }
     );
 } );
-
 
 function onEndCrop(coords, dimensions) {
 	document.getElementById('width').value = Math.floor(dimensions.width * <?php if (isset($_GET['size_ratio'])): echo doubleval($_GET['size_ratio']); else: echo 1; endif; ?>);
 	document.getElementById('height').value = Math.floor(dimensions.height * <?php if (isset($_GET['size_ratio'])): echo doubleval($_GET['size_ratio']); else: echo 1; endif; ?>);
 	document.getElementById('x').value = Math.round(coords.x1 * <?php if (isset($_GET['size_ratio'])): echo doubleval($_GET['size_ratio']); else: echo 1; endif; ?>);
 	document.getElementById('y').value = Math.round(coords.y1 * <?php if (isset($_GET['size_ratio'])): echo doubleval($_GET['size_ratio']); else: echo 1; endif; ?>);
-	
+
 }
