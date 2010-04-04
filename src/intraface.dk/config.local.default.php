69e2b61b2cc360b039f58e4ac106fdebe2a61328
Sune T. Jensen <sj@sunet.dk>
2010-04-04 17:11:32 +0000

    Cleanup

--- src/intraface.dk/config.local.default.php
+++ src/intraface.dk/config.local.default.php
@@ -35,10 +35,9 @@ define('CONNECTION_INTERNET', true); // if the system has access to dns and more
 define('SERVER_STATUS', 'TEST'); // if the system is in PRODUCTION or TEST mode
 
 // error log
-define('ERROR_HANDLE_LEVEL', E_ALL); //  which levels should error_handler take care of: E_ALL
-define('ERROR_LEVEL_CONTINUE_SCRIPT', 0); // Which level should the script continue executing. Development: 0, Production: E_USER_NOTICE ^ E_NOTICE
-define('ERROR_REPORT_EMAIL', ''); // if you want to recieve an e-mail on every error.
 define('ERROR_LOG', PATH_ROOT.'intraface/log/error.log'); // exact directory and filename
+define('K2_LOG', '/var/lib/wwwrun/intraface/log/k2.log');
+error_reporting(E_ALL ^ E_NOTICE); // E_ALL for development setup.
 
 // timezone and local
 define('TIMEZONE', 'Europe/Copenhagen');
