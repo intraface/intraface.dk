766c70ac5d81f215b49a319ca82ea4728d51e006
Lars Olesen <lars@intraface.dk>
2010-04-05 07:47:51 +0000

    Added number to account

--- src/Intraface/modules/accounting/Controller/templates/daybook/posts.tpl.php
+++ src/Intraface/modules/accounting/Controller/templates/daybook/posts.tpl.php
@@ -1,14 +1,14 @@
 <table class="stripe">
-<caption>Poster i kassekladden</caption>
+<caption><?php e(t('Posts in the daybook')); ?></caption>
 <thead>
     <tr>
-        <th>Dato</th>
-        <th>Bilag</th>
-        <th>Tekst</th>
-        <th>Konto</th>
-        <th>Debet</th>
-        <th>Kredit</th>
-        <th>Reference</th>
+        <th><?php e(t('Date')); ?></th>
+        <th><?php e(t('Voucher')); ?></th>
+        <th><?php e(t('Text')); ?></th>
+        <th><?php e(t('Account')); ?></th>
+        <th><?php e(t('Debet')); ?></th>
+        <th><?php e(t('Credit')); ?></th>
+        <th><?php e(t('Reference')); ?></th>
         <th>&nbsp;</th>
     </tr>
 </thead>
@@ -18,7 +18,7 @@
         <td><?php e($p['date_dk']); ?></td>
         <td><a href="<?php e(url('../voucher/' . $p['voucher_id'])); ?>"><?php e($p['voucher_number']); ?></a></td>
         <td><?php e($p['text']); ?></td>
-        <td><a href="<?php e(url('../account/' . $p['account_id'])); ?>"><?php e($p['account_name']); ?></a></td>
+        <td><a href="<?php e(url('../account/' . $p['account_id'])); ?>"><?php e($p['account_number']); ?> <?php e($p['account_name']); ?></a></td>
         <td class="amount"><?php e(amountToOutput($p['debet'])); ?></td>
         <td class="amount"><?php e(amountToOutput($p['credit'])); ?></td>
         <td><?php if (!empty($p['reference'])) e($p['reference']); ?></td>
