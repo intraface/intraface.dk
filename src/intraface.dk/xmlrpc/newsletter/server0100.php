5ee8547b2efb30c679e00421ce36e836f05223fa
Lars Olesen <lars@intraface.dk>
2010-05-22 10:12:34 +0000

    No reason to inject either bucker nor doctrine, as it is not used.

--- src/intraface.dk/xmlrpc/newsletter/server0100.php
+++ src/intraface.dk/xmlrpc/newsletter/server0100.php
@@ -5,5 +5,5 @@ XML_RPC2_Backend::setBackend('php');
 $HTTP_RAW_POST_DATA = file_get_contents('php://input');
 $options = array('prefix' => 'newsletter.', 'encoding' => 'utf-8');
 
-$server = XML_RPC2_Server::create(new Intraface_XMLRPC_Newsletter_Server0100($bucket->get('Doctrine_Connection_Common')), $options);
+$server = XML_RPC2_Server::create(new Intraface_XMLRPC_Newsletter_Server0100(), $options);
 $server->handleCall();
