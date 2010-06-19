77a9656ec7371cf5530a92cfb9526d3ce91ee62d
Lars Olesen <lars@intraface.dk>
2010-06-19 23:07:57 +0000

    Removed old comment directory.

--- src/Intraface/modules/comment/Controller/Index.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Intraface_modules_comment_Controller_Index extends k_Component
-{
-    function renderHtml()
-    {
-        return new k_SeeOther($this->url('../../../../modules/comment'));
-    }
-}
