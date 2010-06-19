dcb8e033788c0f8f7e776355dcc85ad6714fda6e
Lars Olesen <lars@intraface.dk>
2010-06-19 22:55:03 +0000

    DB sql injected instead of started directly.

--- src/Intraface/Controller/SwitchIntranet.php
+++ src/Intraface/Controller/SwitchIntranet.php
@@ -10,22 +10,24 @@
 class Intraface_Controller_SwitchIntranet extends k_Component
 {
     protected $template;
+    protected $db;
 
-    function __construct(k_TemplateFactory $template)
+    function __construct(k_TemplateFactory $template, DB_Sql $db)
     {
         $this->template = $template;
+        $this->db = $db;
     }
 
     function renderHtml()
     {
         $this->document->setTitle('Switch intranet');
 
-        if (isset($_GET["id"]) && $this->getKernel()->user->hasIntranetAccess($_GET['id'])) {
+        if ($this->getKernel()->user->hasIntranetAccess($this->query('id'))) {
             // @todo make sure a new user is stored in Auth, otherwise
             //       the access to the modules are not correctly maintained.
             //       Right now I just clear permisions when getting the new user
             //       which probably is the most clever solution.
-        	if ($this->getKernel()->user->setActiveIntranetId(intval($_GET['id']))) {
+        	if ($this->getKernel()->user->setActiveIntranetId(intval($this->query('id')))) {
         		return new k_SeeOther($this->url('../'));
         	} else {
         		throw new Exception('Could not change intranet');
@@ -38,17 +40,10 @@ class Intraface_Controller_SwitchIntranet extends k_Component
 
     function getIntranets()
     {
-        // @todo b�r hente en liste vha. intranethall�jsaen
-        $db = new DB_Sql;
-        $db->query("SELECT DISTINCT(intranet.id), name FROM intranet INNER JOIN permission ON permission.intranet_id = intranet.id WHERE permission.user_id = " . $this->getKernel()->user->getId() . " ORDER BY name");
+        $this->db->query("SELECT DISTINCT(intranet.id), name FROM intranet INNER JOIN permission ON permission.intranet_id = intranet.id WHERE permission.user_id = " . $this->getKernel()->user->getId() . " ORDER BY name");
         $accessible_intranets = array();
-        while ($db->nextRecord()) {
-            /*
-            if (!$this->getKernel()->user->hasIntranetAccess($db->f("id"))) {
-                continue;
-            }
-            */
-            $accessible_intranets[$db->f('id')] = $db->f('name');
+        while ($this->db->nextRecord()) {
+            $accessible_intranets[$this->db->f('id')] = $this->db->f('name');
         }
 
         return $accessible_intranets;
