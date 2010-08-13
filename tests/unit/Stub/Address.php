46c1d553a4d2697f7f5058feeb83012e7fcc3bca
Sune T. Jensen <sj@sunet.dk>
2010-08-13 20:11:29 +0000

    Corrected character

--- tests/unit/Stub/Address.php
+++ tests/unit/Stub/Address.php
@@ -3,7 +3,7 @@ class Stub_Address
 {
     function get($key = '')
     {
-        $info = array('name' => 'Lars Olesen', 'address' => 'Græsvangen 8, Syvsten', 'postcode' => 9300, 'city' => 'Aarhus N', 'country' => 'Danmark', 'cvr' => '123456789', 'ean' => '', 'phone' => '75820811', 'email' => 'lars@legestue.net', 'address_id' => 1);
+        $info = array('name' => 'Lars Olesen', 'address' => 'GrÃ¦svangen 8, Syvsten', 'postcode' => 9300, 'city' => 'Aarhus N', 'country' => 'Danmark', 'cvr' => '123456789', 'ean' => '', 'phone' => '75820811', 'email' => 'lars@legestue.net', 'address_id' => 1);
         if (empty($key)) return $info;
         else return $info[$key];
     }
