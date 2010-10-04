2006771004ce5a5dcd6fa609edb7fc02278ca903
Lars Olesen <lars@intraface.dk>
2010-10-04 19:31:27 +0000

    Removed encoding.

--- src/intraface.dk/xmlrpc/contact/server.php
+++ src/intraface.dk/xmlrpc/contact/server.php
@@ -4,7 +4,7 @@ require_once '../../common.php';
 XML_RPC2_Backend::setBackend('php');
 $HTTP_RAW_POST_DATA = file_get_contents('php://input');
 
-$options = array('prefix' => 'contact.', 'encoding' => 'iso-8859-1');
+$options = array('prefix' => 'contact.');
 
 $server = XML_RPC2_Server::create(new Intraface_XMLRPC_Contact_Server(), $options);
 $server->handleCall();
