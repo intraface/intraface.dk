66b9b3a85de8dc360cd5463909856cb6911f05cf
Lars Olesen <lars@intraface.dk>
2010-10-05 23:03:14 +0200

    Added t() to make translation.

--- src/Intraface/Controller/templates/delete.tpl.php
+++ src/Intraface/Controller/templates/delete.tpl.php
@@ -1,4 +1,4 @@
 <form action="<?php e(url('.', array('delete'))); ?>" method="post">
-<div>Are you sure, you want to delete?</div>
+<div><?php e(t('Are you sure, you want to delete?')); ?></div>
 <div><input type="hidden" name="_method" value="delete" /><input type="submit" name="delete" value="<?php e(t('Delete')); ?>" /></div>
 </form>
