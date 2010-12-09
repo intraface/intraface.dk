78b7fc5137222ed27ae396d8059df62a7e442332
Lars Olesen <lars@intraface.dk>
2010-12-09 13:52:51 +0100

    Moved stubs to Stub/Keyword.php

--- tests/unit/Stub/Keyword.php
+++ tests/unit/Stub/Keyword.php
@@ -1,19 +1,4 @@
 <?php
-class FakeKeywordAppendObject
-{
-    public $kernel;
-
-    function __construct()
-    {
-        $this->kernel = new Stub_Kernel;
-    }
-
-    function get()
-    {
-        return 1;
-    }
-}
-
 class FakeKeywordAppendKeyword
 {
     public $id;
@@ -33,5 +18,45 @@ class FakeKeywordAppendKeyword
     function getKeyword()
     {
         return $this->keyword;
+    }
+}
+
+class FakeKeywordAppendObject
+{
+    public $kernel;
+
+    function __construct()
+    {
+        $this->kernel = new Stub_Kernel;
+    }
+
+    function get()
+    {
+        return 1;
+    }
+}
+
+class FakeKeywordObject
+{
+    public $kernel;
+
+    function __construct()
+    {
+        $this->kernel = new Stub_Kernel;
+    }
+
+    function get()
+    {
+        return 1;
+    }
+}
+
+class MyStringKeyword extends Keyword
+{
+    function __construct($object, $id = 0)
+    {
+        $this->registerType(1, 'cms');
+        $this->registerType(2, 'contact');
+        parent::__construct($object, $id);
     }
 }
