8ac00394a1b1eba60e52ea7b09ba0150900268e9
Lars Olesen <lars@intraface.dk>
2010-12-14 00:44:42 +0100

    Moved whitelist to phpunit.xml

--- tests/unit/AllTests.php
+++ tests/unit/AllTests.php
@@ -2,21 +2,6 @@
 require_once 'k.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
-PHPUnit_Util_Filter::addDirectoryToWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/'), '.php');
-PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/certificates/'), '.php');
-PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/'), 'tpl.php');
-PHPUnit_Util_Filter::removeDirectoryFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/config/'), '.php');
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/accounting/include_frontpage.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/administration/include_frontpage.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/contact/include_frontpage.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/invoice/include_front.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/onlinepayment/include_front.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/order/include_front.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/procurement/include_frontpage.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/product/include_frontpage.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/todo/include_frontpage.php'));
-PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../../src/Intraface/modules/modulepackage/include_front.php'));
-
 class AllTests
 {
     public static function suite()
