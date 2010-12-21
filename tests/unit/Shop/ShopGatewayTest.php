4aeac957f750b0f79ce5357c9ed0b1efa20870ab
Lars Olesen <lars@intraface.dk>
2010-12-21 18:25:11 +0100

    Moved truncate to tearDown()

--- tests/unit/Shop/ShopGatewayTest.php
+++ tests/unit/Shop/ShopGatewayTest.php
@@ -7,18 +7,19 @@ class ShopGatewayTest extends PHPUnit_Framework_TestCase
 {
     private $gateway;
     protected $backupGlobals = FALSE;
+    protected $db;
 
     function setUp()
     {
-        $db = MDB2::singleton(DB_DSN);
-        $result = $db->query('TRUNCATE shop');
-
-
-        // $this->kernel = new Stub_Kernel;
+        $this->db = MDB2::singleton(DB_DSN);
         $this->gateway = new Intraface_modules_shop_Shop_Gateway;
-        
     }
-    
+
+    function tearDown()
+    {
+        $result = $this->db->query('TRUNCATE shop');
+    }
+
     private function createShop($values = array())
     {
         $shop = new Intraface_modules_shop_Shop();
@@ -27,7 +28,7 @@ class ShopGatewayTest extends PHPUnit_Framework_TestCase
             $shop->intranet_id = $values['intranet_id'];
         }
         $shop->save();
-        
+
         return $shop->getId();
     }
 
@@ -41,12 +42,12 @@ class ShopGatewayTest extends PHPUnit_Framework_TestCase
     function testFindById()
     {
         $id = $this->createShop();
-        
+
         $shop = $this->gateway->findById($id);
         $this->assertEquals(1, $shop->getId());
         $this->assertEquals('Test shop', $shop->getName());
     }
-    
+
     function testFindAll()
     {
         $id = $this->createShop();
@@ -54,7 +55,7 @@ class ShopGatewayTest extends PHPUnit_Framework_TestCase
         Intraface_Doctrine_Intranet::singleton(2);
         $id = $this->createShop();
         Intraface_Doctrine_Intranet::singleton(1);
-        
+
         $collection = $this->gateway->findAll();
         $this->assertEquals(2, $collection->count());
     }
