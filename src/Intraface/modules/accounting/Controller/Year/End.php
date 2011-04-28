a9aaa3865c1598875f1aad62125013be7cbc69d0
Lars Olesen <lars@intraface.dk>
2011-04-28 09:58:08 +0200

    Cleaned up file

--- src/Intraface/modules/accounting/Controller/Year/End.php
+++ src/Intraface/modules/accounting/Controller/Year/End.php
@@ -8,96 +8,6 @@ class Intraface_modules_accounting_Controller_Year_End extends k_Component
         $this->template = $template;
     }
 
-	function POST()
-	{
-        $year = $this->getYear();
-        $account = new Account($year);
-        $year_end = new YearEnd($year);
-        $post = new Post(new Voucher($year));
-        $vat_period = new VatPeriod($year);
-
-	    // disse g�r det muligt let at skifte mellem trinene
-        if (!empty($_POST['previous'])) {
-        	$year_end = new YearEnd($year);
-        	$year_end->setStep($_POST['step'] - 2);
-
-        }
-        elseif (!empty($_POST['next'])) {
-        	$year_end = new YearEnd($year);
-        	$year_end->setStep($_POST['step']);
-        }
-
-        // her reagerer vi p� de forskellige trin
-        if (!empty($_POST['step_save_result'])) {
-        	$year_end = new YearEnd($year);
-
-        	if (!$year_end->saveStatement('operating')) {
-        		throw new Exception('Kunne ikke gemme resultatopgørelsen');
-        	}
-        	// her skal den s� gemme resultatopg�relsen.
-
-        	$year_end->setStep($_POST['step']);
-        }
-
-        elseif (!empty($_POST['step_save_balance'])) {
-        	$year_end = new YearEnd($year);
-
-        	if (!$year_end->saveStatement('balance')) {
-        		throw new Exception('Kunne ikke gemme balancen');
-        	}
-
-        	$year_end->setStep($_POST['step']);
-        }
-        elseif (!empty($_POST['step_transfer_result'])) {
-        	$year_end = new YearEnd($year);
-
-        	if (!$year_end->resetYearResult()) {
-        		throw new Exception('Kunne ikke nulstille årets resultat');
-        	}
-
-        	$year_end->setStep($_POST['step']);
-        }
-        elseif (!empty($_POST['step_reverse_result_account_reset'])) {
-        	$year_end = new YearEnd($year);
-        	if (!$year_end->resetYearResult('reverse')) {
-        		echo $year_end->error->view();
-        		throw new Exception('Kunne ikke tilbageføre årets resultat');
-        	}
-        	$year_end->setStep($_POST['step'] - 1);
-        }
-
-        // step 1
-        elseif (!empty($_POST['step_things_stated'])) {
-        	$year_end = new YearEnd($year);
-        	$year_end->setStep($_POST['step']);
-        }
-
-        // overf�rsel af �rsopg�relsen
-        elseif (!empty($_POST['step_result'])) {
-        	$year_end = new YearEnd($year);
-        	$account = new Account($year);
-        	$year->setSetting('result_account_id', $_POST['result_account_id']);
-
-        	if ($year_end->resetOperatingAccounts()) {
-        		$year_end->setStep($_POST['step']);
-        	} else {
-        		echo $year_end->error->view();
-        	}
-        } elseif (!empty($_POST['step_lock_year'])) {
-        	if (!empty($_POST['lock']) AND $_POST['lock'] == '1') {
-        		$year->lock();
-        	}
-        	$year_end = new YearEnd($year);
-        	$year_end->setStep($_POST['step']);
-        } elseif (!empty($_POST['step_reverse_result_reset'])) {
-        	$year_end = new YearEnd($year);
-        	$year_end->resetOperatingAccounts('reverse');
-        	$year_end->setStep($_POST['step'] - 1);
-        }
-
-        return $this->render();
-	}
-
 	function renderHtml()
 	{
         $smarty = $this->template->create(dirname(__FILE__) . '/../templates/year/end');
@@ -195,15 +105,99 @@ class Intraface_modules_accounting_Controller_Year_End extends k_Component
 
         }
 
-
-
-
         $worksheet->hideGridLines();
-
-        // Let's send the file
         $workbook->close();
-
     }
+    
+	function postForm()
+	{
+        $year = $this->getYear();
+        $account = new Account($year);
+        $year_end = new YearEnd($year);
+        $post = new Post(new Voucher($year));
+        $vat_period = new VatPeriod($year);
+
+	    // These makes it possible to switch between steps
+        if (!empty($_POST['previous'])) {
+        	$year_end = new YearEnd($year);
+        	$year_end->setStep($_POST['step'] - 2);
+
+        }
+        elseif (!empty($_POST['next'])) {
+        	$year_end = new YearEnd($year);
+        	$year_end->setStep($_POST['step']);
+        }
+
+        // reacts to the different steps
+        if (!empty($_POST['step_save_result'])) {
+        	$year_end = new YearEnd($year);
+
+        	if (!$year_end->saveStatement('operating')) {
+        		throw new Exception('Kunne ikke gemme resultatopgørelsen');
+        	}
+        	// @todo save "resultatopgørelsen"
+
+        	$year_end->setStep($_POST['step']);
+        }
+
+        elseif (!empty($_POST['step_save_balance'])) {
+        	$year_end = new YearEnd($year);
+
+        	if (!$year_end->saveStatement('balance')) {
+        		throw new Exception('Kunne ikke gemme balancen');
+        	}
+
+        	$year_end->setStep($_POST['step']);
+        }
+        elseif (!empty($_POST['step_transfer_result'])) {
+        	$year_end = new YearEnd($year);
+
+        	if (!$year_end->resetYearResult()) {
+        		throw new Exception('Kunne ikke nulstille årets resultat');
+        	}
+
+        	$year_end->setStep($_POST['step']);
+        }
+        elseif (!empty($_POST['step_reverse_result_account_reset'])) {
+        	$year_end = new YearEnd($year);
+        	if (!$year_end->resetYearResult('reverse')) {
+        		echo $year_end->error->view();
+        		throw new Exception('Kunne ikke tilbageføre årets resultat');
+        	}
+        	$year_end->setStep($_POST['step'] - 1);
+        }
+
+        // step 1
+        elseif (!empty($_POST['step_things_stated'])) {
+        	$year_end = new YearEnd($year);
+        	$year_end->setStep($_POST['step']);
+        }
+
+        // transfer "resultatopgørelsen"
+        elseif (!empty($_POST['step_result'])) {
+        	$year_end = new YearEnd($year);
+        	$account = new Account($year);
+        	$year->setSetting('result_account_id', $_POST['result_account_id']);
+
+        	if ($year_end->resetOperatingAccounts()) {
+        		$year_end->setStep($_POST['step']);
+        	} else {
+        		echo $year_end->error->view();
+        	}
+        } elseif (!empty($_POST['step_lock_year'])) {
+        	if (!empty($_POST['lock']) AND $_POST['lock'] == '1') {
+        		$year->lock();
+        	}
+        	$year_end = new YearEnd($year);
+        	$year_end->setStep($_POST['step']);
+        } elseif (!empty($_POST['step_reverse_result_reset'])) {
+        	$year_end = new YearEnd($year);
+        	$year_end->resetOperatingAccounts('reverse');
+        	$year_end->setStep($_POST['step'] - 1);
+        }
+
+        return $this->render();
+	}
 
     function getAccount()
     {
