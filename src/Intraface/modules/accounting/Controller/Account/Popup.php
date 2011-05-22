03340910e45987c2150abf245ca0087451d19fb0
Lars Olesen <lars@intraface.dk>
2011-05-22 20:44:19 +0200

    Reverted to k_HttpResponse

--- src/Intraface/modules/accounting/Controller/Account/Popup.php
+++ src/Intraface/modules/accounting/Controller/Account/Popup.php
@@ -28,7 +28,7 @@ class Intraface_modules_accounting_Controller_Account_Popup extends k_Component
 
         $smarty = $this->template->create(dirname(__FILE__) . '/../templates/account/popup');
 
-        return new k_HtmlResponse($smarty->render($this));
+        return new k_HttpResponse(200, $smarty->render($this));
     }
 
     function getDocument()
