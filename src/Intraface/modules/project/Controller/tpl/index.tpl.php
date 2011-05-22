f4a9733275cff373f5a523d9b44ba7aa658fb5fe
Lars Olesen <lars@intraface.dk>
2011-05-22 20:39:55 +0200

    Removed unused modul project

--- src/Intraface/modules/project/Controller/tpl/index.tpl.php
+++
@@ -1,7 +0,0 @@
-<h1><?php e(t('Projects')); ?></h1>
-
-<ul class="options">
-    <li><a href="<?php e(url('create')); ?>"><?php e(t('Create')); ?></a></li>
-</ul>
-
-<p>This module is under construction. Check back after next update, and it might be ready.</p>
