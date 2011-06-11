523b5f0970382a1dd1df589977ec6c34d29575c8
Lars Olesen <lars@intraface.dk>
2011-06-11 20:59:49 +0200

    E_DEPRECATED not reported in error_reporting() as many subpackages has deprecated behavior

--- src/intraface.dk/demo/config.local.example.php
+++ src/intraface.dk/demo/config.local.example.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL &~E_DEPRECATED);
 set_include_path(get_include_path());
+
 define('INTRAFACE_XMLPRC_SERVER_PATH', 'http://localhost/intraface/intraface.dk/xmlrpc/');
 define('PATH_CACHE', '.');
+$GLOBALS['masterpassword'] = 'xxx';
--- tests/unit/config.test.example.php
+++ tests/unit/config.test.example.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL &~E_DEPRECATED);
+
 define('DB_HOST', 'localhost');
 define('DB_PASS', '');
 define('DB_USER', 'root');
