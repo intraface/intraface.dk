21ddc325133fcfe1f219310a985a8cae733777e1
Lars Olesen <lars@intraface.dk>
2011-06-12 20:00:43 +0200

    Removed unused setting

--- src/Intraface/config/setting_kernel.php
+++ src/Intraface/config/setting_kernel.php
@@ -1,31 +1,19 @@
 <?php
-/**
- * @todo Hvad gør vi egentlig med disse settings. Bør settings, der hører de
- * enkelte moduler til være i en setting-fil under modulet, eller hvad var vi
- * blevet enige om? /LO
- *
- */
- 
 // userprefences
 $_setting['rows_pr_page'] = 20;
 $_setting['theme'] = 2;
 $_setting['ptextsize'] = 'small';
 
-#
-# Muligheder
-# 0 => 3x7
-# 1 => 2x8
-#
+// Muligheder
+// 0 => 3x7
+// 1 => 2x8
+//
 
 $_setting['htmleditor'] = 'tinymce';
 
 $_setting['label'] = 0;
 
 $_setting['language'] = 'dk';
-
-// skulle bruges til at man altid loggende ind i systemet, hvor man var - 
-// se kernel - men kan ikke få det til at virke
-$_setting['kernel.last_page'] = 'http://www.intraface.dk/main/index.php';
 
 $_setting['homepage.message'] = 'view';
 $_setting['homepage.last_view'] = '0000-00-00 00:00:00';
@@ -38,8 +26,8 @@ $_setting['bank_account_number'] = '';
 $_setting['giro_account_number'] = '';
 
 $_setting['webshop.show_online'] = 0;
-$_setting['webshop.confirmation_text'] = 'Tak for din bestilling i vores webshop. Vi behandler ordrene så hurtigt vi kan.';
-$_setting['webshop.webshop_receipt'] = "<h3>Nu sker der følgende</h3>\n\n<ol>\n  <li>\n    <h4>Ordren modtages</h4>\n    <p>Vi modtager ordren og behandler den i løbet af højst 3 arbejdsdage.</p>\n  </li>\n  <li>\n    <h4>Ordren sendes</h4>\n    <p>Ordren sendes som almindelig brev med Post Danmark. De bruger typisk 1-2 dage med at få produktet frem.</p>\n  </li>\n  <li>\n    <h4>Du pakker ud</h4>\n    <p>Forventningsfuld åbner du brevet og begynder at kigge på tingene. Hvis der er nogle problemer med leverancen, kontakter du os.</p>\n  </li>\n</ol>";
+$_setting['webshop.confirmation_text'] = 'Tak for din bestilling i vores webshop. Vi behandler ordrene sï¿½ hurtigt vi kan.';
+$_setting['webshop.webshop_receipt'] = "<h3>Nu sker der fï¿½lgende</h3>\n\n<ol>\n  <li>\n    <h4>Ordren modtages</h4>\n    <p>Vi modtager ordren og behandler den i lï¿½bet af hï¿½jst 3 arbejdsdage.</p>\n  </li>\n  <li>\n    <h4>Ordren sendes</h4>\n    <p>Ordren sendes som almindelig brev med Post Danmark. De bruger typisk 1-2 dage med at fï¿½ produktet frem.</p>\n  </li>\n  <li>\n    <h4>Du pakker ud</h4>\n    <p>Forventningsfuld ï¿½bner du brevet og begynder at kigge pï¿½ tingene. Hvis der er nogle problemer med leverancen, kontakter du os.</p>\n  </li>\n</ol>";
 
 $_setting['comment.gravatar'] = 'show';
 $_setting['comment.gravatar.default_url'] = 'http://www.intraface.dk/images/gravatar/gravatar-default.gif';
