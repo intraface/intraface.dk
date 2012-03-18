249a1d38558c0626fec7c43fe6225649f94c41df
Lars Olesen <lars@intraface.dk>
2012-03-18 14:00:57 +0100

    Cleaned up code to make it more readable

--- src/Intraface/modules/accounting/Controller/State/Procurement.php
+++ src/Intraface/modules/accounting/Controller/State/Procurement.php
@@ -17,71 +17,41 @@ class Intraface_modules_accounting_Controller_State_Procurement extends k_Compon
 
     function renderHtml()
     {
-        $procurement_module = $this->getKernel()->module('procurement');
-        $accounting_module = $this->getKernel()->useModule('accounting');
+        $this->getKernel()->module('procurement');
+        $this->getKernel()->useModule('accounting');
 
-        $year = $this->getYear();
-        $voucher = new Voucher($year);
+        $voucher = new Voucher($this->getYear());
         if (!$this->getYear()->readyForState($this->getModel()->get('this_date'))) {
             return new k_SeeOther($this->url('selectyear'));
         }
-        $procurement = $this->getProcurement();
-        $items = $procurement->getItems();
-        $i = 0;
-
-        if (count($items) > 0) {
-            // @todo what should be implemented
-            // implement to a line for each item
-        }
-        /* @todo What should this be used for?
-         if ($procurement->get('price_items') - $items_amount > 0) {
-         $this->value['debet_account'][$i++] = array('text' => '', 'amount' => number_format($procurement->get('price_items') - $items_amount, 2, ',', '.'));
-         }
-
-         if ($procurement->get('price_shipment_etc') > 0) {
-         $this->value['debet_account'][$i++] = array('text' => $this->t('shipment etc'), 'amount' => $procurement->get('dk_price_shipment_etc'));
-         }
-         */
-        $smarty = $this->template->create(dirname(__FILE__) . '/../templates/state/procurement');
 
         $data = array(
-        	'procurement' => $procurement,
-        	'year' => $year,
+        	'procurement' => $this->getProcurement(),
+        	'year' => $this->getYear(),
         	'voucher' => $voucher,
-        	'items' => $items,
+        	'items' => $this->getProcurement()->getItems(),
         	'value' => $this->getValues());
+        $this->document->setTitle('State procurement #' . $this->getProcurement()->get('id'));
 
+        $smarty = $this->template->create(dirname(__FILE__) . '/../templates/state/procurement');
         return $smarty->render($this, $data);
-
     }
 
     function postForm()
     {
-        $procurement_module = $this->getKernel()->module('procurement');
-        $accounting_module = $this->getKernel()->useModule('accounting');
+        $this->getKernel()->module('procurement');
+        $this->getKernel()->useModule('accounting');
 
-        $procurement = $this->getProcurement();
-
-        if (isset($_POST['state'])) {
-            if ($procurement->checkStateDebetAccounts($this->getYear(), $_POST['debet_account'])) {
-                if ($procurement->state($this->getYear(), $_POST['voucher_number'], $_POST['voucher_date'], $_POST['debet_account'], (int)$_POST['credit_account_number'], $this->getKernel()->getTranslation('procurement'))) {
+        if ($this->body('state')) {
+            if ($this->getProcurement()->checkStateDebetAccounts($this->getYear(), $this->body('debet_account'))) {
+                if ($this->getProcurement()->state($this->getYear(), $this->body('voucher_number'), $this->body('voucher_date'), $this->body('debet_account'), (int)$this->body('credit_account_number'), $this->getKernel()->getTranslation('procurement'))) {
                     return new k_SeeOther($this->url('../'));
                 }
-                $procurement->error->set('Kunne ikke bogføre posten');
+                $this->getProcurement()->error->set('Kunne ikke bogføre posten');
             }
         }
 
         return $this->render();
-    }
-
-    function getYear()
-    {
-        return $year = new Year($this->getKernel());
-    }
-
-    function getProcurement()
-    {
-        return $this->context->getProcurement();
     }
 
     function getValues()
@@ -89,11 +59,11 @@ class Intraface_modules_accounting_Controller_State_Procurement extends k_Compon
         $items_amount = 0; // @todo what is this?
         if ($this->body()) {
             $this->value = $this->body();
-            if (isset($_POST['add_line'])) {
+            if ($this->body('add_line')) {
                 array_push($this->value['debet_account'], array('text' => '', 'amount' => '0,00'));
                 return $this->value;
-            } elseif (isset($_POST['remove_line'])) {
-                foreach ($_POST['remove_line'] AS $key => $void) {
+            } elseif ($this->body('remove_line')) {
+                foreach ($this->body('remove_line') AS $key => $void) {
                     array_splice($this->value['debet_account'], $key, 1);
                 }
                 return $this->value;
@@ -104,16 +74,29 @@ class Intraface_modules_accounting_Controller_State_Procurement extends k_Compon
             $procurement = $this->getProcurement();
             $this->value = $procurement->get();
             if ($procurement->get('price_items') - $items_amount > 0) {
-                $this->value['debet_account'][$i++] = array('text' => '',
+                $this->value['debet_account'][$i++] = array(
+                	'text' => '',
                     'amount' => number_format($procurement->get('price_items') - $items_amount, 2, ',', '.'));
             }
 
             if ($procurement->get('price_shipment_etc') > 0) {
-                $this->value['debet_account'][$i++] = array('text' => $this->t('shipment etc'), 'amount' => $procurement->get('dk_price_shipment_etc'));
+                $this->value['debet_account'][$i++] = array(
+                	'text' => $this->t('shipment etc'),
+                	'amount' => $procurement->get('dk_price_shipment_etc'));
             }
         }
 
         return $this->value;
+    }
+
+    function getYear()
+    {
+        return $this->year = new Year($this->getKernel());
+    }
+
+    function getProcurement()
+    {
+        return $this->context->getProcurement();
     }
 
     function getKernel()
