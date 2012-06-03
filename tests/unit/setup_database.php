861489e7fc989d3c46eb222dbc75772461c8cedd
Lars Olesen <lars@intraface.dk>
2012-06-03 11:11:21 +0200

    File to help Travis CI setup the database for the tests

---
+++ tests/unit/setup_database.php
@@ -0,0 +1,17 @@
+<?php
+define('SERVER_STATUS', 'DEVELOPEMENT');
+define('DB_DSN', 'mysql://root:@localhost/intraface_test');
+define('DB_NAME', 'intraface_test');
+
+require_once 'Ilib/ClassLoader.php';
+$install_class = dirname(__FILE__). '/../../install/Install.php';
+
+if (!file_exists($install_class)) {
+    throw new Exception('The install class is not present. Probably because you should not run it now!');
+    exit;
+}
+require $install_class;
+
+$install = new Intraface_Install;
+$install->dropDatabase();
+$install->createDatabaseSchema();
