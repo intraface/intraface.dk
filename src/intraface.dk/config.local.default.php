35288f492c4c0d0aa3a21b04019a0631136c951e
Lars Olesen <lars@intraface.dk>
2012-11-27 20:31:14 +0100

    Changed some values

--- src/intraface.dk/config.local.default.php
+++ src/intraface.dk/config.local.default.php
@@ -13,43 +13,56 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 // database
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'intraface');
 define('DB_NAME', 'intraface_test');
 
 // net
 define('NET_SCHEME', 'http://'); // http:// or https://
-define('NET_HOST', 'localhost'); // www.intraface.dk
-define('NET_DIRECTORY', '/intraface/src/intraface.dk/'); // subdirectory. if non keep empty
+define('NET_HOST', 'localhost:8080'); // www.intraface.dk
+define('NET_DIRECTORY', '/src/intraface.dk/'); // subdirectory. if non keep empty
+
+// define paths - remember trailing slahs
+define('PATH_ROOT', dirname(__FILE__) . '/../');
+
+// optional upload path - remember trailing slash 
+define('PATH_UPLOAD', '/var/cache/intraface/upload/');
+
+// optional path to cache - remember trailing slash
+define('PATH_CACHE', '/var/cache/intraface/');
+
+// set include path
+define('PATH_INCLUDE_PATH', PATH_ROOT . PATH_SEPARATOR . get_include_path());
+set_include_path(PATH_INCLUDE_PATH);
+
+// error logs
+define('ERROR_LOG', '/var/log/intraface/error.log'); // exact directory and filename
+define('K2_LOG', '/var/log/intraface/k2.log');
 
 // smtp
 define('SMTP_USERNAME', '');
 define('SMTP_PASSWORD', '');
 define('SMTP_HOST', '');
 
-// paths
-define('PATH_ROOT', dirname(__FILE__) . '/../'); // remember trailing slash
-define('PATH_INCLUDE_PATH', PATH_ROOT . PATH_SEPARATOR . get_include_path()); // remember to use constant PATH_SEPARATOR after every path
-// optional: define('PATH_UPLOAD', '/home/.investor/intraface/upload/'); // remember trailing slash
-// optional: define('PATH_CACHE', PATH_ROOT . 'cache/'); // remember trailing slash - path to cache
-
-//
-define('CONNECTION_INTERNET', true); // if the system has access to dns and more from the internet. true or false
-define('SERVER_STATUS', 'TEST'); // if the system is in PRODUCTION or TEST mode
-
-// error log
-define('ERROR_LOG', PATH_ROOT.'intraface/log/error.log'); // exact directory and filename
-define('K2_LOG', '/var/lib/wwwrun/intraface/log/k2.log');
-
 // timezone and local
 define('TIMEZONE', 'Europe/Copenhagen');
 define('COUNTRY_LOCAL', 'da_DK');
 
-// for intranet maintenance
-define('INTRAFACE_INTRANETMAINTENANCE_INTRANET_PRIVATE_KEY', 'privatekeyshouldbereplaced'); // the private key of the intranet that has intranetmaintenance
-define('INTRAFACE_INTRANETMAINTENANCE_SHOP_ID', 0); // the id of the shop used to modulepackage
-define('INTRAFACE_XMLRPC_SERVER_URL', ''); // the url for intraface xml-rpc server. Empty for the default url.
-define('INTRAFACE_XMLRPC_DEBUG', false);
+// for intranet maintenance 
+// the private key of the intranet that has intranetmaintenance
+define('INTRAFACE_INTRANETMAINTENANCE_INTRANET_PRIVATE_KEY', 'privatekeyshouldbereplaced'); 
 
+// the id of the shop used to modulepackage
+define('INTRAFACE_INTRANETMAINTENANCE_SHOP_ID', 0); 
+
+// the url for intraface xml-rpc server. Empty for the default url.
+define('INTRAFACE_XMLRPC_SERVER_URL', '');
+define('INTRAFACE_XMLRPC_DEBUG', false);
 define('INTRAFACE_ONLINEPAYMENT_PROVIDER', 'Testing');
 define('INTRAFACE_ONLINEPAYMENT_MERCHANT', '123');
 define('INTRAFACE_ONLINEPAYMENT_MD5SECRET', 'fake');
+
+// if the system has access to dns and more from the internet. true or false
+define('CONNECTION_INTERNET', true);
+
+// if the system is in PRODUCTION or TEST mode
+define('SERVER_STATUS', 'TEST'); 
