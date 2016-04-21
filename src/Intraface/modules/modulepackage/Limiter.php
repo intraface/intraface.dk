23c93397de5d6de7eea174d9073cd8be05e377e9
Lars Olesen <lars@intraface.dk>
2016-04-21 17:14:12 +0000

    PHP debug statements found

--- src/Intraface/modules/modulepackage/Limiter.php
+++ src/Intraface/modules/modulepackage/Limiter.php
@@ -4,15 +4,13 @@
  */
 class ModulePackage_Limit
 {
-    
     /**
      * @var array limiters
      */
     private $limiters;
-    
+
     public function __construct($intranet)
     {
-        
         require_once 'Intraface/modules/modulepackage/Manager.php';
         $manager = new Intraface_modules_modulepackage_Manager($intranet);
 
@@ -25,14 +23,11 @@ class ModulePackage_Limit
             $limiters = array_merge($limiters, $package_module->get('limiters'));
         }
         $this->limiters = $limiters;
-        
-        print_r($limiters);
-        
+
     }
-    
-    
+
     public function check($limiter, $assert_value)
     {
-        
+
     }
 }
